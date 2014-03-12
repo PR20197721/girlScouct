2ce614f9bfd6e6eb47c9dd19b721c3646e12136d
Mike Zou <mzou@northps.com>
2014-03-12 10:49:55 -0400

    Initial CQ code check in.

---
+++ cq/bundle/src/test/java/org/girlscouts/web/SimpleUnitTest.java
@@ -0,0 +1,14 @@
+package org.girlscouts.web;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleUnitTest {
+
+    @Test
+    public void someTest() {
+        assertTrue(true);
+    }
+
+}
---
+++ cq/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+VERSION=$1
+
+# Get the current version if version number is not specified
+if [ -z $VERSION ]; then
+    VERSION=`sed '10q;d' pom.xml | sed 's/.*<version>\(.*\)<\/version>/\1/'`
+fi
+
+curl -u admin:admin -F file=@"$HOME/.m2/repository/org/girlscouts/web/girlscouts-content/$VERSION/girlscouts-content-$VERSION.zip" -F name="girlscouts-web" -F force=true -F install=true http://localhost:4502/crx/packmgr/service.jsp
