826dc8cb1d196934498711bfb70070ba5749bc25
Mike Zou <mzou@northps.com>
2014-04-07 12:19:46 -0400

    Change ajax from async to sync

--- web/app/src/main/content/jcr_root/apps/girlscouts/clientlibs/js/main.js
+++ web/app/src/main/content/jcr_root/apps/girlscouts/clientlibs/js/main.js
@@ -3,6 +3,12 @@ girlscouts = {};
 girlscouts.components = {};
 girlscouts.functions = {};
 
-girlscouts.functions.createPath = function(path) {
-	
+girlscouts.functions.createPath = function(path, type) {
+	if (!type) type = "cq:Page";
+	$.ajax({
+		type: 'POST',
+		url: "/apps/girlscouts/wcm/components/path-creator.html", 
+		data: {"path": path, "type" : type},
+		async: false
+	});
 };
