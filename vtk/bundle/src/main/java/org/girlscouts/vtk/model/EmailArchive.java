f02e741fe7bf7685adea98a266f8d5780d7b1624
Mike Zou <mzou@northps.com>
2014-05-08 09:25:50 -0400

    Email Archive

--- vtk/bundle/src/main/java/org/girlscouts/vtk/model/EmailArchive.java
+++ vtk/bundle/src/main/java/org/girlscouts/vtk/model/EmailArchive.java
@@ -1,10 +1,27 @@
 package org.girlscouts.vtk.model;
 
+import java.util.Date;
+
 /**
+ * An email archive.
+ * We do not save the recipients.
  * 
  * @author mike
  *
  */
 public interface EmailArchive {
-
+    /**
+     * @return the type {@link EmailArchiveType}.
+     */
+    EmailArchiveType getType();
+    
+    /**
+     * @return the date that this email was sent.
+     */
+    Date getSentDate();
+    
+    /**
+     * @return the content of this email with HTML tags.
+     */
+    String getContent();
 }
