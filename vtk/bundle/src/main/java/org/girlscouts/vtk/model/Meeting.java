9bf216db919282288cceaa371c645c1d9f8a1f99
Mike Zou <mzou@northps.com>
2014-05-07 21:27:36 -0400

    Meeting interface

---
+++ vtk/bundle/src/main/java/org/girlscouts/vtk/model/Meeting.java
@@ -0,0 +1,21 @@
+package org.girlscouts.vtk.model;
+
+import java.util.List;
+
+public interface Meeting {
+    // B2014B4
+    String getId();
+    // Brownie
+    String getLevel();
+    // Inventor Part One
+    String getName();
+    // Descriptive phase
+    String getBlurb();
+    // Science / Financial 
+    String getCategory();
+    
+    List<Asset> getAids();
+    List<Asset> getResources();
+    
+    List<AgendaItem> getAgendaItems();
+}
