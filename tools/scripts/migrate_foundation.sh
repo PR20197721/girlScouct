ee390feab88def0a8a55e4fa07dd7cf0cdf26996
Mike Zou <mzou@northps.com>
2014-05-29 17:50:30 -0400

    Update migration script with word boundaries.

--- tools/scripts/migrate_foundation.sh
+++ tools/scripts/migrate_foundation.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 # This script migrates from foundation components to girlscouts components
-# /libs/foundation/components/text
-# /libs/foundation/components/textimage
-# /libs/foundation/components/image
+# This is for Mac. For GNU sed, replace [[:<:]] and [[:>:]] with \b
+# This script should only be run under /content, NOT /apps
 
-find . -name '.content.xml' -exec bash -c "sed -i'' -e 's/foundation\/components\/text/girlscouts\/components\/text/g' {};\
-                                           sed -i'' -e 's/foundation\/components\/textimage/girlscouts\/components\/textimage/g' {};\
-                                           sed -i'' -e 's/foundation\/components\/image/girlscouts\/components\/image/g' {};" \;
+find . -name '.content.xml' -exec bash -c "sed -i'' -e 's/[[:<:]]foundation\/components\/text[[:>:]]/girlscouts\/components\/text/g' {};\
+                                           sed -i'' -e 's/[[:<:]]foundation\/components\/textimage[[:>:]]/girlscouts\/components\/textimage/g' {};\
+                                           sed -i'' -e 's/[[:<:]]foundation\/components\/image[[:>:]]/girlscouts\/components\/image/g' {};" \;
