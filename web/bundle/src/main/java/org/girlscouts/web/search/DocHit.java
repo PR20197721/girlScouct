ff992a5a8d91e79abea213ada157039d4c08c2e1
Mike Zou <mzou@northps.com>
2014-06-06 10:36:46 -0400

    Doc search fixed

--- web/bundle/src/main/java/org/girlscouts/web/search/DocHit.java
+++ web/bundle/src/main/java/org/girlscouts/web/search/DocHit.java
@@ -1,19 +1,26 @@
 package org.girlscouts.web.search;
 
 import java.util.Map;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.day.cq.search.result.Hit;
 
 public final class DocHit {
+    private static final Logger log = LoggerFactory.getLogger(DocHit.class);
     private final Hit hit;
-    private static Pattern[] excerptPatterns = {
-        Pattern.compile("^.*?>"),
+    private static final Pattern STRONG_PATTERN = Pattern.compile("<strong>.*?</strong>");
+    private static final Pattern STRIP_STRONG_PATTERN = Pattern.compile("</?strong>");
+    private static final Pattern[] EXCERPT_PATTERNS = {
+        Pattern.compile("<.*?>"),
         Pattern.compile("<.*?$"),
-        Pattern.compile("<.*?>")
+        Pattern.compile("^.*?>")
     };
 
     public DocHit(Hit hit) {
@@ -30,8 +37,20 @@ public final class DocHit {
 
     public String getExcerpt() throws RepositoryException {
         String excerpt = this.hit.getExcerpt();
-        for (Pattern pattern: excerptPatterns) {
+        log.error("#### orig excerpt:" + excerpt);
+        
+        Matcher queryMatcher = STRONG_PATTERN.matcher(excerpt);
+        String queryString = null;
+        if (queryMatcher.find()) {
+            queryString = STRIP_STRONG_PATTERN.matcher(queryMatcher.group()).replaceAll("");
+        }
+
+        for (Pattern pattern: EXCERPT_PATTERNS) {
             excerpt = pattern.matcher(excerpt).replaceAll("");
+        }
+
+        if (queryString != null) {
+            excerpt = excerpt.replaceAll(queryString, "<strong>" + queryString + "</strong>");
         }
         return excerpt;
     }
