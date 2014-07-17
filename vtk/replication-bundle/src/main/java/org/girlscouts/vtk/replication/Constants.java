3bc2444d629d5d4a99985dc24482eb724824bdfa
Mike Zou <mzou@northps.com>
2014-07-17 10:31:45 -0400

    Update constants

--- vtk/replication-bundle/src/main/java/org/girlscouts/vtk/replication/Constants.java
+++ vtk/replication-bundle/src/main/java/org/girlscouts/vtk/replication/Constants.java
@@ -4,7 +4,10 @@ public interface Constants {
     static String PRIMARY_TYPE = "nt:unstructured";
     static String FROM_PUBLISHER_PROPERTY = "fromPublisher";
     static String NODE_REMOVED_PROPERTY = "nodeRemoved";
+    // TODO: make it an OSGI conf
     static String ROOT_PATH = "/vtk";
-    static String NODE_GRAVEYARD_ROOT = "/var/vtk/graveyard";
+    static String NODE_REPLICATION_ROOT = "/var/vtk/replication";
     static String MODE_PROPERTY = "org.girlscouts.vtk.replication.mode";
+    static int EVENT_UPDATE = 0;
+    static int EVENT_REMOVE = 1;
 }
