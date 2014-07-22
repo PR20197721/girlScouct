02d14cbc7f3f97da5b1d59f9151c940d6a2ba12d
Mike Zou <mzou@northps.com>
2014-07-22 16:49:15 -0400

    Update NodeEventCollector

--- vtk/replication-bundle/src/main/java/org/girlscouts/vtk/replication/NodeEventCollector.java
+++ vtk/replication-bundle/src/main/java/org/girlscouts/vtk/replication/NodeEventCollector.java
@@ -1,7 +1,5 @@
 package org.girlscouts.vtk.replication;
 
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
@@ -15,8 +13,6 @@ import org.slf4j.LoggerFactory;
 public class NodeEventCollector {
     private static Logger log = LoggerFactory.getLogger(NodeEventCollector.class);
     private static int PROPERTY_UPDATE = Event.PROPERTY_ADDED | Event.PROPERTY_CHANGED | Event.PROPERTY_REMOVED;
-    private static String[] _IGNORED_NAMESPACES = new String[] {Constants.FROM_PUBLISHER_PROPERTY};
-    private static Set<String> IGNORED_NAMESPACES = new HashSet<String>(Arrays.asList(_IGNORED_NAMESPACES));
     
     public static class NodeEvent {
         private String path;
@@ -33,6 +29,7 @@ public class NodeEventCollector {
         public int getType() {
             return type;
         }
+
         @Override
         public boolean equals(Object obj) {
             return (obj instanceof NodeEvent) && (((NodeEvent)obj).path == this.path);
@@ -48,23 +45,14 @@ public class NodeEventCollector {
     public static Set<NodeEvent> getEvents(EventIterator iter) {
         Set<NodeEvent> nodes = new LinkedHashSet<NodeEvent>();
         
-        int i = 0;
         while (iter.hasNext()) {
             Event event = iter.nextEvent();
             try {
                 String path = event.getPath();
                 int type = event.getType();
-                //////////////////////////////////////
-                log.error("i = " + Integer.toString(i++) + "#### event path = " + path + "### event type = " + event.getType());
 
                 // If this event is about node property change
                 if ((event.getType() & PROPERTY_UPDATE) != 0) {
-                    String property = path.substring(path.lastIndexOf('/') + 1);
-                    String namespace = property.split(":")[0];
-                    if (IGNORED_NAMESPACES.contains(namespace)) {
-                        log.debug("This property is ignored. " + property);
-                        continue;
-                    }
                     path = path.substring(0, path.lastIndexOf('/'));
                 }
                 nodes.add(new NodeEvent(path, type));
