fca48d99f67b9c9355f9dfa1437d87532d0f1041
Mike Zou <mzou@northps.com>
2014-07-22 16:54:06 -0400

    Update Constants

--- vtk/replication-bundle/src/main/java/org/girlscouts/vtk/replication/Constants.java
+++ vtk/replication-bundle/src/main/java/org/girlscouts/vtk/replication/Constants.java
@@ -3,12 +3,13 @@ package org.girlscouts.vtk.replication;
 import javax.jcr.observation.Event;
 
 public interface Constants {
-    static String PRIMARY_TYPE = "nt:unstructured";
-    static String NODE_REMOVED_PROPERTY = "nodeRemoved";
+    String PRIMARY_TYPE = "nt:unstructured";
+    String NODE_REMOVED_PROPERTY = "nodeRemoved";
     // TODO: make it an OSGI conf
-    static String ROOT_PATH = "/vtk";
-    static int EVENT_UPDATE = 0;
-    static int EVENT_REMOVE = 1;
-    static String JOB_NAME = "Publish VTK Node Job";
-    final int PROPERTY_UPDATE = Event.PROPERTY_ADDED | Event.PROPERTY_CHANGED | Event.PROPERTY_REMOVED;
+    String ROOT_PATH = "/vtk";
+    int EVENT_UPDATE = 0;
+    int EVENT_REMOVE = 1;
+    String JOB_NAME = "Publish VTK Node Job";
+    int PROPERTY_UPDATE = Event.PROPERTY_ADDED | Event.PROPERTY_CHANGED | Event.PROPERTY_REMOVED;
+    long REPLICATION_INTERVAL = 60L;
 }
