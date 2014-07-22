9d95ce76c1ee48f2cf5c3d35a0c8213fe973ab42
Mike Zou <mzou@northps.com>
2014-07-22 16:46:57 -0400

    Get NodeEventCollector back.

---
+++ vtk/replication-bundle/src/main/java/org/girlscouts/vtk/replication/NodeEventCollector.java
@@ -0,0 +1,77 @@
+package org.girlscouts.vtk.replication;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import javax.jcr.RepositoryException;
+import javax.jcr.observation.Event;
+import javax.jcr.observation.EventIterator;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NodeEventCollector {
+    private static Logger log = LoggerFactory.getLogger(NodeEventCollector.class);
+    private static int PROPERTY_UPDATE = Event.PROPERTY_ADDED | Event.PROPERTY_CHANGED | Event.PROPERTY_REMOVED;
+    private static String[] _IGNORED_NAMESPACES = new String[] {Constants.FROM_PUBLISHER_PROPERTY};
+    private static Set<String> IGNORED_NAMESPACES = new HashSet<String>(Arrays.asList(_IGNORED_NAMESPACES));
+    
+    public static class NodeEvent {
+        private String path;
+        private int type;
+        
+        public NodeEvent(String path, int type) {
+            this.path = path;
+            this.type = type;
+        }
+        public String getPath() {
+            return path;
+        }
+        
+        public int getType() {
+            return type;
+        }
+        @Override
+        public boolean equals(Object obj) {
+            return (obj instanceof NodeEvent) && (((NodeEvent)obj).path == this.path);
+        }
+
+        @Override
+        public int hashCode() {
+            // Only look at the path, ignore the type
+            return path.hashCode();
+        }
+    }
+  
+    public static Set<NodeEvent> getEvents(EventIterator iter) {
+        Set<NodeEvent> nodes = new LinkedHashSet<NodeEvent>();
+        
+        int i = 0;
+        while (iter.hasNext()) {
+            Event event = iter.nextEvent();
+            try {
+                String path = event.getPath();
+                int type = event.getType();
+                //////////////////////////////////////
+                log.error("i = " + Integer.toString(i++) + "#### event path = " + path + "### event type = " + event.getType());
+
+                // If this event is about node property change
+                if ((event.getType() & PROPERTY_UPDATE) != 0) {
+                    String property = path.substring(path.lastIndexOf('/') + 1);
+                    String namespace = property.split(":")[0];
+                    if (IGNORED_NAMESPACES.contains(namespace)) {
+                        log.debug("This property is ignored. " + property);
+                        continue;
+                    }
+                    path = path.substring(0, path.lastIndexOf('/'));
+                }
+                nodes.add(new NodeEvent(path, type));
+            } catch (RepositoryException e) {
+                log.warn("Cannot get path of a VTK node event.");
+            }
+        }
+        return nodes;
+    }
+}
