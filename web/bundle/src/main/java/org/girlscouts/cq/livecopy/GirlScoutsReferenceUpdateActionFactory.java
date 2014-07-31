3ed4afc8c24924248dff8da038b79adc1aa28c5e
Mike Zou <mzou@northps.com>
2014-07-31 13:15:54 -0400

    LiveAction WIP

--- web/bundle/src/main/java/org/girlscouts/cq/livecopy/GirlScoutsReferenceUpdateActionFactory.java
+++ web/bundle/src/main/java/org/girlscouts/cq/livecopy/GirlScoutsReferenceUpdateActionFactory.java
@@ -1,5 +1,7 @@
 package org.girlscouts.cq.livecopy;
 
+import javax.jcr.Node;
+
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Property;
 import org.apache.felix.scr.annotations.Service;
@@ -16,72 +18,98 @@ import com.day.cq.wcm.msm.api.LiveRelationship;
 
 @Component(metatype=false)
 @Service
-public class GirlScoutsReferenceUpdateActionFactory implements LiveActionFactory {
+public class GirlScoutsReferenceUpdateActionFactory implements LiveActionFactory<LiveAction> {
 
-    @Property(name="liveActionName")
-    private static final String[] LIVE_ACTION_NAME = { GirlScoutsReferenceUpdateActionFactory.GirlScoutsReferenceUpdateAction.class.getSimpleName(), "GSReferencesUpdate" };
+    @Property(value=GirlScoutsReferenceUpdateActionFactory.actionname)
+    private static final String actionname = "GSReferencesUpdate";
 
-    public LiveAction createAction(Resource arg0) throws WCMException {
-        // TODO Auto-generated method stub
+    public LiveAction createAction(Resource resource) throws WCMException {
+        System.err.println("Resource path = " + resource.getPath());
         return null;
     }
 
     public String createsAction() {
-        // TODO Auto-generated method stub
-        return null;
+        return actionname;
     }
     
     public static class GirlScoutsReferenceUpdateAction implements LiveAction {
+        // Level 1 branch. e.g. /content/girlscouts-template
+        private static final int BRANCH_LEVEL = 1;
 
-        public void execute(ResourceResolver arg0, LiveRelationship arg1,
-                ActionConfig arg2, boolean arg3) throws WCMException {
-            // TODO Auto-generated method stub
-            
-        }
-
-        public void execute(Resource arg0, Resource arg1,
-                LiveRelationship arg2, boolean arg3, boolean arg4)
+        public void execute(Resource source, Resource target,
+                LiveRelationship relation, boolean autosave, boolean isResetRollout)
                 throws WCMException {
-            // TODO Auto-generated method stub
+            Node sourceNode = (Node)source.adaptTo(Node.class);
+            Node targetNode = (Node)target.adaptTo(Node.class);
+            if (sourceNode == null) {
+                throw new WCMException("Cannot access node: " + source);
+            }
+            if (targetNode == null) {
+                throw new WCMException("Cannot access node: " + target);
+            }
             
-        }
-
-        public void execute(ResourceResolver arg0, LiveRelationship arg1,
-                ActionConfig arg2, boolean arg3, boolean arg4)
-                throws WCMException {
-            // TODO Auto-generated method stub
+            String sourcePath = source.getPath();
+            String targetPath = target.getPath();
+            String sourceBranch = getBranch(sourcePath);
+            String targetBranch = getBranch(targetPath);
             
+            System.err.println("src = " + sourcePath + " dest = " + targetPath);
         }
-
+        
         public String getName() {
-            // TODO Auto-generated method stub
-            return null;
+            return GirlScoutsReferenceUpdateActionFactory.actionname;
+        } 
+        
+        private String getBranch(String path) throws WCMException {
+            if (!path.startsWith("/")) {
+                throw new WCMException("Not absolute path: " + path);
+            }
+            
+            String[] parts = path.substring(1).split("/"); // Skip the root slash
+            if (parts.length < BRANCH_LEVEL + 1) {
+                throw new WCMException("Cannot get level " + BRANCH_LEVEL + " branch: " + path);
+            }
+            
+            StringBuilder branchBuilder = new StringBuilder();
+            for (int i = 0; i < BRANCH_LEVEL + 1; i++) {
+                branchBuilder.append("/").append(parts[i]);
+            }
+            return branchBuilder.toString();
+        }
+
+        public void execute(ResourceResolver rr, LiveRelationship relation,
+                ActionConfig config, boolean autosave, boolean isResetRollout)
+                throws WCMException {
+            // deprecated 
+        }
+
+        public void execute(ResourceResolver rr, LiveRelationship relation,
+                ActionConfig config, boolean autosave) throws WCMException {
+            // deprecated 
         }
 
         public String getParameterName() {
-            // TODO Auto-generated method stub
+            // deprecated 
             return null;
         }
 
         public String[] getPropertiesNames() {
-            // TODO Auto-generated method stub
+            // deprecated 
             return null;
         }
 
         public int getRank() {
-            // TODO Auto-generated method stub
+            // deprecated 
             return 0;
         }
 
         public String getTitle() {
-            // TODO Auto-generated method stub
+            // deprecated 
             return null;
         }
 
         public void write(JSONWriter arg0) throws JSONException {
-            // TODO Auto-generated method stub
-            
+            // deprecated 
         }
-        
     }
 }
