80c46af5e8abee4b6b99c8d66df42989e0028dbd
Mike Zou <mzou@northps.com>
2014-07-31 16:26:00 -0400

    Reference update working

--- web/bundle/src/main/java/org/girlscouts/cq/livecopy/GirlScoutsReferencesUpdateActionFactory.java
+++ web/bundle/src/main/java/org/girlscouts/cq/livecopy/GirlScoutsReferencesUpdateActionFactory.java
@@ -1,9 +1,13 @@
 package org.girlscouts.cq.livecopy;
 
 import javax.jcr.Node;
+import javax.jcr.Property;
+import javax.jcr.PropertyIterator;
+import javax.jcr.PropertyType;
+import javax.jcr.RepositoryException;
+import javax.jcr.Value;
 
 import org.apache.felix.scr.annotations.Component;
-import org.apache.felix.scr.annotations.Property;
 import org.apache.felix.scr.annotations.Service;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
@@ -16,15 +20,15 @@ import com.day.cq.wcm.msm.api.LiveAction;
 import com.day.cq.wcm.msm.api.LiveActionFactory;
 import com.day.cq.wcm.msm.api.LiveRelationship;
 
+@SuppressWarnings("deprecation")
 @Component(metatype=false)
 @Service
 public class GirlScoutsReferencesUpdateActionFactory implements LiveActionFactory<LiveAction> {
 
-    @Property(name="liveActionName")
+    @org.apache.felix.scr.annotations.Property(name="liveActionName")
     private static final String[] LIVE_ACTION_NAME = { GirlScoutsReferencesUpdateActionFactory.GirlScoutsReferencesUpdateAction.class.getSimpleName(), "gsReferencesUpdate" };
 
     public LiveAction createAction(Resource resource) throws WCMException {
-        System.err.println("Resource path = " + resource.getPath());
         return new GirlScoutsReferencesUpdateAction();
     }
 
@@ -35,7 +39,7 @@ public class GirlScoutsReferencesUpdateActionFactory implements LiveActionFactor
     public static class GirlScoutsReferencesUpdateAction implements LiveAction {
         // Level 1 branch. e.g. /content/girlscouts-template
         private static final int BRANCH_LEVEL = 1;
-
+        
         public void execute(Resource source, Resource target,
                 LiveRelationship relation, boolean autosave, boolean isResetRollout)
                 throws WCMException {
@@ -53,12 +57,62 @@ public class GirlScoutsReferencesUpdateActionFactory implements LiveActionFactor
             String sourceBranch = getBranch(sourcePath);
             String targetBranch = getBranch(targetPath);
             
-            System.err.println("src = " + sourcePath + " dest = " + targetPath);
+            PropertyIterator iter;
+            try {
+                iter = targetNode.getProperties();
+                while (iter.hasNext()) {
+                    Property property = iter.nextProperty();
+                    String propertyName = property.getName();
+                    if (propertyName.startsWith("jcr:") || 
+                            propertyName.startsWith("cq:") ||
+                            propertyName.startsWith("sling:")) {
+                        // Skip CQ properties
+                        continue;
+                    }
+                    if (!property.isMultiple()) {
+                        if (property.getType() == PropertyType.STRING) {
+                            String stringValue = property.getString();
+                            stringValue = replaceBranch(stringValue, sourceBranch, targetBranch);
+                            if (stringValue != null) {
+                                targetNode.setProperty(property.getName(), stringValue);
+                            } 
+                        }
+                    } else {
+                        Value[] values = property.getValues();
+                        if (values.length > 0 && 
+                                values[0].getType() == PropertyType.STRING) { // Values are of the same type.
+                            String[] stringValues = new String[values.length];
+                            boolean replacedFlag = false;
+                            for (int i = 0; i < values.length; i++) {
+                                Value value = values[i];
+                                String stringValue = value.getString();
+                                String newStringValue = replaceBranch(stringValue, sourceBranch, targetBranch);
+                                if (newStringValue != null) {
+                                    stringValues[i] = newStringValue;
+                                    replacedFlag = true;
+                                } else {
+                                    stringValues[i] = stringValue;
+                                }
+                            }
+                            if (replacedFlag) {
+                                targetNode.setProperty(property.getName(), stringValues);
+                            }
+                        }
+                    }
+                }
+            } catch (RepositoryException e) {
+                throw new WCMException(e.getMessage(), e);
+            }
         }
         
-        public String getName() {
-            return GirlScoutsReferencesUpdateActionFactory.LIVE_ACTION_NAME[0];
-        } 
+        private String replaceBranch(String value, String sourceBranch, String targetBranch) {
+            String regex = "\\Q" + sourceBranch + "\\E";
+            if (value.indexOf(sourceBranch) != -1) {
+                return value.replaceAll(regex, targetBranch);
+            } else {
+                return null; // Returns null if not match
+            }
+        }
         
         private String getBranch(String path) throws WCMException {
             if (!path.startsWith("/")) {
@@ -77,39 +131,43 @@ public class GirlScoutsReferencesUpdateActionFactory implements LiveActionFactor
             return branchBuilder.toString();
         }
 
+        public String getName() {
+            return GirlScoutsReferencesUpdateActionFactory.LIVE_ACTION_NAME[0];
+        } 
+        
+        @Deprecated
         public void execute(ResourceResolver rr, LiveRelationship relation,
                 ActionConfig config, boolean autosave, boolean isResetRollout)
                 throws WCMException {
-            // deprecated 
         }
 
+        @Deprecated
         public void execute(ResourceResolver rr, LiveRelationship relation,
                 ActionConfig config, boolean autosave) throws WCMException {
-            // deprecated 
         }
 
+        @Deprecated
         public String getParameterName() {
-            // deprecated 
             return null;
         }
 
+        @Deprecated
         public String[] getPropertiesNames() {
-            // deprecated 
             return null;
         }
 
+        @Deprecated
         public int getRank() {
-            // deprecated 
             return 0;
         }
 
+        @Deprecated
         public String getTitle() {
-            // deprecated 
             return null;
         }
 
-        public void write(JSONWriter arg0) throws JSONException {
-            // deprecated 
+        @Deprecated
+        public void write(JSONWriter writer) throws JSONException {
         }
     }
 }
