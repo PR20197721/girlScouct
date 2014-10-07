e7189eabe2157b17467fd6f40ac775e23400a8c3
gargmanu <gargmanu123@yahoo.com>
2014-10-07 13:29:14 -0400

    Reverted to the head version

--- web/app/src/main/content/jcr_root/etc/designs/girlscouts/clientlibs/js/slideshow.js
+++ web/app/src/main/content/jcr_root/etc/designs/girlscouts/clientlibs/js/slideshow.js
@@ -10,7 +10,6 @@ $(function() {
 	  //function that assigns variable based on viewport size
 	  function assignment() {
 	    var viewportWidth = ($(window).width() + 15);
-	    
 	    switch(true) {
 	      case (viewportWidth < tinySize):
 	        containerHeight = '400px';
@@ -40,11 +39,7 @@ $(function() {
 	  $(window).resize(function() {
 	    assignment();
 	  });
-	  
-	  $(window).resize(function () {
-		    $("#grid").height($("<container for grid>").height());
-		$(".slick-viewport").height($("#grid").height());
-		});
+
 
 	  //necessary for view-b
 	  $(document).on("click", "#SUP1", function(){
@@ -87,6 +82,7 @@ $(function() {
 });
 
 function setTimer(timer,play){
+	//alert("Hello I am here relaxing" +autoplay +timer);
 	var pboolean = play =='true';
 	var playspeed = parseInt(timer);
 	$('.meow').slick({
