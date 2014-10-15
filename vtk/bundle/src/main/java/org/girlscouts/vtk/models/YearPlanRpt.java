190ff1b2249bb720b08c06914994413842ac2021
alexyakobovich <ayakobovich@northps.com>
2014-10-15 09:45:39 -0400

    rpt class

---
+++ vtk/bundle/src/main/java/org/girlscouts/vtk/models/YearPlanRpt.java
@@ -0,0 +1,41 @@
+package org.girlscouts.vtk.models;
+
+public class YearPlanRpt {
+
+		String council, troop, troopAge, troopName;
+
+		public String getCouncil() {
+			return council;
+		}
+
+		public void setCouncil(String council) {
+			this.council = council;
+		}
+
+		public String getTroop() {
+			return troop;
+		}
+
+		public void setTroop(String troop) {
+			this.troop = troop;
+		}
+
+		public String getTroopAge() {
+			return troopAge;
+		}
+
+		public void setTroopAge(String troopAge) {
+			this.troopAge = troopAge;
+		}
+
+		public String getTroopName() {
+			return troopName;
+		}
+
+		public void setTroopName(String troopName) {
+			this.troopName = troopName;
+		}
+		
+		
+		
+}
