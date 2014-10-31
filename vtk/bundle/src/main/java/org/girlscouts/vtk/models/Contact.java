0a774ede81f1620305fe7f17b9ac4473ad3efcc4
alexyakobovich <ayakobovich@northps.com>
2014-10-31 15:49:00 -0400

    Automatic Commit 1

--- vtk/bundle/src/main/java/org/girlscouts/vtk/models/Contact.java
+++ vtk/bundle/src/main/java/org/girlscouts/vtk/models/Contact.java
@@ -7,6 +7,62 @@ public class Contact {
 
 		
 	
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getAddress1() {
+		return address1;
+	}
+
+	public void setAddress1(String address1) {
+		this.address1 = address1;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	public String getSuite() {
+		return suite;
+	}
+
+	public void setSuite(String suite) {
+		this.suite = suite;
+	}
+
 	public String getPhone() {
 		return phone;
 	}
