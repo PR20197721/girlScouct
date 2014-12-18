756404e88a62033e09ad2327d6446f0115bd48a3
Mike Zou <mzou@northps.com>
2014-12-18 12:10:59 -0500

    selectionchanged event caught. timezonedatetime found

--- web/app/src/main/content/jcr_root/apps/girlscouts/clientlibs/js/TimezoneSelection.js
+++ web/app/src/main/content/jcr_root/apps/girlscouts/clientlibs/js/TimezoneSelection.js
@@ -2,8 +2,12 @@ girlscouts.components.TimezoneSelection = CQ.Ext.extend(CQ.form.Selection, {
 	listeners: {
 		selectionchanged: {
 			scope: this,
-			fn: function(widget, value) {
-				console.info('###selectionchanged. newvalue=' + value);
+			fn: function(that, value) {
+				var datetimes = that.findParentByType('panel').findByType('timezonedatetime');
+				for (var i = 0; i < datetimes.length; i++) {
+					var datetime = datetimes[i];
+					console.info('###' + i + ' selectionchanged. newvalue=' + value);
+				}
 			}
 		}
 	}
