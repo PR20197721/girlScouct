b523027b7c883e03908d4684666bc34b833b908f
Mike Zou <mzou@northps.com>
2014-12-18 14:35:07 -0500

    Got timezone from council property

--- web/app/src/main/content/jcr_root/apps/girlscouts/clientlibs/js/TimezoneSelection.js
+++ web/app/src/main/content/jcr_root/apps/girlscouts/clientlibs/js/TimezoneSelection.js
@@ -1,4 +1,39 @@
 girlscouts.components.TimezoneSelection = CQ.Ext.extend(CQ.form.Selection, {
+	timezone: null,
+	
+    initComponent:function() {
+        // call parent initComponent
+        girlscouts.components.TimezoneSelection.superclass.initComponent.call(this);
+        
+        // Setup timezone 
+    	var url = window.location.pathname;
+    	var path = CQ.shared.HTTP.getPath(url);
+    	
+    	// New event from scaffolding page
+    	var regexScaffolding = /\/etc\/scaffolding\/[^/]+\/event/; //e.g. /etc/scaffolding/gsnetx/event
+    	if (regexScaffolding.test(path)) {
+    		var targetPathProperty = regexScaffolding.exec(path) + '/jcr:content/cq:targetPath';
+    		var response = CQ.shared.HTTP.get(targetPathProperty);
+    		if (response.status == 200) {
+    			path = response.body;
+    		}
+    	}
+    	
+    	var regex = /^\/content\/[^/]+\/[^/]+/; // e.g. /content/girlscouts-prototype/en
+    	if (regex.test(path)) {
+    		var timezoneProperty = regex.exec(path) + '/jcr:content/timezone';
+    		var response = CQ.shared.HTTP.get(timezoneProperty);
+    		if (response.status == 200) {
+    			this.timezone = response.body;
+    		} else {
+    			this.timezone = this.defaultTimezone;
+    		}
+    	} else {
+    		this.timezone = this.defaultTimezone;
+    	}
+    	alert('this.timezone=' +this.timezone);
+    },
+    
 	listeners: {
 		selectionchanged: {
 			scope: this,
