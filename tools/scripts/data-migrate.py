85c459542c5e34fdfb1430e80fbfcf5873dd90e9
Mike Zou <mzou@northps.com>
2015-01-08 12:45:55 -0500

    Data migrate update

--- tools/scripts/data-migrate.py
+++ tools/scripts/data-migrate.py
@@ -78,47 +78,71 @@ def switch_component(name, switch):
 
 def rcp(branch):
     "RCP data."
-    os.system("vlt rcp -b " + str(batch_size) + " " + exclude + " -r -u -n http://" + src_conf['username'] + ":" + src_conf['password'] + "@" + src_conf['url'] + "/crx/-/jcr:root" + branch + " http://" + dst_conf["username"] + ":" + dst_conf["password"] + "@localhost:" + port + "/crx/-/jcr:root" + branch);
+    os.system("vlt rcp -b " + str(batch_size) + " " + exclude + " -r -u -n http://" + src_conf['username'] + ":" + src_conf['password'] + "@" + src_conf['url'] + "/crx/-/jcr:root" + branch + " http://" + dst_conf["username"] + ":" + dst_conf["password"] + "@" + dst_conf["url"] + "/crx/-/jcr:root" + branch);
     print "### Migrated branch: " + branch
 
 ####################
 # Main
 ####################
 if len(sys.argv) < 2:
-    sys.exit('Please provide target env: local-auth local-pub dev-auth dev-pub stage-auth stage-pub');
+    sys.exit('Please provide target env: local dev stage');
 
-dst_env = sys.argv[1]
-runmode = dst_env.split('-')[1]
+# Fetch conf
+dst_env = sys.argv[1] + '-auth'
 dst_conf = all_conf[dst_env]
 src_env = dst_conf['source']
 src_conf = all_conf[src_env]
-port = '4502'
-if runmode == 'pub':
-    port = '4503'
 
-# Stop workflow launcher and listener to prevent DAM workflows
-if runmode == 'auth':
-    switch_component('com.adobe.granite.workflow.core.launcher.WorkflowLauncherImpl', False);
-    switch_component('com.adobe.granite.workflow.core.launcher.WorkflowLauncherListener', False);
+print '========================================='
+print 'Stop workflow launcher and listener to prevent DAM workflows'
+print '========================================='
+switch_component('com.adobe.granite.workflow.core.launcher.WorkflowLauncherImpl', False);
+switch_component('com.adobe.granite.workflow.core.launcher.WorkflowLauncherListener', False);
 
+print '========================================='
+print 'Migrate data from upstream auth local auth'
+print '========================================='
+dst_conf['url'] = 'localhost:4502'
 for branch in branches:
+    print '========================================='
+    print 'Migrating branch: ' + branch
+    print '========================================='
     rcp(branch)
 
-# Re-enable workflow launcher and listener
-if runmode == 'auth':
-    switch_component('com.adobe.granite.workflow.core.launcher.WorkflowLauncherImpl', True);
-    switch_component('com.adobe.granite.workflow.core.launcher.WorkflowLauncherListener', True);
+print '========================================='
+print 'Re-enable workflow launcher and listener'
+print '========================================='
+switch_component('com.adobe.granite.workflow.core.launcher.WorkflowLauncherImpl', True);
+switch_component('com.adobe.granite.workflow.core.launcher.WorkflowLauncherListener', True);
+
+# Reset conf
+dst_env = sys.argv[1] + '-pub'
+dst_conf = all_conf[dst_env]
+src_env = dst_conf['source']
+src_conf = all_conf[src_env]
+
+print '========================================='
+print 'Migrate data from auth to pub'
+print '========================================='
+src_conf['url'] = 'localhost:4502'
+for branch in branches:
+    print '========================================='
+    print 'Migrating branch: ' + branch
+    print '========================================='
+    rcp(branch)
 
 migrate_tool = 'java -jar ' + MIGRATE_TOOL_JAR + ' http://' + dst_conf['url'] + '/crx/server ' + dst_conf['username'] + ' ' + dst_conf['password'] + ' '
-if runmode == 'pub':
-    # Remove deactivated nodes
-    for branch in branches:
-        os.system(migrate_tool + 'removedeactivated ' + branch)     
+print '========================================='
+print 'Remove deactivated nodes'
+print '========================================='
+for branch in branches:
+    os.system(migrate_tool + 'removedeactivated ' + branch)     
 
-if dst_env.split('-')[0] == 'stage':
-    # Update VTK link 
-    for branch in branches:
-        os.system(migrate_tool + 'updatelink ' + branch)     
+print '========================================='
+print 'Update VTK link' 
+print '========================================='
+for branch in branches:
+    os.system(migrate_tool + 'updatelink ' + branch)     
 
 elapsed_time = time.time() - start_time
 print "All Done. Time elapsed: " + str(elapsed_time) + " seconds."
