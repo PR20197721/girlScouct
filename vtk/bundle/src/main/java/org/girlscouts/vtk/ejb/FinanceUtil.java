02e8f66053f319c094690a3db2082ec648c3c721
Igor <igor.kaplunov1@gmail.com>
2015-03-02 09:38:39 -0500

    Added email method needs testing and debugging also needs backend support

--- vtk/bundle/src/main/java/org/girlscouts/vtk/ejb/FinanceUtil.java
+++ vtk/bundle/src/main/java/org/girlscouts/vtk/ejb/FinanceUtil.java
@@ -1,9 +1,16 @@
 package org.girlscouts.vtk.ejb;
 
+import java.io.IOException;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
+import javax.mail.internet.InternetAddress;
+import javax.mail.util.ByteArrayDataSource;
+
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.HtmlEmail;
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.Service;
@@ -11,8 +18,10 @@ import org.girlscouts.vtk.dao.TroopDAO;
 import org.girlscouts.vtk.models.Finance;
 import org.girlscouts.vtk.models.FinanceConfiguration;
 import org.girlscouts.vtk.models.Troop;
-import org.girlscouts.vtk.models.User;
-import org.codehaus.jackson.map.ObjectMapper;
+
+import com.day.cq.mailer.MessageGateway;
+import com.day.cq.mailer.MessageGatewayService;
+import com.day.text.csv.Csv;
 
 @Component
 @Service(value = FinanceUtil.class)
@@ -20,6 +29,9 @@ public class FinanceUtil {
 
 	@Reference
 	TroopDAO troopDAO;
+	
+	@Reference
+	private MessageGatewayService messageGatewayService;
 
 	public Finance getFinances(Troop troop, int qtr, String currentYear) {
 		return troopDAO.getFinances(troop, qtr, currentYear);
@@ -47,6 +59,67 @@ public class FinanceUtil {
 		String period = params.get(Finance.PERIOD)[0];
 		troopDAO.setFinanceConfiguration(troop, currentYear, income, expenses, period);
 		// TODO NOTIFY Council here
+	}
+	
+	public void sendFinanceDataEmail(Troop troop, int qtr, String currentYear){
+		Finance finance = getFinances(troop, qtr, currentYear);
+		FinanceConfiguration financeConfig = getFinanceConfig(troop, currentYear);
+		
+		try {
+			MessageGateway<HtmlEmail> messageGateway = messageGatewayService.getGateway(HtmlEmail.class);
+			
+			StringWriter writer = new StringWriter();
+			Csv csvWriter = new Csv();
+			csvWriter.writeInit(writer);
+			
+			csvWriter.writeRow("Income", "", "Expenses", "");
+			
+			List<String> incomeFields = financeConfig.getIncomeFields();
+			List<String> expenseFields = financeConfig.getExpenseFields();
+			
+			for(int i = 0; i < incomeFields.size() || i < expenseFields.size(); i++){
+				String incomeField = "";
+				String incomeValue = "";
+				if(i < incomeFields.size()){
+					incomeField = incomeFields.get(i);
+					incomeValue = Double.toString(finance.getIncomeByName(incomeField));
+				}
+				String expenseField = "";
+				String expenseValue = "";
+				if(i < expenseFields.size()){
+					expenseField = incomeFields.get(i);
+					expenseValue = Double.toString(finance.getExpenseByName(expenseField));
+				}
+				csvWriter.writeRow(incomeField, incomeValue, expenseField, expenseValue);
+				
+			}
+			
+			csvWriter.close();
+			
+			writer.close();
+			String csvContents = writer.toString();
+			
+			
+			HtmlEmail email = new HtmlEmail();
+			ArrayList<InternetAddress> emailRecipients = new ArrayList<InternetAddress>();
+			
+			email.setTo(emailRecipients);
+			email.setSubject("Troop Finances");
+			
+			
+			email.attach(new ByteArrayDataSource(csvContents.getBytes(),"text/csv"), "finances.csv", "Finances Data");
+			
+			messageGateway.send(email);
+			
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (EmailException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
 
