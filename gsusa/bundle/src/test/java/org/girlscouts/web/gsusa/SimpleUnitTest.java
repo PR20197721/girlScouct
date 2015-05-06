ac000a286f12dc592df3008ab00b9691da752063
Mike Zou <mzou@northps.com>
2015-05-06 10:39:55 -0400

    GSUSA initial check in.

---
+++ gsusa/bundle/src/test/java/org/girlscouts/web/gsusa/SimpleUnitTest.java
@@ -0,0 +1,14 @@
+package org.girlscouts.web.gsusa;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleUnitTest {
+
+    @Test
+    public void someTest() {
+        assertTrue(true);
+    }
+
+}
