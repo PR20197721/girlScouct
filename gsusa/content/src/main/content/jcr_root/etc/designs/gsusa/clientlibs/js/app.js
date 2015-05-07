2a26b19dcdf5616a8c0953d53afc98e35ff158cf
Mike Zou <mzou@northps.com>
2015-05-07 10:06:42 -0400

    Add clientlibs

---
+++ gsusa/content/src/main/content/jcr_root/etc/designs/gsusa/clientlibs/js/app.js
@@ -0,0 +1,13 @@
+/*jslint browser: true*/
+/*global $, jQuery*/
+var $ = jQuery.noConflict();
+(function ($) {
+  'use strict';
+// Foundation JavaScript
+// Documentation can be found at: http://foundation.zurb.com/docs
+  $(document).foundation();
+  //document ready startes here.
+  $(document).ready(function () {
+    console.log();
+  });
+}($));
