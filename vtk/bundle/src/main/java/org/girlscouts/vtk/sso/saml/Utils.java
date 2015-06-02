19b0a71e07e50afad78902f49a020fef18c866f7
alexyakobovich <ayakobovich@northps.com>
2015-06-02 09:40:14 -0400

    pom+ localhost Forbidden

--- vtk/bundle/src/main/java/org/girlscouts/vtk/sso/saml/Utils.java
+++ vtk/bundle/src/main/java/org/girlscouts/vtk/sso/saml/Utils.java
@@ -144,7 +144,7 @@ public class Utils {
 					"Detected use of ENTITY in XML, disabled to prevent XXE/XEE attacks");
 		}
 
-		 factory = DocumentBuilderFactory.newInstance();
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 		factory.setNamespaceAware(true);
 		
 		// Add various options explicitly to prevent XXE attacks. add try/catch around every
