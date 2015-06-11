a8b4f2cb01db4120dfc9310c69410ff42db5cf9a
David Lubin <dlubin@northpointdigital.com>
2015-06-11 15:12:17 -0400

    Almost done, needs backend proxy

--- gsusa/content/src/main/content/jcr_root/etc/designs/gsusa/clientlibs/js/council-finder.js
+++ gsusa/content/src/main/content/jcr_root/etc/designs/gsusa/clientlibs/js/council-finder.js
@@ -6,30 +6,56 @@ $CQ(function() {
 });
 
 function searchZip( ev ){
-	var $zip = ev.data.zip;
+	var zip = ev.data.zip;
 	var baseURL = ev.data.url;
-	var $id = ev.data.div-id;
-	$CQ.getJSON(baseURL + '.zip.json', outputResult(data, $id));
+	var id = ev.data.div-id;
+	$CQ.getJSON(baseURL + '.zip.json', outputResult(data, id));
 }
 
 function searchState( ev ){
-	var $state = ev.data.state;
+	var state = ev.data.state;
 	var baseURL = ev.data.url;
-	var $id = ev.data.div-id;
-	$CQ.getJSON(baseURL + '.state.json', outputResult(data, $id));
+	var id = ev.data.div-id;
+	$CQ.getJSON(baseURL + '.state.json', outputResult(data, id));
 }
 
 function searchCouncilCode( ev ){
-	var $council = ev.data.council-code;
+	var council = ev.data.council-code;
 	var baseURL = ev.data.url;
-	var $id = ev.data.div-id;
-	$CQ.getJSON(baseURL + '.council.json', outputResult(data, $id));
+	var id = ev.data.div-id;
+	$CQ.getJSON(baseURL + '.council.json', outputResult(data, id));
 }
 
-function outputResult( data, $id ) {
-	var $html = [];
+function outputResult( data, id ) {
+	var html = [];
 	html.push("<p>Results:</p>");
-	$.each(data.resultsArray, function(i, value){
-		
+	$CQ.each(data.resultsArray, function(i, value){
+		html.push(
+		"<p><strong>", 
+		value.councils[i].councilFullName, 
+		"</strong></p><p>",
+		value.councils[i].city,
+		", ",
+		value.councils[i].state,
+		" ",
+		value.councils[i].zipcode,
+		"</p><p>",
+		value.councils[i].phone,
+		"</p><p>E-mail: ",
+		value.councils[i].email,
+		"</p><p>Web Site: ",
+		value.councils[i].website,
+		"</p><p>Facebook: ",
+		value.councils[i].facebook,
+		"</p><p>Twitter: ",
+		value.councils[i].twitter,
+		"</p><p><a class=\"finder-button\" href=\"",
+		value.councils[i].joinUrl,
+		"\">Join</a><a class=\"finder-button\" href=\"",
+		value.councils[i].volunteerUrl,
+		"\">Volunteer</a><a class=\"finder-button\" href=\"",
+		value.councils[i].onlineRegistrationUrl,
+		"\">Online Registration</a></p><br/>");
 	});
+	$CQ( id ).html(html.join(""));
 }
