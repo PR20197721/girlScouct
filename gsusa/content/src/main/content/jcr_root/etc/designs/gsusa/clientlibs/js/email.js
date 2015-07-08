0cdabd319ea97d260ee98e08fbaf55d2e18f5355
Anna Dulfan <adulfan@northpointdigital.com>
2015-07-08 12:33:10 -0600

    added the message to the footer and hidden the email form

--- gsusa/content/src/main/content/jcr_root/etc/designs/gsusa/clientlibs/js/email.js
+++ gsusa/content/src/main/content/jcr_root/etc/designs/gsusa/clientlibs/js/email.js
@@ -1,5 +1,5 @@
 $('.email-signin').submit(function (event) {
-    var me = event.target;
+  var me = event.target;
 	var emailAddress = $(me).find("[name='email']").val();
 	var corphomeValue = $(me).find("[name='CORPHOME']").val();
 
@@ -11,8 +11,9 @@ $('.email-signin').submit(function (event) {
 		if(txt.search(/OK/i) == -1) {
 			alert('Sorry, we are having a difficulty processing your request.\nPlease try again later.');
 		} else {
-			// show thank you 
+			// show thank you
 			$(me).find("[name='email']").val('');
+			$('.email-signin').hide();
 			$('.email-output-message').html("<p>Thank you! We look forward to staying connected</p>");
 		}
 	});
