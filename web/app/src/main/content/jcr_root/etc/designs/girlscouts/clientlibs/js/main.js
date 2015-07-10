641a7108b35f39b45a41b947b1639e47be702915
Mike Zou <mzou@northpointdigital.com>
2015-07-10 11:09:16 -0400

    Cherry-picking accordion from root into hotfix.

--- web/app/src/main/content/jcr_root/etc/designs/girlscouts/clientlibs/js/main.js
+++ web/app/src/main/content/jcr_root/etc/designs/girlscouts/clientlibs/js/main.js
@@ -3,3 +3,57 @@ $(document).foundation();
 var girlscouts = girlscouts || {};
 girlscouts.components = girlscouts.components || {};
 girlscouts.functions = girlscouts.functions || {};
+
+if (!Date.now) {
+	  Date.now = function now() {
+	    return new Date().getTime();
+	  };
+	}
+
+function toggleParsys(s)
+{
+    var componentPath = s;
+    
+    this.toggle = function()
+    {
+    	if (componentPath)
+        {
+    		var parsysComp = CQ.WCM.getEditable(componentPath);
+    		
+    		if(parsysComp.hidden == true){
+    			parsysComp.show();
+    		}
+    		else{
+    			parsysComp.hide();
+    		}
+        }
+    };
+
+    this.hideParsys = function()
+    {
+        if (componentPath)
+        {
+            var parsysComp = CQ.WCM.getEditable(componentPath);
+
+            if (parsysComp)
+            {
+                parsysComp.hide();
+            }
+        }
+    };
+
+    this.showParsys = function()
+    {
+        if (componentPath)
+        {
+            var parsysComp = CQ.WCM.getEditable(componentPath);
+
+            if (parsysComp)
+            {
+                parsysComp.show();
+            }
+        }
+    };
+
+    return this;
+};
