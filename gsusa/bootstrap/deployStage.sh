6639c167837703874c013295eaddbcb39e5a6db2
ikim <ikim@northps.com>
2015-07-21 17:28:38 -0400

    Added missing renditions in temp folder

--- gsusa/bootstrap/deployStage.sh
+++ gsusa/bootstrap/deployStage.sh
@@ -7,7 +7,7 @@ if [ -z $VERSION ]; then
     VERSION=`sed '10q;d' ../pom.xml | sed 's/.*<version>\(.*\)<\/version>/\1/'`
 fi
 
-SERVER_LIST=(54.85.69.30:4503 author-girlscouts-stage.adobecqms.net:80)
+SERVER_LIST=(54.85.69.30:4503 54.86.13.38:4502)
 
 for server in ${SERVER_LIST[@]}; do
 	echo "Trying server $server"
@@ -15,7 +15,7 @@ for server in ${SERVER_LIST[@]}; do
 	if [ $? -ne 0 ]; then
 		echo "Server $server is down. Skipping..."
 	else
-		if [ $server -eq 'author-girlscouts-stage.adobecqms.net:80' ]; then
+		if [ $server -eq '54.86.13.38:4502' ]; then
 			echo "Disabling Author Dam Update Asset and  Dam MetaData Writeback workflows"
 			curl -u admin:4U5Hsq5Q_I "http://$server/system/console/components/com.adobe.granite.workflow.core.launcher.WorkflowLauncherImpl/com.adobe.granite.workflow.core.launcher.WorkflowLauncherImpl" -H 'Accept: application/json, text/javascript, */*; q=0.01' -H 'Cache-Control: no-cache' --data 'action=disable'
 			curl -u admin:4U5Hsq5Q_I "http://$server/system/console/components/com.adobe.granite.workflow.core.launcher.WorkflowLauncherListener/com.adobe.granite.workflow.core.launcher.WorkflowLauncherListener" -H 'Accept: application/json, text/javascript, */*; q=0.01' -H 'Cache-Control: no-cache' --data 'action=disable'
@@ -24,7 +24,7 @@ for server in ${SERVER_LIST[@]}; do
 		echo "Deploying to http://$server"
 		curl -u "admin:4U5Hsq5Q_I" -F file=@"$HOME/.m2/repository/org/girlscouts/web/gsusa-bootstrap/$VERSION/gsusa-bootstrap-$VERSION.zip" -F name="gsusa-bootstrap" -F force=true -F install=true http://$server/crx/packmgr/service.jsp
 
-		if [ $server -eq 'author-girlscouts-stage.adobecqms.net:80' ]; then
+		if [ $server -eq '54.86.13.38:4502' ]; then
 			echo "Re-enabling Author Dam Update Asset and  Dam MetaData Writeback workflows"
 			curl -u admin:4U5Hsq5Q_I "http://$server/system/console/components/com.adobe.granite.workflow.core.launcher.WorkflowLauncherImpl/com.adobe.granite.workflow.core.launcher.WorkflowLauncherImpl" -H 'Accept: application/json, text/javascript, */*; q=0.01' -H 'Cache-Control: no-cache' --data 'action=enable'
 			curl -u admin:4U5Hsq5Q_I "http://$server/system/console/components/com.adobe.granite.workflow.core.launcher.WorkflowLauncherListener/com.adobe.granite.workflow.core.launcher.WorkflowLauncherListener" -H 'Accept: application/json, text/javascript, */*; q=0.01' -H 'Cache-Control: no-cache' --data 'action=enable'
