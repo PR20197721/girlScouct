4a3a5c555e3c20b17bdb5189ffd8e1eefd7c5a58
Mike Zou <mzou@northpointdigital.com>
2015-07-28 19:38:48 -0400

    Update filter to accept a enabled option.

--- vtk/bundle/src/main/java/org/girlscouts/vtk/impl/filters/MaintenanceFilter.java
+++ vtk/bundle/src/main/java/org/girlscouts/vtk/impl/filters/MaintenanceFilter.java
@@ -1,6 +1,7 @@
 package org.girlscouts.vtk.impl.filters;
 
 import java.io.IOException;
+import java.util.Dictionary;
 
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -9,32 +10,34 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.Cookie;
 
+import org.apache.felix.scr.annotations.Activate;
 import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Modified;
 import org.apache.felix.scr.annotations.Properties;
 import org.apache.felix.scr.annotations.Property;
 import org.apache.felix.scr.annotations.Service;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
+import org.osgi.service.component.ComponentContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@Component(enabled = false)
+@Component(
+    enabled = false, 
+    label = "Girl Scouts VTK Maintenance Filter",
+    description = "Filter that redirects the user to the maintenance page", 
+    metatype = true
+)
 @Service
 @Properties({ 
-    @Property(name = "sling.filter.scope", value = "REQUEST"),
-    @Property(name = "service.ranking", intValue = -1000) 
+    @Property(name = "sling.filter.scope", value = "REQUEST", propertyPrivate = true),
+    @Property(name = "service.ranking", intValue = -1000, propertyPrivate = true),
+    @Property(name = "enabled", boolValue = false, label = "Enabled")
 })
-/**
- * 
- * @author mike
- * This is the filter that reads the "additionalCss" properties of a node and
- * add CSS when the node is being included.
- *
- */
 public class MaintenanceFilter implements javax.servlet.Filter {
 
-    private static final Logger log = LoggerFactory
-            .getLogger(MaintenanceFilter.class);
+    private static final Logger log = LoggerFactory.getLogger(MaintenanceFilter.class);
+    private boolean enabled;
 
     public void destroy() {
         // Nothing to do
@@ -43,6 +46,10 @@ public class MaintenanceFilter implements javax.servlet.Filter {
     public void doFilter(ServletRequest request, ServletResponse response,
             FilterChain filterChain) throws IOException, ServletException {
         SlingHttpServletRequest req = (SlingHttpServletRequest)request;
+        if (!enabled) {
+            filterChain.doFilter(request, response);
+        }
+
         Cookie[] cookies = req.getCookies();
         if (cookies != null) {
             for (Cookie cookie : cookies) {
@@ -63,6 +70,13 @@ public class MaintenanceFilter implements javax.servlet.Filter {
     }
 
     public void init(FilterConfig config) throws ServletException {
-        // Nothing to do
     }
+    
+    @Activate
+	@Modified
+	private void updateConfig(ComponentContext context) {
+		@SuppressWarnings("rawtypes")
+		Dictionary configs = context.getProperties();
+		enabled = (Boolean)configs.get("enabled");
+	}
 }
