7faacb39f9d147779134441b5567f1bfcbbe8761
Mike Zou <mzou@northpointdigital.com>
2015-08-06 15:45:37 -0400

    Move state field logic.

--- gsusa/content/src/main/content/jcr_root/etc/designs/gsusa/clientlibs/js/cqformfix.js
+++ gsusa/content/src/main/content/jcr_root/etc/designs/gsusa/clientlibs/js/cqformfix.js
@@ -1,3 +1,32 @@
 // form fix for error message line spacing.
 // Please check error message of the permission-request form for its effect
 $('.form_error').prev().hide();
+
+//State field is mandatory is US is selected.
+$(document).ready(function() {
+    var countryField = $('select[name="Country"]');
+    var stateField = $('select[name="State"]');
+    var star = $(stateField.parent().parent().find('.form_leftcolmark')[0]);
+
+    function adjustStateField() {
+        var stateVal = stateField.val();
+        if (countryField.val() == 'US') {
+        	stateField.children().first().attr('value', '');
+            if (stateVal == undefined || stateVal == null || stateVal == 'N/A') {
+                stateField.val('');
+            }
+            star.show();
+        } else {
+        	stateField.children().first().attr('value', 'N/A');
+            if (stateVal == undefined || stateVal == null || stateVal == '') {
+                stateField.val('N/A');
+            }
+            star.hide();
+        }
+    }
+
+    countryField.on('change', function(){
+    	adjustStateField();
+    });
+    adjustStateField();
+});
