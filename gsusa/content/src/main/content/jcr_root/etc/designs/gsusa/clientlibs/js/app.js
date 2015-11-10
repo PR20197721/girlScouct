407b1cdab11a3dfe67dcc8dda67beedefb1af83d
Mike Zou <mzou@northpointdigital.com>
2015-11-10 13:57:02 -0500

    Release candidate targeting 11/17/2015
    Philip's tickets

--- gsusa/content/src/main/content/jcr_root/etc/designs/gsusa/clientlibs/js/app.js
+++ gsusa/content/src/main/content/jcr_root/etc/designs/gsusa/clientlibs/js/app.js
@@ -104,8 +104,9 @@
               && target.closest('.button.arrow').siblings('form').css('display') !== 'none') {
             $('.join.button.arrow').siblings('form').addClass('hide');
           }
-      if (target.closest('.standalone-donate').length === 0 && target.closest('a.button').siblings('form').css('display') !== 'none') {
-            $('a.button').siblings('form').addClass('hide');
+      if (target.closest('.standalone-donate').length === 0 && target.closest('a.button.form').siblings('form').css('display') !== 'none') {
+            $('a.button.form').siblings('form').addClass('hide');
+            $('a.button.form').removeClass('hide');
           }
     });
   }
@@ -625,11 +626,12 @@ if($.browser.msie && parseFloat($.browser.version)<10){
     equilize_our_stories();
   });
   // form on the Donate Tile.
-  $("#tag_tile_button_local, .standalone-donate a.button").on('click', function (e){
+  $("#tag_tile_button_local, .standalone-donate a.button.form").on('click', function (e){
     e.preventDefault();
     // $('.formDonate').addClass('hide');
     // if($('.formDonate').is(':hidden')) {
       $('.formDonate').toggleClass('hide');
+      $(this).toggleClass('hide');
       $('.formDonate input[type="text"]').focus();
     // }
   })
