9707f0459dfdaa18637517e9c96f5806560ed518
Mike Zou <mzou@northpointdigital.com>
2015-11-18 13:41:34 -0500

    Add Rich Text to meet the cookies dialog

---
+++ gsusa/content/src/main/content/jcr_root/apps/gsusa/clientlibs/js/meet-the-cookies-multifield-widget.js
@@ -0,0 +1,30 @@
+gsusa.components.MeetTheCookiesMultifield = CQ.Ext.extend(CQ.form.MultiField, {
+    constructor: function(config) {
+        config = config || {};
+        defaults = {};
+        config = CQ.Util.applyDefaults(config, defaults);
+        gsusa.components.MeetTheCookiesMultifield.superclass.constructor.call(this, config);
+    },
+
+    // overriding CQ.Ext.Component#initComponent
+    initComponent: function() {
+        gsusa.components.MeetTheCookiesMultifield.superclass.initComponent.call(this);
+
+        var dialog = this.findParentByType("dialog");
+        dialog.on('beforesubmit', function(){
+            this.items.each(function(item/*,index, length*/) {
+                if (item instanceof CQ.form.MultiField.Item) {
+                	var field = item.field;
+			    	
+                    var hiddenValue = field.title.getValue() + "|||" +
+                                field.image.getValue() + "|||" +
+                                field.description.getValue() + "|||" +
+                                field.buttonTitle.getValue() + "|||" +
+                                field.buttonLink.getValue();
+                    field.hiddenField.setValue(hiddenValue);
+                }
+            }, this);
+        }, this);
+    }
+});
+CQ.Ext.reg('meetthecookiesmultifield', gsusa.components.MeetTheCookiesMultifield);
