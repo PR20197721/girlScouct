526e2e6f1d526652d5d77af17a71862bf447d5ca
Mike Zou <mzou@northpointdigital.com>
2015-12-07 22:23:41 -0500

    Update prefetch script
    Add -t for putty

--- tools/scripts/prefetch-booth-results/prefetch.sh
+++ tools/scripts/prefetch-booth-results/prefetch.sh
@@ -1,9 +1,10 @@
+
 #!/bin/sh
 
 # UPDATE DISPATCHER IPS, separated by spaces
-OTHER_DISPATCHERS=(10.44.3.138)
+OTHER_DISPATCHERS=(10.44.3.92 10.44.3.231 10.44.3.154)
 # UPDATE SSH USERNAME
-USERNAME=cookieuser
+USERNAME=cse
 
 TMP_FILE=/home/$USERNAME/booth-result-cache.tar.gz
 BOOTH_RESULT_DIR=/mnt/var/www/html/content/gsusa/en/cookies
@@ -16,7 +17,7 @@ date
 curl -H "CQ-Action: Activate" -H "CQ-Handle: $BOOTH_RESULT_DIR" -H "Content-Length: 0" -H "Content-Type: application/octet-stream" http://localhost/dispatcher/invalidate.cache
 
 # prefetch all zip cache
-cat zipUrls.txt | xargs -n 1 -P $CONCURRENCY wget --spider --force-html
+cat /home/$USERNAME/zipUrls.txt | xargs -n 1 -P $CONCURRENCY wget --spider --force-html
 
 # tar the cached files up
 cd $BOOTH_RESULT_DIR
@@ -27,7 +28,7 @@ for dispatcher in "${OTHER_DISPATCHERS[@]}"; do
     echo "Copying cache to $dispatcher"
     scp $TMP_FILE $USERNAME@$dispatcher:$TMP_FILE
     # Add --touch so that the cache file is newer that .stat.
-    ssh $USERNAME@$dispatcher "tar -C $BOOTH_RESULT_DIR --touch -xvf $TMP_FILE"
+    ssh -t $USERNAME@$dispatcher "sudo tar -C $BOOTH_RESULT_DIR --touch -xvf $TMP_FILE"
 done
 
 echo "########## Prefetching finished for cookie booth pages."
