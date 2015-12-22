1c579e45632a41dd2000097c5feb678a079bd13e
Mike Zou <mzou@northpointdigital.com>
2015-12-22 16:48:08 -0500

    Move script

--- gsusa/content/src/main/content/jcr_root/etc/designs/gsusa/clientlibs/js/app.js
+++ gsusa/content/src/main/content/jcr_root/etc/designs/gsusa/clientlibs/js/app.js
@@ -772,3 +772,20 @@ function printObjectProperties(objectToInspect) {
   }
 }
 
+$(document).ready(function(){
+	// Setup "contact local council" form
+	$('.booth-finder form#contactlocalcouncil').submit(function(){
+		$.post($(this).attr('action'), $(this).serialize(), function(response) {
+			// Remove blank lines
+			response = response.replace(/^\s*\n/gm, '').trim();
+			if (response.toUpperCase() == 'OK') {
+				$('#contactlocalcouncil').html('Thank you. A representative will contact you shortly.');
+			} else {
+				$('#contactlocalcouncil div.error').html(response);
+			}
+		});
+
+		// Prevent default
+		return false;
+	});
+});
