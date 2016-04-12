7083fb61e555429d6f723e507bbe80189945d578
David Lubin <dlubin@northpointdigital.com>
2016-04-12 17:12:46 -0400

    The last event search fix only works for some councils. Rolling back to the master version

--- web/bundle/src/main/java/org/girlscouts/web/events/search/EventsSrch.java
+++ web/bundle/src/main/java/org/girlscouts/web/events/search/EventsSrch.java
@@ -15,7 +15,6 @@ import java.util.Set;
 
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
-import javax.jcr.Session;
 
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.girlscouts.web.events.search.impl.FacetBuilderImpl;
@@ -23,11 +22,8 @@ import org.girlscouts.web.search.utils.SearchUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.day.cq.search.PredicateGroup;
-import com.day.cq.search.Query;
 import com.day.cq.search.QueryBuilder;
 import com.day.cq.search.result.Hit;
-import com.day.cq.search.result.SearchResult;
 
 public class EventsSrch  
 {
@@ -56,11 +52,9 @@ public class EventsSrch
 	public void search(String q,String[] tags,String offset, String month,String year, String startdtRange, String enddtRange, String region,String path,String facetsPath )
 	{
 		try {
-System.err.println("tata evnt search 1 :"+ new java.util.Date() );			
 			createFacets(facetsPath);
-System.err.println("tata evnt search 2 :"+ new java.util.Date() );
 			eventResults(q,offset,month,year,startdtRange,enddtRange,region,tags,path);
-System.err.println("tata evnt search 3 :"+ new java.util.Date() );
+			
 			// Don't delete this code if in future client need count for the tagging just enable this functionality.
 			// searchResultsInfo = SearchUtils.combineSearchTagsCounts(searchResultsInfo,facetAndTags);
 			
@@ -86,8 +80,8 @@ System.err.println("tata evnt search 3 :"+ new java.util.Date() );
 	
 	
 	private void eventResults(String q,String offset,String month,String year, String startdtRange, String enddtRange,String region,String[] tags, String path) throws RepositoryException{
-Session session= slingRequest.getResourceResolver().adaptTo(Session.class);
-System.err.println("tata evnt search 6 :"+ new java.util.Date() );
+
+		
 		this.searchResultsInfo = new SearchResultsInfo();
 		
 		// Generate Regions
@@ -96,42 +90,23 @@ System.err.println("tata evnt search 6 :"+ new java.util.Date() );
 		if(q!=null && !q.isEmpty()){
 			log.info("Search Query Term [" +q +"]");
 			searchQuery.put("fulltext",q);
-			searchQuery.put("p.limit", "2000");
-			searchQuery.put("type", "cq:Page");
-			searchQuery.put("p.offset", "0");
-			
-		}else{
-			searchQuery.put("p.limit", "200");
-			searchQuery.put("p.offset", "0");
-			//searchQuery.put("type", "cq:Page");
-			if( tags ==null ||  tags.length <= 0 ){
-				searchQuery.put("2_property", "@jcr:content/data/start");
-				searchQuery.put("2_property.operation", "exists");
-			}
-			
 		}
-System.err.println("tata ** evnt search 7 :"+ new java.util.Date() );
-			
-		
+		searchQuery.put("type", "cq:Page");
 		searchQuery.put("path",path);
-System.err.println("tata eventSearch: "+ path +" : "+q);		
-
-//--searchQuery.put("p.limit", "-1");
-
-//searchQuery.put("orderby","@jcr:content/data/start");
-searchQuery.put("orderby","@start");
-searchQuery.put("orderby.sort", "asc");
+		searchQuery.put("1_property",EVENTS_PROP);
+		searchQuery.put("p.limit", "-1");
+		searchQuery.put("orderby","@jcr:content/data/start");
+		searchQuery.put("orderby.sort", "asc");
 		log.debug("Query Parameter : " +q);
-System.err.println("tata evnt search 8 :"+ new java.util.Date() );
+		
 		if(tags.length > 0) {
 			try{
-				searchQuery.put("1_property",EVENTS_PROP);
 				addToDefaultQuery(searchQuery,tags);
 			}catch(Exception e){
 				log.error("Tagging could be parsed correctly");
 			}
 		}
-System.err.println("tata evnt search 9 :"+ new java.util.Date() );
+		
 		if((startdtRange!=null  && !startdtRange.isEmpty()) && (enddtRange!=null && !enddtRange.isEmpty())){
 			addDateRangesToQuery(startdtRange, enddtRange,searchQuery);
 		}
@@ -139,36 +114,17 @@ System.err.println("tata evnt search 9 :"+ new java.util.Date() );
 		if(region!=null && !region.isEmpty() && !region.equals("choose")){
 			addRegionToQuery(region);
 		}
-System.err.println("tata evnt search 10 :"+ new java.util.Date() );
+		
 		List<String> relts = new ArrayList<String>(); 
-		
-		
-		//List<Hit>hits = SearchUtils.performContentSearch(searchQuery,slingRequest,this.queryBuilder);
-		
-		PredicateGroup predicateGroup = PredicateGroup.create(searchQuery);
-		Query query =this.queryBuilder.createQuery(predicateGroup, session);
-		query.setExcerpt(true);
-System.err.println("tata "+predicateGroup.toString());
-		SearchResult searchResults=query.getResult();
-		java.util.List<Hit> hits = searchResults.getHits();
-		
-		
+		List<Hit>hits = SearchUtils.performContentSearch(searchQuery,slingRequest,this.queryBuilder);
 		if(hits!=null && !hits.isEmpty()){
-System.err.println("tata size: "+ hits.size() );			
 			for(int i=0;i<hits.size();i++){
-				
 				Hit ht = hits.get(i);
-				String pth = ht.getNode().isNodeType("cq:Page") ? ht.getPath() : ht.getPath();
-System.err.println("*** tata*** "+ pth);				
-if( pth.trim().endsWith("/jcr:content/data") )
-	pth= pth.replace("/jcr:content/data", "");
-System.err.println("tata -- "+ pth);	
+				String pth = ht.getNode().isNodeType("cq:Page")?ht.getPath():ht.getPath();
 				relts.add(pth);
 			}
 		}		
-System.err.println("tata evnt search 11 :"+ new java.util.Date() );	
 		searchResultsInfo.setResults(relts);
-System.err.println("tata evnt search 12 :"+ new java.util.Date() );
 	}
 	
 	public SearchResultsInfo getSearchResultsInfo(){
@@ -257,9 +213,6 @@ System.err.println("tata evnt search 12 :"+ new java.util.Date() );
 	private List<String> eventRegions(String path) {
 		Map<String, String> region = new LinkedHashMap<String, String>();
 		
-//if( true )return new ArrayList<String>();
-		
-System.err.println("tata evnt search 6_1 :"+ new java.util.Date() );
 		// As compare after() depends on the today which keeps on changing as it has a time format in it
 		// So we need to convert it into the date format yyyy-MM-dd so event and today return the same 
 		Date today = new Date();
@@ -272,17 +225,13 @@ System.err.println("tata evnt search 6_1 :"+ new java.util.Date() );
 		}
 		region.put("type", "cq:Page");
 		region.put("path",path);
-System.err.println("tata evnt search 6_1_1 :"+ new java.util.Date() +" : "+ path );	
 		region.put("p.limit", "-1");
-		
 		Set<String> regions = new HashSet<String>();
 		long startTime = System.nanoTime();
 		Date eventDate = null;
 		String eventDt = "";
-System.err.println("tata evnt search 6_2 :"+ new java.util.Date() );
 		try {
 			List<Hit> hits = SearchUtils.performContentSearch(region,slingRequest,this.queryBuilder);
-System.err.println("tata evnt search 6_3 :"+ new java.util.Date() );
 			for(Hit hts : hits) {
 				eventDate = null;
 				eventDt = "";
@@ -310,7 +259,7 @@ System.err.println("tata evnt search 6_3 :"+ new java.util.Date() );
 								regions.add(propNode.getProperty("region").getString());
 							}
 						}
-System.err.println("tata evnt search 6_4 :"+ new java.util.Date() );
+						
 					}catch(Exception e) {
 						log.error("Event Node doesn't contains required jcr:content/data Node" +e.getMessage());
 					}
@@ -321,7 +270,6 @@ System.err.println("tata evnt search 6_4 :"+ new java.util.Date() );
 		}
 		List<String> list = new ArrayList<String>(regions);
 		Collections.sort(list);
-System.err.println("tata evnt search 6_5 :"+ new java.util.Date() );
 		long endTime = System.nanoTime();
 		log.debug("-------------Time Take in Milliseconds --------------" +(endTime - startTime)/1000000);
 		return list;
