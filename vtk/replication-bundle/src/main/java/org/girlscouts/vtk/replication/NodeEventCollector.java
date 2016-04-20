2187a11b7e6fae0b6c7fc85125c589880222f8bb
Mike Zou <mzou@northpointdigital.com>
2016-04-19 22:21:04 -0400

    Revert "Add path info to replication warning msg"
    This reverts commit 751c89949dada4012eac37a933e76bfa02d62dc7.

--- vtk/replication-bundle/src/main/java/org/girlscouts/vtk/replication/NodeEventCollector.java
+++ vtk/replication-bundle/src/main/java/org/girlscouts/vtk/replication/NodeEventCollector.java
@@ -55,14 +55,13 @@ public class NodeEventCollector {
             Event rawEvent = iter.nextEvent();
             try {
             	if (!(rawEvent instanceof JackrabbitEvent)) {
-            		log.warn("This is not a JackrabbitEvent. Discard. Path: " + rawEvent.getPath());
+            		log.warn("This is not a JackrabbitEvent. Discard.");
             		continue;
             	}
             	JackrabbitEvent event = (JackrabbitEvent)rawEvent;
             	
             	if (event.isExternal()) {
-            		log.warn("This is an external event. Discard. Path: " + event.getPath());
-            		log.warn("Even though this is an external event, what is the username? " + event.getUserID());
+            		log.warn("This is an external event. Discard.");
             		continue;
             	}
             	
@@ -70,7 +69,7 @@ public class NodeEventCollector {
             	// This will prevent receiver-generated events, 
             	// which is created by admin, from being replicated.
             	if (!"vtk".equals(event.getUserID())) {
-            		log.info("This is an event not created by vtk user. Discard. Path: " + event.getPath());
+            		log.info("This is an event not created by vtk user. Discard.");
             		continue;
             	}
 
