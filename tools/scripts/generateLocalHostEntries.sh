127941959cb234176d44127cb0c755d44fd44961
ikim <inkoo@yahoo.com>
2016-04-21 17:40:31 -0400

    Fixed dev/stage/uat my61 hostnames

--- tools/scripts/generateLocalHostEntries.sh
+++ tools/scripts/generateLocalHostEntries.sh
@@ -4,7 +4,7 @@
 
 #gsenv=( 'my-dev61:girlscout-elb1usea-1AVJ6LQH3AXDY-1374442522.us-east-1.elb.amazonaws.com' 'my-stage61:girlscout-elb1usea-1MA06QDIVAQQY-84840997.us-east-1.elb.amazonaws.com' 'my-uat61:girlscout-elb1usea-1JJZWGJR1W4PM-1508890334.us-east-1.elb.amazonaws.com' )
 
-gsenv=( 'my-dev61:author-girlscouts-dev-aem61a.adobecqms.net' 'my-stage61:author-girlscouts-stage-aem61.adobecqms.net' 'my-uat61:author-girlscouts-uat-aem61.adobecqms.net' )
+gsenv=( 'my61-dev:author-girlscouts-dev-aem61a.adobecqms.net' 'my61-stage:author-girlscouts-stage-aem61.adobecqms.net' 'my61-uat:author-girlscouts-uat-aem61.adobecqms.net' )
 
 for i in "${!gsenv[@]}"; do
 	thisEnv=`echo ${gsenv[$i]} | sed -e 's/^\([a-z0-9-]*\):\(.*\)/\1/'`
