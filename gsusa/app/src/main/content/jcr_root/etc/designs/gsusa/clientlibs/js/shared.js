49917af7f42d1a411eee256a97232ebb76579fe3
qheolet <qheolet@gmail.com>
2016-04-22 14:18:31 -0400

    Refact The JS

--- gsusa/app/src/main/content/jcr_root/etc/designs/gsusa/clientlibs/js/shared.js
+++ gsusa/app/src/main/content/jcr_root/etc/designs/gsusa/clientlibs/js/shared.js
@@ -1,51 +1,41 @@
 //Shared with council components
 if (!Date.now) {
-      Date.now = function now() {
+    Date.now = function now() {
         return new Date().getTime();
-      };
-    }
+    };
+}
 
 
-function toggleParsys(s)
-{
+function toggleParsys(s) {
     var componentPath = s;
 
-    this.toggle = function()
-    {
-        if (componentPath)
-        {
+    this.toggle = function() {
+        if (componentPath) {
             var parsysComp = CQ.WCM.getEditable(componentPath);
 
-            if(parsysComp.hidden == true){
+            if (parsysComp.hidden == true) {
                 parsysComp.show();
-            }
-            else{
+            } else {
                 parsysComp.hide();
             }
         }
     };
 
-    this.hideParsys = function()
-    {
-        if (componentPath)
-        {
+    this.hideParsys = function() {
+        if (componentPath) {
             var parsysComp = CQ.WCM.getEditable(componentPath);
 
-            if (parsysComp)
-            {
+            if (parsysComp) {
                 parsysComp.hide();
             }
         }
     };
 
-    this.showParsys = function()
-    {
-        if (componentPath)
-        {
+    this.showParsys = function() {
+        if (componentPath) {
             var parsysComp = CQ.WCM.getEditable(componentPath);
 
-            if (parsysComp)
-            {
+            if (parsysComp) {
                 parsysComp.show();
             }
         }
@@ -55,29 +45,25 @@ function toggleParsys(s)
 };
 
 function anchorCheck() {
-    $('.accordion dt > :first-child').each(function(i, value) {
-        var parsysID = $(value).parent().data('target');
-        var target = $(this).parent().next().find('.content');
-        var toggle = $(this);
-        var parsysID = $(this).parent().data('target');
-        var anchor = $(this).parent().attr('id');
-        if(anchor != "" && window.location.hash.replace("#","") == anchor){
-            toggle.addClass('on');
-            target.slideDown();
-            $(this).parent().addClass('on');
-        }
-    });
+   var hash = window.location.hash;
+   if(hash!=="" && $(hash).length > 0){
+    setTimeout(function() {
+        $(hash).children('h6').trigger('click');
+    }, 200);
+    
+   
+   }
 }
 
-function inPageAnchorCheck(e){
-    $('.cookie-page .accordion dt > :first-child').each(function(i, value){
+function inPageAnchorCheck(e) {
+    $('.cookie-page .accordion dt > :first-child').each(function(i, value) {
         var target = $(this).parent().next().find('.content');
         var toggle = $(this);
         var parsysID = $(this).parent().data('target');
         toggle.removeClass('on');
         target.slideUp();
         $(this).parent().removeClass('on');
-        if(window[parsysID] != null && window[parsysID].hideParsys != undefined){
+        if (window[parsysID] != null && window[parsysID].hideParsys != undefined) {
             window[parsysID].hideParsys();
         }
     });
@@ -87,10 +73,11 @@ function inPageAnchorCheck(e){
         var toggle = $(this);
         var parsysID = $(this).parent().data('target');
         var anchor = $(this).parent().attr('id');
-        if(anchor != "" && e.target.href.substring(e.target.href.indexOf("#")).replace("#","") == anchor){
-            toggle.addClass('on');
-            target.slideDown();
-            $(this).parent().addClass('on');
+        if (anchor != "" && e.target.href.substring(e.target.href.indexOf("#")).replace("#", "") == anchor) {
+            // toggle.addClass('on');
+            // target.slideDown();
+            // $(this).parent().addClass('on');
+            $("#" + anchor).trigger('click');
         }
     });
 }
@@ -147,39 +134,66 @@ function inPageAnchorCheck(e){
 //  vtk_accordion():void
 //
 
+var CHECK;
 
-function vtk_accordion() {
-  $('.accordion dt > :first-child').on('click', function(e) {
-    e.stopPropagation();
-
-    //Catch Elements
-    var target = $(this).parent().data('target');
-    var toggle = $(this);
-
-    //Reset State
+function closeAllAccordion() {
     $('.accordion-navigation > .content').slideUp('slow');
     $('[data-target]').removeClass('on');
     $('[data-target]').children('h6').removeClass('on');
-
-        
-     //Open/close  the Accordion 
-    $('#' + target).slideToggle('slow');
-    
-
-    //Add the Class to give some formatt to the accordion
-    $(toggle).parents('dt').toggleClass('on');
-    $(toggle).toggleClass('on');
-
-
-    //For Web Component. See main.js:toggleParsys
-     if(window[ target ] != null){
-       window[ target ].toggle();
-     }
-      return false;
-  });
 }
 
-$(document).ready(function(){
-  vtk_accordion(); 
-  $('.accordion a').click(inPageAnchorCheck);
+function toggleAccordion(target, that) {
+    if (CHECK !== that) {
+
+        $('#' + target).slideToggle('slow');
+        //Add the Class to give some formatt to the accordion
+        $(that).parents('dt').toggleClass('on');
+        $(that).toggleClass('on');
+    }
+
+
+    CHECK = that;
+}
+
+function vtk_accordion() {
+    $('.accordion dt > :first-child').on('click', function(e) {
+        e.stopPropagation();
+
+        //Catch Elements
+        var target = $(this).parent().data('target');
+        var toggle = $(this);
+
+        //Reset State
+        closeAllAccordion();
+
+        //Open/close  the Accordion 
+        toggleAccordion(target, this);
+
+        //For Web Component. See main.js:toggleParsys
+        if (window[target].hasOwnProperty('toggle')) {
+            window[target].toggle();
+        }
+
+    });
+
+    anchorCheck();
+}
+
+
+
+$(document).ready(function() {
+    console.log('mamam mia')
+    vtk_accordion();
+    $('.accordion a').click(inPageAnchorCheck);
+
+
+    //Check if the hash change without reload the page
+
+    $(window).on('hashchange', function() {
+      anchorCheck();
+    });
+
 });
+
+
+
