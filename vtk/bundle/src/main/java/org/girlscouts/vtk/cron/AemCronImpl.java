f1893acf6ff13c7f7d9ca96108fe9130428a848c
alexyakobovich <ayakobovich@northps.com>
2016-05-26 13:56:34 -0400

    run demo cront at 4:54:04 am

--- vtk/bundle/src/main/java/org/girlscouts/vtk/cron/AemCronImpl.java
+++ vtk/bundle/src/main/java/org/girlscouts/vtk/cron/AemCronImpl.java
@@ -23,7 +23,7 @@ description = "rm demo temp nodes from db"
 @Properties({
 @Property(name = "service.description", value = "Girl Scouts DEM Demo Service",propertyPrivate=true),
 @Property(name = "service.vendor", value = "Girl Scouts", propertyPrivate=true), 
-@Property( name = "scheduler.expression", label="scheduler.expression", value = "* 4 * * * ?",description="cron expression"),
+@Property( name = "scheduler.expression", label="scheduler.expression", value = "4 54 4 * *  ?",description="cron expression"),
 
 @Property(name = "scheduler.concurrent", boolValue=false, propertyPrivate=true),
 @Property(name="scheduler.runOn", value="SINGLE",propertyPrivate=true)
