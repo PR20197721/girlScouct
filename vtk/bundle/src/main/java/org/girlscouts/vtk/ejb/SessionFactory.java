61d27ee52fe21720fa9ab64b5910eb850a787764
alexyakobovich <ayakobovich@northps.com>
2016-07-12 09:33:15 -0400

    vtk2

--- vtk/bundle/src/main/java/org/girlscouts/vtk/ejb/SessionFactory.java
+++ vtk/bundle/src/main/java/org/girlscouts/vtk/ejb/SessionFactory.java
@@ -1,9 +1,13 @@
 package org.girlscouts.vtk.ejb;
 
+import java.sql.SQLException;
+
 import javax.jcr.LoginException;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
 import javax.jcr.SimpleCredentials;
+import javax.sql.DataSource;
+
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.Service;
@@ -20,12 +24,29 @@ public class SessionFactory {
 	@Reference
 	private SlingRepository repository;
 
+	 @Reference(target = "(&(objectclass=javax.sql.DataSource)(datasource.name=alex))")
+	 private DataSource dataSource;
+	 
+	 public java.sql.Connection getConnection() throws SQLException{
+		 return dataSource.getConnection();
+	 }
 	public Session getSession() throws RepositoryException, LoginException {
+	
+		try{
+			System.err.println( "test Connection: " );
+			System.err.println( "test: "+ getConnection() );
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+			
 		Session adminSession = repository.loginAdministrative(null);
 		Session session = adminSession.impersonate(new SimpleCredentials("admin","admin".toCharArray()));
 		adminSession.logout();
 		adminSession = null;
 		return session;
+		
+		
+		
 	}
 
 	public void closeSession(Session session) {
