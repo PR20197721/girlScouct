e5da534d96e7b95bdcaa9237c98039a60bbbba35
clickpunch <phil@clickpunch.com>
2016-07-12 11:44:57 -0400

    GSDO-511: missing fixes for top nav

--- vtk/deployPC.sh
+++ vtk/deployPC.sh
@@ -4,16 +4,17 @@ VERSION=$1
 
 # Get the current version if version number is not specified
 if [ -z $VERSION ]; then
-    VERSION=`sed '10q;d' pom.xml | sed 's/.*<version>\(.*\)<\/version>/\1/'`
+    VERSION=`head -1 ../VERSIONS.txt | cut -d ' ' -f 1`
 fi
 
 SERVER_LIST=(localhost)
-PORT_LIST=(4502 4503 4505 4506)
+PORT_LIST=(4502 4503 4504 4505 4506)
 
 for server in ${SERVER_LIST[@]}; do
 	echo "Trying server $server"
 	for port in ${PORT_LIST[@]}; do
 		echo "Trying server $server:$port..."
+#		/usr/bin/nc -z $server $port
 		C:\\ZIP\\Utility\\nc.exe -z $server $port
 		if [ $? -ne 0 ]; then
 			echo "Server $server:$port is down. Skipping..."
