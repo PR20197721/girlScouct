8c61683a28bec27fcacdd08107892c1bebb7184e
David Lubin <dlubin@northpointdigital.com>
2016-08-11 11:44:17 -0400

    Revert "GSWP-707 - increase StyleFilter servlet ranking to 300 so that it comes before WCMComponentFilter"
    This reverts commit 4d472a7cef7dd9e5d841eeddc5ddea5ca1b63b2b.

--- web/bundle/src/main/java/org/girlscouts/web/filters/StyleFilter.java
+++ web/bundle/src/main/java/org/girlscouts/web/filters/StyleFilter.java
@@ -25,7 +25,7 @@ import com.day.cq.wcm.api.components.IncludeOptions;
 @Component
 @Service
 @Properties({ @Property(name = "sling.filter.scope", value = "COMPONENT"),
-        @Property(name = "service.ranking", intValue = 300) })
+        @Property(name = "service.ranking", intValue = 100) })
 /**
  * 
  * @author mike
