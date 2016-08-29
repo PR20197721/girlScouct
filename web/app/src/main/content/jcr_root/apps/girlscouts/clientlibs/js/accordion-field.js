bd4e5f72d56639a7e407229a4d82166734c5731b
David Lubin <dlubin@northpointdigital.com>
2016-08-29 17:56:43 -0400

    GSWP-717 - Identifiers functional, New Identifiers are randomly generated but can be changed, old accordions will still work, but are still prone to 'downshift' if an accordion above is deleted. This is only avoided by using unique identifiers.

--- web/app/src/main/content/jcr_root/apps/girlscouts/clientlibs/js/accordion-field.js
+++ web/app/src/main/content/jcr_root/apps/girlscouts/clientlibs/js/accordion-field.js
@@ -20,6 +20,7 @@
  */
 
 deleteAccordionFlag = false;
+accIndex = 0;
 
 girlscouts.functions.deleteaccordion=function(field, event){
 	if(deleteAccordionFlag == false){
@@ -32,175 +33,6 @@ girlscouts.functions.deleteaccordion=function(field, event){
 			});
 	}
 };
-
-//fix for loadcontent listener
-girlscouts.components.AccordionPanel = CQ.Ext.extend(CQ.Ext.Panel,{
-    panelValue: '',
-
-    constructor: function(config){
-        config = config || {};
-        girlscouts.components.AccordionPanel.superclass.constructor.call(this, config);
-    },
-
-    initComponent: function () {
-    	girlscouts.components.AccordionPanel.superclass.initComponent.call(this);
-
-        this.panelValue = new CQ.Ext.form.Hidden({
-            name: this.name
-        });
-
-        this.add(this.panelValue);
-
-        var multifield = this.findParentByType('multifield'),
-            dialog = this.findParentByType('dialog');
-
-        dialog.on('beforesubmit', function(){
-            var value = this.getValue();
-
-            if(value){
-                this.panelValue.setValue(value);
-            }
-        },this);
-
-        dialog.on('loadcontent', function(){
-            if(_.isEmpty(multifield.dropTargets)){
-                multifield.dropTargets = [];
-            }
-
-            multifield.dropTargets = multifield.dropTargets.concat(this.getDropTargets());
-
-            _.each(multifield.dropTargets, function(target){
-                if (!target.highlight) {
-                    return;
-                }
-
-                var dialogZIndex = parseInt(dialog.el.getStyle("z-index"), 10);
-
-                if (!isNaN(dialogZIndex)) {
-                    target.highlight.zIndex = dialogZIndex + 1;
-                }
-            })
-        }, this);
-
-        if(dialog.gsInit){
-            return;
-        }
-
-        var tabPanel = multifield.findParentByType("tabpanel");
-
-        if(tabPanel){
-            tabPanel.on("tabchange", function(panel){
-                panel.doLayout();
-            });
-        }
-
-        dialog.on('hide', function(){
-            var editable = CQ.utils.WCM.getEditables()[this.path];
-
-            //dialog caching is a real pain when there are multifield items; donot cache
-            delete editable.dialogs[CQ.wcm.EditBase.EDIT];
-            delete CQ.WCM.getDialogs()["editdialog-" + this.path];
-        }, dialog);
-
-        dialog.gsInit = true;
-    },
-
-    afterRender : function(){
-        girlscouts.components.AccordionPanel.superclass.afterRender.call(this);
-
-        this.items.each(function(){
-            if(!this.contentBasedOptionsURL
-                || this.contentBasedOptionsURL.indexOf(CQ.form.Selection.PATH_PLACEHOLDER) < 0){
-                return;
-            }
-
-            this.processPath(this.findParentByType('dialog').path);
-        })
-    },
-
-    getValue: function () {
-        var pData = "";
-
-        this.items.each(function(i){
-            if(i.xtype == "label" || i.xtype == "hidden" || !i.hasOwnProperty("dName")){
-                return;
-            }
-
-            if(pData == ""){
-            	pData = i.getValue();
-            }else{
-            	pData = pData + "," + i.getValue();
-            }
-        });
-
-        return pData;
-    },
-
-    setValue: function (value) {
-        this.panelValue.setValue(value);
-
-        var pData = value.split(',');
-
-        var index = 0;
-        this.items.each(function(i){
-            if(i.xtype == "label" || i.xtype == "hidden" || !i.hasOwnProperty("dName")){
-                return;
-            }
-
-            i.setValue(pData[index]);
-            index++;
-            
-            i.fireEvent('loadcontent', this);
-        });
-    },
-
-    getDropTargets : function() {
-        var targets = [], t;
-
-        this.items.each(function(){
-            if(!this.getDropTargets){
-                return;
-            }
-
-            t = this.getDropTargets();
-
-            if(_.isEmpty(t)){
-                return;
-            }
-
-            targets = targets.concat(t);
-        });
-
-        return targets;
-    },
-
-    validate: function(){
-        var valid = true;
-
-        this.items.each(function(i){
-            if(!i.hasOwnProperty("dName")){
-                return;
-            }
-
-            if(!i.isVisible()){
-                i.allowBlank = true;
-                return;
-            }
-
-            if(!i.validate()){
-                valid = false;
-            }
-        });
-
-        return valid;
-    },
-
-    getName: function(){
-        return this.name;
-    }
-});
-
-CQ.Ext.reg("accordionpanel", girlscouts.components.AccordionPanel);
 
 girlscouts.components.AccordionWidget = CQ.Ext.extend(CQ.form.CompositeField, {
 	
@@ -254,9 +86,9 @@ girlscouts.components.AccordionWidget = CQ.Ext.extend(CQ.form.CompositeField, {
         this.add(new CQ.Ext.form.Label({text: "Identifier"}));
         
         this.idField = new CQ.Ext.form.TextField({
-        	allowBlank: "{Boolean}false",
+        	allowBlank: false,
             listeners: {
-            	loadcontent: {
+            	added: {
             		scope:this,
             		fn:this.createId
             	},
@@ -277,6 +109,13 @@ girlscouts.components.AccordionWidget = CQ.Ext.extend(CQ.form.CompositeField, {
         this.nameField.setValue(parts[0]);
         this.anchorField.setValue(parts[1]);
         this.idField.setValue(parts[2]);
+        
+	    if(this.idField.value == null || this.idField.value == ""){
+	    	this.idField.setValue(accIndex);
+	    	this.idField.defaultValue = accIndex;
+	    }
+	    
+	    accIndex++;
         this.hiddenField.setValue(value);
     },
 
@@ -286,7 +125,7 @@ girlscouts.components.AccordionWidget = CQ.Ext.extend(CQ.form.CompositeField, {
     },
 
     // overriding CQ.form.CompositeField#getRawValue
-    getRawValue: function() {
+    getRawValue: function() {   	
         return this.nameField.getValue() + "|||" 
         	+ this.anchorField.getValue() + "|||"
         	+ this.idField.getValue();
@@ -297,20 +136,17 @@ girlscouts.components.AccordionWidget = CQ.Ext.extend(CQ.form.CompositeField, {
         this.hiddenField.setValue(this.getValue());
     },
     
-    //private
-    createID: function() {
-    	    if(this.idField.value == null){
-    	    	this.idField.setValue(this.idField.id);
-    	    }
-    }
+    createId: function() {
+	    var text = "";
+	    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+	    for( var i=0; i < 8; i++ )
+	        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+	    this.idField.setValue(text);
+	}
 
 });
 
 // register xtype
 CQ.Ext.reg('accordionfield', girlscouts.components.AccordionWidget);
-
-girlscouts.createID = function() {
-    if(this.idField.value == null){
-    	this.idField.setValue(this.idField.id);
-    }
-}
