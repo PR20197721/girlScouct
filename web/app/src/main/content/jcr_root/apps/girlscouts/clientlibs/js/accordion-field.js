4f85a354e832b6462fc028303b156954782ad37a
David Lubin <dlubin@northpointdigital.com>
2016-08-29 14:40:06 -0400

    GSWP-717 -WIP

--- web/app/src/main/content/jcr_root/apps/girlscouts/clientlibs/js/accordion-field.js
+++ web/app/src/main/content/jcr_root/apps/girlscouts/clientlibs/js/accordion-field.js
@@ -33,6 +33,175 @@ girlscouts.functions.deleteaccordion=function(field, event){
 	}
 };
 
+//fix for loadcontent listener
+girlscouts.components.AccordionPanel = CQ.Ext.extend(CQ.Ext.Panel,{
+    panelValue: '',
+
+    constructor: function(config){
+        config = config || {};
+        girlscouts.components.AccordionPanel.superclass.constructor.call(this, config);
+    },
+
+    initComponent: function () {
+    	girlscouts.components.AccordionPanel.superclass.initComponent.call(this);
+
+        this.panelValue = new CQ.Ext.form.Hidden({
+            name: this.name
+        });
+
+        this.add(this.panelValue);
+
+        var multifield = this.findParentByType('multifield'),
+            dialog = this.findParentByType('dialog');
+
+        dialog.on('beforesubmit', function(){
+            var value = this.getValue();
+
+            if(value){
+                this.panelValue.setValue(value);
+            }
+        },this);
+
+        dialog.on('loadcontent', function(){
+            if(_.isEmpty(multifield.dropTargets)){
+                multifield.dropTargets = [];
+            }
+
+            multifield.dropTargets = multifield.dropTargets.concat(this.getDropTargets());
+
+            _.each(multifield.dropTargets, function(target){
+                if (!target.highlight) {
+                    return;
+                }
+
+                var dialogZIndex = parseInt(dialog.el.getStyle("z-index"), 10);
+
+                if (!isNaN(dialogZIndex)) {
+                    target.highlight.zIndex = dialogZIndex + 1;
+                }
+            })
+        }, this);
+
+        if(dialog.gsInit){
+            return;
+        }
+
+        var tabPanel = multifield.findParentByType("tabpanel");
+
+        if(tabPanel){
+            tabPanel.on("tabchange", function(panel){
+                panel.doLayout();
+            });
+        }
+
+        dialog.on('hide', function(){
+            var editable = CQ.utils.WCM.getEditables()[this.path];
+
+            //dialog caching is a real pain when there are multifield items; donot cache
+            delete editable.dialogs[CQ.wcm.EditBase.EDIT];
+            delete CQ.WCM.getDialogs()["editdialog-" + this.path];
+        }, dialog);
+
+        dialog.gsInit = true;
+    },
+
+    afterRender : function(){
+        girlscouts.components.AccordionPanel.superclass.afterRender.call(this);
+
+        this.items.each(function(){
+            if(!this.contentBasedOptionsURL
+                || this.contentBasedOptionsURL.indexOf(CQ.form.Selection.PATH_PLACEHOLDER) < 0){
+                return;
+            }
+
+            this.processPath(this.findParentByType('dialog').path);
+        })
+    },
+
+    getValue: function () {
+        var pData = "";
+
+        this.items.each(function(i){
+            if(i.xtype == "label" || i.xtype == "hidden" || !i.hasOwnProperty("dName")){
+                return;
+            }
+
+            if(pData == ""){
+            	pData = i.getValue();
+            }else{
+            	pData = pData + "," + i.getValue();
+            }
+        });
+
+        return pData;
+    },
+
+    setValue: function (value) {
+        this.panelValue.setValue(value);
+
+        var pData = value.split(',');
+
+        var index = 0;
+        this.items.each(function(i){
+            if(i.xtype == "label" || i.xtype == "hidden" || !i.hasOwnProperty("dName")){
+                return;
+            }
+
+            i.setValue(pData[index]);
+            index++;
+            
+            i.fireEvent('loadcontent', this);
+        });
+    },
+
+    getDropTargets : function() {
+        var targets = [], t;
+
+        this.items.each(function(){
+            if(!this.getDropTargets){
+                return;
+            }
+
+            t = this.getDropTargets();
+
+            if(_.isEmpty(t)){
+                return;
+            }
+
+            targets = targets.concat(t);
+        });
+
+        return targets;
+    },
+
+    validate: function(){
+        var valid = true;
+
+        this.items.each(function(i){
+            if(!i.hasOwnProperty("dName")){
+                return;
+            }
+
+            if(!i.isVisible()){
+                i.allowBlank = true;
+                return;
+            }
+
+            if(!i.validate()){
+                valid = false;
+            }
+        });
+
+        return valid;
+    },
+
+    getName: function(){
+        return this.name;
+    }
+});
+
+CQ.Ext.reg("accordionpanel", girlscouts.components.AccordionPanel);
+
 girlscouts.components.AccordionWidget = CQ.Ext.extend(CQ.form.CompositeField, {
 	
     hiddenField: null,
@@ -87,6 +256,10 @@ girlscouts.components.AccordionWidget = CQ.Ext.extend(CQ.form.CompositeField, {
         this.idField = new CQ.Ext.form.TextField({
         	allowBlank: "{Boolean}false",
             listeners: {
+            	loadcontent: {
+            		scope:this,
+            		fn:this.createId
+            	},
                 change: {
                     scope:this,
                     fn:this.updateHidden
@@ -135,3 +308,9 @@ girlscouts.components.AccordionWidget = CQ.Ext.extend(CQ.form.CompositeField, {
 
 // register xtype
 CQ.Ext.reg('accordionfield', girlscouts.components.AccordionWidget);
+
+girlscouts.createID = function() {
+    if(this.idField.value == null){
+    	this.idField.setValue(this.idField.id);
+    }
+}
