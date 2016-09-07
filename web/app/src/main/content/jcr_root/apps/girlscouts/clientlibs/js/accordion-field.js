63a3c2c678b7561d95a25893fe4d0d503bc29a66
David Lubin <dlubin@northpointdigital.com>
2016-09-07 10:54:28 -0400

    GSWP-717 - Update delete message

--- web/app/src/main/content/jcr_root/apps/girlscouts/clientlibs/js/accordion-field.js
+++ web/app/src/main/content/jcr_root/apps/girlscouts/clientlibs/js/accordion-field.js
@@ -27,7 +27,7 @@ girlscouts.functions.deleteaccordion=function(field, event){
 		deleteAccordionFlag = true;
 		CQ.Ext.Msg.show({
 			title: "Warning", 
-			msg: "If you delete this field, all content contained within will be removed as well. You can RESTORE the content at a later point ON THIS PAGE ONLY by using the same identifier. " +
+			msg: "If you delete this field, all content contained within will be removed as well. You can RESTORE the content at a later point IN THIS ACCORDION COMPONENT ONLY by using the same identifier. " +
 			"If you do not want to delete this field, click 'Cancel' below to undo all changes",
 			buttons: CQ.Ext.Msg.OK
 			});
