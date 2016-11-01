2abdedce1f92a6a9dc160db5d0e6773d0d2eb149
lika2 <kaisum.li@ey.com>
2016-11-01 18:41:12 -0400

    WIP

--- vtk/bundle/src/main/java/org/girlscouts/vtk/models/Asset.java
+++ vtk/bundle/src/main/java/org/girlscouts/vtk/models/Asset.java
@@ -26,11 +26,22 @@ public class Asset implements Serializable {
 	@Field(path = true) private String path;
 	@Field Boolean isCachable;
 	@Field(id = true) private String uid;
+	@Field  private Boolean isOutdoorRelated;
 	private boolean isDbUpdate=false;
 	
 	
 	public String getDocType() {
 		return docType;
+	}
+	
+
+
+	public Boolean getIsOutdoorRelated() {
+		return isOutdoorRelated;
+	}
+
+	public void setIsOutdoorRelated(Boolean isOutdoorRelated) {
+		this.isOutdoorRelated = isOutdoorRelated;
 	}
 
 	public void setDocType(String docType) {
