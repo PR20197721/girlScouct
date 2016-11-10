c7baa1cd56fc91a1df896eaaa34b6597db4e1c5b
David Lubin <dlubin@northpointdigital.com>
2016-11-10 14:46:05 -0500

    GSWP-812 - Allow rollouts to preview only

---
+++ web/bundle/src/main/java/org/girlscouts/cq/workflow/PreviewRolloutProcess.java
@@ -0,0 +1,151 @@
+package org.girlscouts.cq.workflow;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import javax.jcr.ValueFormatException;
+
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.Reference;
+import org.apache.felix.scr.annotations.Service;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.resource.ResourceResolverFactory;
+import org.osgi.framework.Constants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.day.cq.replication.AgentIdFilter;
+import com.day.cq.replication.ReplicationActionType;
+import com.day.cq.replication.ReplicationException;
+import com.day.cq.replication.ReplicationOptions;
+import com.day.cq.replication.Replicator;
+import com.day.cq.wcm.api.Page;
+import com.day.cq.wcm.api.WCMException;
+import com.day.cq.wcm.msm.api.LiveRelationship;
+import com.day.cq.wcm.msm.api.LiveRelationshipManager;
+import com.day.cq.wcm.msm.api.RolloutManager;
+import com.day.cq.workflow.WorkflowException;
+import com.day.cq.workflow.WorkflowSession;
+import com.day.cq.workflow.exec.WorkItem;
+import com.day.cq.workflow.exec.WorkflowProcess;
+import com.day.cq.workflow.metadata.MetaDataMap;
+import javax.jcr.Value;
+
+@Component
+@Service
+public class PreviewRolloutProcess implements WorkflowProcess {
+	@Property(value = "Roll out a page if it is the source page of a live copy, and then activate target pages to preview publisher.")
+	static final String DESCRIPTION = Constants.SERVICE_DESCRIPTION;
+	@Property(value = "Girl Scouts")
+	static final String VENDOR = Constants.SERVICE_VENDOR;
+	@Property(value = "Girl Scouts Preview Roll out Process")
+	static final String LABEL = "process.label";
+    private static Logger log = LoggerFactory.getLogger(PreviewRolloutProcess.class);
+    
+    @Reference
+    RolloutManager rolloutManager;
+
+    @Reference
+    private Replicator replicator;
+    
+    @Reference
+    private LiveRelationshipManager relationManager;
+    
+	@Reference
+	private ResourceResolverFactory resourceResolverFactory;
+
+    public void execute(WorkItem item, WorkflowSession workflowSession, MetaDataMap metadata)
+            throws WorkflowException {
+        Session session = workflowSession.getSession();
+        ResourceResolver resourceResolver = null;
+
+        try {
+            resourceResolver = resourceResolverFactory.getResourceResolver(Collections.singletonMap(
+                    "user.jcr.session",
+                    (Object)session
+            ));
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+
+        String srcPath = item.getWorkflowData().getPayload().toString();
+        MetaDataMap mdm = item.getWorkflowData().getMetaDataMap();
+        Value singleValue = null;
+        Value[] values = null;
+        try {
+            values = (Value[]) mdm.get("councils");
+		} catch (Exception e) {
+			try{
+				singleValue = (Value) mdm.get("councils");
+			}catch(Exception e1){
+				e1.printStackTrace();
+			}
+		} 
+
+        Resource srcRes = resourceResolver.resolve(srcPath);
+        Page srcPage = (Page)srcRes.adaptTo(Page.class);
+        
+        if (srcPage == null) {
+            log.error("Resource is not a page. Quit. " + srcPath);
+            return;
+        }
+        
+        if (!relationManager.isSource(srcRes)) {
+            log.error("Not a live copy source page. Quit. " + srcPath);
+            return;
+        }
+
+        try {
+            Collection<LiveRelationship> relations = relationManager.getLiveRelationships(srcPage, null, null, true);
+            for (LiveRelationship relation : relations) {
+            	Resource targetResource = resourceResolver.resolve(relation.getTargetPath());
+            	Boolean proceed = false;
+            	if(!targetResource.getResourceType().equals(Resource.RESOURCE_TYPE_NON_EXISTING)){
+            		if(values == null){
+            			if(singleValue != null){
+            				if(targetResource.getPath().startsWith(singleValue.getString())){
+            					proceed = true;
+            				}
+            			}
+            		}else{
+	            		for(Value v : values){
+	            			if(targetResource.getPath().startsWith(v.getString())){
+	            				proceed = true;
+	            			}
+	            		}
+            		}
+            	}
+            	if(proceed == true){
+            		
+            	    String agentId = "publishpreview";
+            	    
+            	    AgentIdFilter filter = new AgentIdFilter(agentId);
+
+            	    ReplicationOptions opts = new ReplicationOptions();
+            	    opts.setFilter(filter);
+            	    
+            		rolloutManager.rollout(resourceResolver, relation, false);
+            		session.save();
+            	    try {
+            	    	String targetPath = relation.getTargetPath();
+    	                // Remove jcr:content
+    	                if (targetPath.endsWith("/jcr:content")) {
+    	                    targetPath = targetPath.substring(0, targetPath.lastIndexOf('/'));
+    	                }
+            	        replicator.replicate(session, ReplicationActionType.ACTIVATE, targetPath, opts);
+            	    } catch(Exception e) {
+            	        e.printStackTrace();
+            	    }
+            	}
+            }
+        } catch (WCMException e) {
+            log.error("WCMException for LiveRelationshipManager");
+        } catch (RepositoryException e) {
+            log.error("RepositoryException for LiveRelationshipManager");
+        }
+    }
+}
