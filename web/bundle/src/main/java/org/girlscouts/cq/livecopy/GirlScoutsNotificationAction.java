33d2a170fe305b3321dd7573553f1b2d96f8bd7b
David Lubin <dlubin@northpointdigital.com>
2016-11-15 17:12:54 -0500

    GSWP-812 - Setting up custom messages

--- web/bundle/src/main/java/org/girlscouts/cq/livecopy/GirlScoutsNotificationAction.java
+++ web/bundle/src/main/java/org/girlscouts/cq/livecopy/GirlScoutsNotificationAction.java
@@ -1,6 +1,11 @@
 package org.girlscouts.cq.livecopy;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -10,11 +15,15 @@ import javax.jcr.Session;
 import javax.jcr.Value;
 import javax.mail.internet.InternetAddress;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.mail.HtmlEmail;
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.Service;
 import org.apache.felix.scr.annotations.Property;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.ResourceUtil;
 import org.apache.sling.api.resource.ValueMap;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
@@ -39,18 +48,9 @@ public class GirlScoutsNotificationAction {
 	@Reference
 	public MessageGatewayService messageGatewayService;
 
-	private Resource source, target;
-	Boolean dontSend;
-	String message;
 	private final Logger log = LoggerFactory.getLogger(GirlScoutsNotificationAction.class);
-	public GirlScoutsNotificationAction(Resource source, Resource target, Boolean dontSend, String message){
-		source = this.source;
-		target = this.target;
-		dontSend = this.dontSend;
-		message = this.message;
-	}
-	
-	public void execute()
+
+	public void execute(Resource source, Resource target, String subject, String message, LiveRelationship relation, ResourceResolver rr)
 					throws WCMException {
 		if (source == null) {
 			log.info("Source is null. Quit");
@@ -78,7 +78,7 @@ public class GirlScoutsNotificationAction {
 				&& status.getAdvancedStatus("msm:isTargetCancelledChild")){
 			String sourcePath = source.getPath();
 			String targetPath = target.getPath();
-			if(configs !=null && configs.get("emlTemplate")!=null){
+			if(!"".equals(message)){
 				String branch = getBranch(targetPath);
 				log.info("**** GirlScoutsNotificationAction: sending email to "+branch.substring(9)+" *****");
 				ResourceResolver resourceResolver = source.getResourceResolver();
@@ -87,9 +87,9 @@ public class GirlScoutsNotificationAction {
 				ValueMap valuemap = homepage.getProperties();
 				String email1=(String)valuemap.get("email1");
 				String email2=(String)valuemap.get("email2");
-				send(sourcePath,targetPath,email1,email2,valuemap);
+				send(sourcePath,targetPath,email1,email2,valuemap, subject, message, rr);
 			}else{
-				throw new WCMException("Email template configuration was not found under /etc/msm/rolloutconfigs/gsdefault/jcr:content/gsNotification");
+				throw new WCMException("Rollout Notification Error - Unable to resolve email addresses");
 			}
 		
 		}
@@ -106,7 +106,7 @@ public class GirlScoutsNotificationAction {
         }
     }
     //send email using cq email service
-	public void send(String nationalPage, String councilPage,String email1, String email2, ValueMap vm) 
+	public void send(String nationalPage, String councilPage,String email1, String email2, ValueMap vm, String subject, String message, ResourceResolver rr) 
 			throws WCMException{
 		try {
 			
@@ -118,21 +118,16 @@ public class GirlScoutsNotificationAction {
 			
 			if(email1!=null && !email1.isEmpty()){//primary addr
 				emailRecipients.add(new InternetAddress(email1));
-			}else {//send to email address for testing
-				String testAddr = (String)configs.get("testEmail");
-				if(testAddr!=null && !testAddr.isEmpty()){ 
-					emailRecipients.add(new InternetAddress(testAddr));
-				}
-			}//backup addr
+			}
 			if(email2!=null && !email2.isEmpty())
 				emailRecipients.add(new InternetAddress(email2));
 
-			email.setSubject("GSUSA Rollout Notification");
-			String html = (String)configs.get("emlTemplate");
-			//html+="<p><b>National page URL:</b> <a href='http://girlscouts-prod.adobecqms.net"+getURL(nationalPage) + "'>http://girlscouts-prod.adobecqms.net"+getURL(nationalPage) +" </a></p>";
-			html+="<p><b>National page URL:</b> http://girlscouts-prod-aem61.adobecqms.net"+getURL(nationalPage) +"</p>";
-			html+="<p><b>Your page URL:</b> "+getRealUrl(councilPage,vm)+"</p>";
-			html+="<p>Click <a href='http://authornew.girlscouts.org"+getURL(councilPage)+"'>here</a> to edit your page.</p>";
+			email.setSubject(subject);
+			String html = message
+					.replaceAll("<%template-page%>", getURL(nationalPage))
+					.replaceAll("<%council-page%>",getRealUrl(councilPage, vm))
+					.replaceAll("<%council-author-page%>", getURL(councilPage));
+			html = parseHtml(html, email, rr);
 			email.setHtmlMsg(html);
 			if(!emailRecipients.isEmpty()){
 				email.setTo(emailRecipients);
@@ -167,8 +162,59 @@ public class GirlScoutsNotificationAction {
 		}
 		return (path+".html").replaceFirst("/content/([^/]+)","https://www.$1.org");
 	}
-
-	public String getName() {
-		return name;
-	} 
+ 
+    public String parseHtml(String html, HtmlEmail email, ResourceResolver rr){
+    	
+    	//Find images and replace them with embeds, embed the image file in the email
+    	final Pattern imgPattern = Pattern.compile("<img src=\"(.*?)\"");
+    	final Matcher imgMatcher = imgPattern.matcher(html);
+    	final StringBuffer imgSb = new StringBuffer();
+    	while(imgMatcher.find()){
+    		byte[] result = null;
+    		try {
+    			String renditionPath = getRenditionPath(imgMatcher.group(1));
+        		Resource imgRes = rr.resolve(renditionPath);
+        		if(ResourceUtil.isNonExistingResource(imgRes)) {
+        			imgRes = rr.resolve(renditionPath.replaceAll("%20"," "));
+        			if(ResourceUtil.isNonExistingResource(imgRes)){
+        				throw(new Exception("Cannot find resource: " + renditionPath));
+        			}
+        		}
+        		Node ntFileNode = imgRes.adaptTo(Node.class);
+        		Node ntResourceNode = ntFileNode.getNode("jcr:content");
+        		InputStream is = ntResourceNode.getProperty("jcr:data").getBinary().getStream();
+        		BufferedInputStream bin = new BufferedInputStream(is);
+        		result = IOUtils.toByteArray(bin);
+        		bin.close();
+        		is.close();
+			} catch (Exception e) {
+				log.error("Input Stream Failed");
+				System.out.println("Input Stream Failed");
+				e.printStackTrace();
+			}
+    		try {
+    			String fileName = imgMatcher.group(1).substring(imgMatcher.group(1).lastIndexOf('/') + 1);
+    			File imgFile = new File(fileName);
+    			FileUtils.writeByteArrayToFile(imgFile,result);
+				imgMatcher.appendReplacement(imgSb, "<img src=cid:" + (email.embed(imgFile,fileName)));
+		    	imgMatcher.appendTail(imgSb);
+		    	html = imgSb.toString();
+			} catch (Exception e) {
+				log.error("Failed to embed image");
+				e.printStackTrace();
+			}
+    	}
+    	
+    	return html;
+    }
+    
+    public String getRenditionPath(String imgPath){
+    	final Pattern pattern = Pattern.compile("/jcr:content/renditions/");
+    	final Matcher matcher = pattern.matcher(imgPath);
+    	if(matcher.find()){
+    		return imgPath;
+    	}else{
+    		return imgPath + "/jcr:content/renditions/original";
+    	}
+    }
 }
--- web/bundle/src/main/java/org/girlscouts/cq/workflow/RolloutProcess.java
+++ web/bundle/src/main/java/org/girlscouts/cq/workflow/RolloutProcess.java
@@ -1,11 +1,22 @@
 package org.girlscouts.cq.workflow;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.InputStream;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import javax.jcr.Node;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.mail.HtmlEmail;
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Property;
 import org.apache.felix.scr.annotations.Reference;
@@ -14,6 +25,8 @@ import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.resource.ResourceResolverFactory;
+import org.apache.sling.api.resource.ResourceUtil;
+import org.apache.sling.api.resource.ValueMap;
 import org.girlscouts.cq.livecopy.GirlScoutsNotificationAction;
 import org.osgi.framework.Constants;
 import org.slf4j.Logger;
@@ -101,6 +114,8 @@ public class RolloutProcess implements WorkflowProcess {
         		"<p><b>Your page URL:</b> <%council-page%></p>" +
         		"<p>Click <a href='<%council-author-page%>'>here</a> to edit your page.</p>";
         
+        String subject = "GSUSA Rollout Notification";
+        
         try {
         	useTemplate = ((Value)mdm.get("useTemplate")).getBoolean();
         	templatePath = ((Value)mdm.get("template")).getString();
@@ -110,13 +125,16 @@ public class RolloutProcess implements WorkflowProcess {
         	}
         }catch(Exception e){}
         
+        try{
+        	subject = ((Value)mdm.get("subject")).getString();
+        }catch(Exception e){}
+        
         try {
         	if(useTemplate){
-        		//Get Template Message
+        		message = getTemplate(templatePath, resourceResolver);
         	}else{
         		message = ((Value)mdm.get("message")).getString();
         	}	
-        	//Message is parsed in GirlScoutsNotificationActionFactory.java
 		} catch (Exception e) {
 			System.err.println("Rollout Error - Unable to Parse Message");
 			e.printStackTrace();
@@ -136,7 +154,8 @@ public class RolloutProcess implements WorkflowProcess {
         }
 
         try {
-            Collection<LiveRelationship> relations = relationManager.getLiveRelationships(srcPage, null, null, true);
+        	GirlScoutsNotificationAction mailActionFactory = new GirlScoutsNotificationAction();
+        	Collection<LiveRelationship> relations = relationManager.getLiveRelationships(srcPage, null, null, true);
             for (LiveRelationship relation : relations) {
             	Resource targetResource = resourceResolver.resolve(relation.getTargetPath());
             	Boolean proceed = false;
@@ -157,10 +176,9 @@ public class RolloutProcess implements WorkflowProcess {
             	}
             	if(proceed == true){
             		rolloutManager.rollout(resourceResolver, relation, false);
-            		//Stop using a live action for the notifications. Change GirlScoutsNotificationActionFactory
-            		//Into a new class and call it normally. Then you can pass whatever values into it that you want
-            		GirlScoutsNotificationAction actionFactory = new GirlScoutsNotificationAction(srcPage.adaptTo(Resource.class), targetResource, dontSend, message);
-            		actionFactory.execute();
+            		if(!dontSend){
+            			mailActionFactory.execute(srcPage.adaptTo(Resource.class), targetResource, subject, message, relation, resourceResolver);
+            		}
             		session.save();
 	                String targetPath = relation.getTargetPath();
 	                // Remove jcr:content
@@ -177,5 +195,25 @@ public class RolloutProcess implements WorkflowProcess {
         } catch (ReplicationException e) {
             log.error("ReplicationException for LiveRelationshipManager");
         }
+    }
+    
+    public String getTemplate(String templatePath, ResourceResolver resourceResolver){
+    	try{
+    		Resource templateResource = resourceResolver.resolve(templatePath);
+    		Resource dataResource = templateResource.getChild("jcr:content/data");
+    		ValueMap templateProps = ResourceUtil.getValueMap(dataResource);
+    		String message = templateProps.get("content","");
+    		String head = "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">" + 
+    				"<html xmlns=\"http://www.w3.org/1999/xhtml\">" + 
+    				"<head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">" +
+    				"<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">" +
+    				"<title>Girl Scouts</title></head>";
+    		String html = head + "<body>" + message + "</body></html>";
+    		return html;
+    	}catch(Exception e){
+    		log.error("No valid template found");
+    		e.printStackTrace();
+    		return "";
+    	}
     }
 }
