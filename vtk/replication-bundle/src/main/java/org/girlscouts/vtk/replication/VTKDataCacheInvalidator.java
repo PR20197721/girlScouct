2b282042b0b10d618fe3c11afb63ef2aed7cad4b
lika2 <kaisum.li@ey.com>
2016-11-21 15:08:40 -0500

    fixing the replciation agent so that it will account for the secone seasonal dispatchers

--- vtk/replication-bundle/src/main/java/org/girlscouts/vtk/replication/VTKDataCacheInvalidator.java
+++ vtk/replication-bundle/src/main/java/org/girlscouts/vtk/replication/VTKDataCacheInvalidator.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 import java.util.Date;
 
 import javax.jcr.RepositoryException;
+import javax.jcr.PathNotFoundException;
 import javax.jcr.Session;
 
 import org.apache.commons.httpclient.HttpClient;
@@ -30,13 +31,16 @@ public class VTKDataCacheInvalidator {
     
     // Interval for the next invalidation, in milliseconds.
     private static final int INTERVAL = 4000;
-    private static final String FLUSH_NODE = "/etc/replication/agents.publish/flush/jcr:content";
+    private static final String  = "FLUSH_NODE/etc/replication/agents.publish/flush/jcr:content";
+    private static final String FLUSH_NODE2 = "/etc/replication/agents.publish/flush2/jcr:content";
     private static final String FLUSH_PROPERTY = "transportUri";
     private static final String SCHEDULER_PATH_PREFIX = "VTK_PATH_";
+    private static final String SCHEDULER_PATH_PREFIX2 = "VTK_PATH2_";
 
     protected HttpClient httpClient;
     protected MultiThreadedHttpConnectionManager connectionManager;
     protected String flushUri;
+    protected String flushUri2 = "";
 
     @Reference
     protected Scheduler scheduler;
@@ -71,6 +75,30 @@ public class VTKDataCacheInvalidator {
             	get.releaseConnection();
             }
             log.info("VTKDataCacheInvalidator: Replication Invalidating cache done ==================");
+            
+            //second seasonal dispatcher if it exists
+            
+            if (!"".equals(flushUri2) && flushUri2 != null) {
+	            log.info("VTKDataCacheInvalidator2: ================== Replication Invalidating cache: ");
+	            GetMethod get2 = new GetMethod(flushUri2);
+	            log.debug("VTKDataCacheInvalidator2: Path: " + path);
+	            get2.setRequestHeader("CQ-Action", "Delete");
+	            get2.setRequestHeader("CQ-Handle", path);
+	            get2.setRequestHeader("CQ-Path", path);
+	            try {
+	                int resStatus = httpClient.executeMethod(get2);
+	                if (resStatus != 200) {
+	                    log.error("VTKDataCacheInvalidator2: Cannot invalidate this path: " + path);
+	                } else {
+	                    log.info("VTKDataCacheInvalidator2: Successfully invalidate the cache: " + path);
+	                }
+	            } catch (Exception e) {
+	                log.info("VTKDataCacheInvalidator2: Cannot invalidate this path: " + path + ". Do nothing.");
+	            } finally{
+	            	get2.releaseConnection();
+	            }
+	            log.info("VTKDataCacheInvalidator2: Replication Invalidating cache done==================");
+            }
         }
     }
     
@@ -87,6 +115,20 @@ public class VTKDataCacheInvalidator {
             log.error("VTKDataCacheInvalidator: RepositoryException while initializing.");
             e.printStackTrace();
         } 
+        
+        //second seasonal dispatcher
+        try {
+        	Session session = repository.loginAdministrative(null);
+            flushUri2 = session.getNode(FLUSH_NODE2).getProperty(FLUSH_PROPERTY).getString();
+            session.logout();
+            log.info("The second flush agent is up and Started. The flushUri is " + flushUri2 + " with the node " + FLUSH_NODE2);
+        } catch(PathNotFoundException pnfe) {
+        	log.info("VTKDataCacheInvalidator2. There is no second flush agent. Please ignore.");
+        	pnfe.printStackTrace();
+        } catch (RepositoryException e) {
+            log.error("VTKDataCacheInvalidator2: RepositoryException while initializing.");
+            e.printStackTrace();
+        } 
     }
     
     @Deactivate
@@ -97,6 +139,7 @@ public class VTKDataCacheInvalidator {
     public void addPath(String path) {
         try {
         	scheduler.fireJobAt(SCHEDULER_PATH_PREFIX + path, new CacheInvalidationJob(path), null, new Date(System.currentTimeMillis() + INTERVAL));
+        	scheduler.fireJobAt(SCHEDULER_PATH_PREFIX2 + path, new CacheInvalidationJob(path), null, new Date(System.currentTimeMillis() + INTERVAL));
         } catch (Exception e) {
             log.error("VTKDataCacheInvalidator: Cannot add path: " + path);
         }
