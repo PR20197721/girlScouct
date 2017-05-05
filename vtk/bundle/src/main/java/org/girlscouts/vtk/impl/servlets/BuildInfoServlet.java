0f4b8f499f2dbf93deb477d23cd0d9e1d0ac2c21
A Kast <Ari.Kast@ey.com>
2017-05-05 13:29:06 -0400

    better 401 protection

--- vtk/bundle/src/main/java/org/girlscouts/vtk/impl/servlets/BuildInfoServlet.java
+++ vtk/bundle/src/main/java/org/girlscouts/vtk/impl/servlets/BuildInfoServlet.java
@@ -12,7 +12,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.util.Properties;
 
-
 @SlingServlet( paths = {"/bin/checkup/vtk"} )
 public class BuildInfoServlet extends SlingSafeMethodsServlet {
 	private static final long serialVersionUID = 5981389970977916595L;
@@ -26,24 +25,26 @@ public class BuildInfoServlet extends SlingSafeMethodsServlet {
 			SlingHttpServletResponse response) {
 
 		HttpSession session = request.getSession(false);
-		if (session == null) {
-				return;
-		} else {
-            try{
-                StringBuilder builder = new StringBuilder();
-                
-                Properties properties = new Properties();
-                properties.load(getClass().getClassLoader().getResourceAsStream("girlscoutsbuild.properties"));
-                  
-                builder.append("built from commit ").append(properties.getProperty("git.commit.id")).append("\n");
-                builder.append("version ").append(properties.getProperty("git.build.version")).append("\n");
-                builder.append("branch ").append(properties.getProperty("git.branch")).append("\n");
-                builder.append("built on ").append(properties.getProperty("git.build.time")).append("\n");
-                
-                response.getWriter().write(builder.toString()); 
-            } catch(Exception e) {
-                e.printStackTrace(System.err);
+        try{
+            if (session == null || session.getAttribute(org.girlscouts.vtk.models.User.class.getName()) == null) {
+                    response.getWriter().write("forbidden"); 
+                    response.setStatus(401);
+                    return;
+            } else {
+                    StringBuilder builder = new StringBuilder();
+                    
+                    Properties properties = new Properties();
+                    properties.load(getClass().getClassLoader().getResourceAsStream("girlscoutsbuild.properties"));
+                      
+                    builder.append("built from commit ").append(properties.getProperty("git.commit.id")).append("\n");
+                    builder.append("version ").append(properties.getProperty("git.build.version")).append("\n");
+                    builder.append("branch ").append(properties.getProperty("git.branch")).append("\n");
+                    builder.append("built on ").append(properties.getProperty("git.build.time")).append("\n");
+                    
+                    response.getWriter().write(builder.toString()); 
             }
+        } catch(Exception e) {
+            e.printStackTrace(System.err);
         }
     }
 
