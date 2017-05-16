eb9dd415ee65220fccfebfecc31e206d70d989ee
Adella Lin <adellalin@gmail.com>
2017-05-16 16:08:59 -0400

    update dev deployment script for new dev publisher

--- vtk/deployDev.sh
+++ vtk/deployDev.sh
@@ -12,5 +12,5 @@ curl -u 'admin:e$Fz&rsBS.XZk$6F'  -X POST http://52.71.87.139:4502/crx/packmgr/s
 curl -u 'admin:e$Fz&rsBS.XZk$6F' -F file=@"$HOME/.m2/repository/org/girlscouts/vtk/girlscouts-vtk-app/$VERSION/girlscouts-vtk-app-$VERSION.zip" -F name="girlscouts-app" -F force=true -F install=true http://52.71.87.139:4502/crx/packmgr/service.jsp
 
 echo "Deleting existing VTK package in publish..."
-curl -u 'admin:e$Fz&rsBS.XZk$6F'  -X POST http://52.1.73.148:4503/crx/packmgr/service/.json/etc/packages/Girl%20Scouts/girlscouts-vtk-app-$VERSION.zip?cmd=delete
-curl -u 'admin:e$Fz&rsBS.XZk$6F' -F file=@"$HOME/.m2/repository/org/girlscouts/vtk/girlscouts-vtk-app/$VERSION/girlscouts-vtk-app-$VERSION.zip" -F name="girlscouts-app" -F force=true -F install=true http://52.1.73.148:4503/crx/packmgr/service.jsp
+curl -u 'admin:e$Fz&rsBS.XZk$6F'  -X POST http://34.224.42.66:4503/crx/packmgr/service/.json/etc/packages/Girl%20Scouts/girlscouts-vtk-app-$VERSION.zip?cmd=delete
+curl -u 'admin:e$Fz&rsBS.XZk$6F' -F file=@"$HOME/.m2/repository/org/girlscouts/vtk/girlscouts-vtk-app/$VERSION/girlscouts-vtk-app-$VERSION.zip" -F name="girlscouts-app" -F force=true -F install=true http://34.224.42.66:4503/crx/packmgr/service.jsp
