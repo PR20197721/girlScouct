a391440dc1760551671a32d58703005cb9609877
kapluig <kapluig@US013760499W10.local>
2017-08-21 15:44:53 -0400

    Modified image path for bulkeditor events

--- web/app/src/main/content/jcr_root/apps/wcm/core/components/gsbulkeditor/csv.java
+++ web/app/src/main/content/jcr_root/apps/wcm/core/components/gsbulkeditor/csv.java
@@ -192,7 +192,7 @@ public class csv extends SlingAllMethodsServlet {
 	                    		case "jcr:content/cq:tags-progLevel":
 	                    			property = "Program Levels";
 	                    			break;
-	                    		case "jcr:content/data/imagePath":
+	                    		case "jcr:content/data/image/fileReference":
 	                    			property = "Image";
 	                    			break;
 	                    		case "jcr:content/data/regOpen-date":
