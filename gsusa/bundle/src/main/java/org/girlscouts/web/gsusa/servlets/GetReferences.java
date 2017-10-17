2584381d2958896804e2ea88e443728e62aa87c3
Dmitriy Bakum <dmitriy.bakum@ey.com>
2017-10-17 16:30:15 -0400

    adding full path of where asset is used in shared asset metrics report

--- gsusa/bundle/src/main/java/org/girlscouts/web/gsusa/servlets/GetReferences.java
+++ gsusa/bundle/src/main/java/org/girlscouts/web/gsusa/servlets/GetReferences.java
@@ -1,16 +1,19 @@
 package org.girlscouts.web.gsusa.servlets;
 
 import com.day.text.csv.Csv;
+import com.day.cq.replication.ReplicationStatus;
 import com.day.cq.wcm.commons.ReferenceSearch;
 import org.apache.felix.scr.annotations.Properties;
 import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.sling.SlingServlet;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.servlets.SlingAllMethodsServlet;
- 
+import org.apache.sling.settings.SlingSettingsService;
+
 import javax.servlet.ServletException;
 import java.io.IOException;
 import java.io.Writer;
@@ -18,9 +21,9 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 import java.util.Iterator;
 
 @SlingServlet
@@ -35,58 +38,67 @@ public class GetReferences extends SlingAllMethodsServlet {
 	 */
 	private static final long serialVersionUID = -1341523521748738122L;
 
+	@Reference
+	private SlingSettingsService settingsService;
+
 	@Override
 	protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response)
 			throws ServletException, IOException {
-		response.setCharacterEncoding("UTF-8");
-		response.setContentType("text/csv");
-		String disposition = "attachment; fileName=references.csv";
-		response.setHeader("Content-Disposition", disposition);
-		final Writer writer = response.getWriter();
-		final Csv csv = new Csv();
-		csv.writeInit(writer);
-		String path = request.getParameter("path");
+		if (!isPublisher()) {
+			response.setContentType("text/csv");
+			String disposition = "attachment; fileName=references.csv";
+			response.setHeader("Content-Disposition", disposition);
+			final Writer writer = response.getWriter();
+			final Csv csv = new Csv();
+			csv.writeInit(writer);
+			String path = request.getParameter("path");
 
-		if (path == null || path.trim().length() == 0) {
-			throw new ServletException("Empty path");
-		}
+			if (path == null || path.trim().length() == 0) {
+				throw new ServletException("Empty path");
+			}
 
-		try {
-			ResourceResolver resolver = request.getResourceResolver();
-			List<String> assetPaths = getAssets(resolver, path);
-			ReferenceSearch referenceSearch = new ReferenceSearch();
-			referenceSearch.setExact(true);
-			referenceSearch.setHollow(true);
-			referenceSearch.setMaxReferencesPerPage(-1);
-			List<List<String>> table = new ArrayList<List<String>>();
-			for (String asset : assetPaths) {
-				List<String> row = new ArrayList<String>();
-				Set<String> councils = new HashSet<String>();
-				row.add(asset);
-				Collection<ReferenceSearch.Info> resultSet = referenceSearch.search(resolver, asset).values();
-				for (ReferenceSearch.Info info : resultSet) {
-					for (String p : info.getProperties()) {
-						int councilNameIndex = p.indexOf("/", "/content/".length());
-						String councilPath = p.substring(0, councilNameIndex);
-						councils.add(councilPath);
+			try {
+				ResourceResolver resolver = request.getResourceResolver();
+				List<String> assetPaths = getAssets(resolver, path);
+				ReferenceSearch referenceSearch = new ReferenceSearch();
+				referenceSearch.setExact(true);
+				referenceSearch.setHollow(true);
+				referenceSearch.setMaxReferencesPerPage(-1);
+				List<List<String>> table = new ArrayList<List<String>>();
+				for (String asset : assetPaths) {
+					List<String> row = new ArrayList<String>();
+					Set<String> councils = new TreeSet<String>();
+					row.add(asset);  
+					Collection<ReferenceSearch.Info> resultSet = referenceSearch.search(resolver, asset).values();
+					for (ReferenceSearch.Info info : resultSet) {
+						for (String p : info.getProperties()) {
+							Resource resource  = resolver.resolve(p);
+							ReplicationStatus replicationStatus = resource.adaptTo(ReplicationStatus.class);
+							if (replicationStatus.isActivated()) {
+								p = p + "(Active)";
+							} else {
+								p = p + "(Not Active)";
+							}
+							councils.add(p);
+						}
 					}
+					row.addAll(councils);
+					table.add(row);
 				}
-				row.addAll(councils);
-				table.add(row);
-			}
-			Collections.sort(table, new Comparator<List<String>>() {
-				public int compare(List<String> a1, List<String> a2) {
-					return a2.size() - a1.size();
+				Collections.sort(table, new Comparator<List<String>>() {
+					public int compare(List<String> a1, List<String> a2) {
+						return a2.size() - a1.size();
+					}
+				});
+				table = transpose(table);
+				for (List<String> row : table) {
+					csv.writeRow(row.toArray(new String[row.size()]));
 				}
-			});
-			table = transpose(table);
-			for (List<String> row : table) {
-				csv.writeRow(row.toArray(new String[row.size()]));
+			} catch (Exception e) {
+				throw new ServletException("Error getting references", e);
+			} finally {
+				csv.close();
 			}
-		} catch (Exception e) {
-			throw new ServletException("Error getting references", e);
-		} finally {
-			csv.close();
 		}
 	}
 
@@ -132,6 +144,13 @@ public class GetReferences extends SlingAllMethodsServlet {
 			}
 		}
 		return results;
+	}
+
+	private boolean isPublisher() {
+		if (settingsService.getRunModes().contains("publish")) {
+			return true;
+		}
+		return false;
 	}
 
 }
