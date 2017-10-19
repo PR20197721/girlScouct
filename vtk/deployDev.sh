33ad91c5ee9953a4804a7f8707a8289da03a2b64
Mike Zou <mzou@northpointdigital.com>
2017-10-19 15:19:23 -0400

    Update IPs in deploy dev script for 6.3

--- vtk/deployDev.sh
+++ vtk/deployDev.sh
@@ -8,9 +8,9 @@ if [ -z $VERSION ]; then
 fi
 
 echo "Deleting existing VTK package in author..."
-curl -u 'admin:e$Fz&rsBS.XZk$6F'  -X POST http://52.71.87.139:4502/crx/packmgr/service/.json/etc/packages/Girl%20Scouts/girlscouts-vtk-app-$VERSION.zip?cmd=delete
-curl -u 'admin:e$Fz&rsBS.XZk$6F' -F file=@"$HOME/.m2/repository/org/girlscouts/vtk/girlscouts-vtk-app/$VERSION/girlscouts-vtk-app-$VERSION.zip" -F name="girlscouts-app" -F force=true -F install=true http://52.71.87.139:4502/crx/packmgr/service.jsp
+curl -u 'admin:e$Fz&rsBS.XZk$6F'  -X POST http://34.205.130.12:4502/crx/packmgr/service/.json/etc/packages/Girl%20Scouts/girlscouts-vtk-app-$VERSION.zip?cmd=delete
+curl -u 'admin:e$Fz&rsBS.XZk$6F' -F file=@"$HOME/.m2/repository/org/girlscouts/vtk/girlscouts-vtk-app/$VERSION/girlscouts-vtk-app-$VERSION.zip" -F name="girlscouts-app" -F force=true -F install=true http://34.205.130.12:4502/crx/packmgr/service.jsp
 
 echo "Deleting existing VTK package in publish..."
-curl -u 'admin:e$Fz&rsBS.XZk$6F'  -X POST http://34.224.42.66:4503/crx/packmgr/service/.json/etc/packages/Girl%20Scouts/girlscouts-vtk-app-$VERSION.zip?cmd=delete
-curl -u 'admin:e$Fz&rsBS.XZk$6F' -F file=@"$HOME/.m2/repository/org/girlscouts/vtk/girlscouts-vtk-app/$VERSION/girlscouts-vtk-app-$VERSION.zip" -F name="girlscouts-app" -F force=true -F install=true http://34.224.42.66:4503/crx/packmgr/service.jsp
+curl -u 'admin:e$Fz&rsBS.XZk$6F'  -X POST http://34.236.166.152:4503/crx/packmgr/service/.json/etc/packages/Girl%20Scouts/girlscouts-vtk-app-$VERSION.zip?cmd=delete
+curl -u 'admin:e$Fz&rsBS.XZk$6F' -F file=@"$HOME/.m2/repository/org/girlscouts/vtk/girlscouts-vtk-app/$VERSION/girlscouts-vtk-app-$VERSION.zip" -F name="girlscouts-app" -F force=true -F install=true http://34.236.166.152:4503/crx/packmgr/service.jsp
