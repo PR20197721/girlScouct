67510441f8032b8007fcc4da10638bdc49cb7ffb
Mike Zou <mzou@northpointdigital.com>
2017-12-27 15:37:27 -0500

    WIP

--- web/app/src/main/content/jcr_root/apps/cq/ui/widgets/source/cancelScheduledActivation.js
+++ web/app/src/main/content/jcr_root/apps/cq/ui/widgets/source/cancelScheduledActivation.js
@@ -1,6 +1,14 @@
 CQ.wcm.SiteAdmin.cancelScheduledActivation = function() {
 	var path = this.getSelectedPages()[0].id;
-	CQ.shared.HTTP.get("/bin/querybuilder.json?1_property=data/payload/path&1_property.value=" + path + "&2_property=status&2_property.value=RUNNING&path=/etc/workflow", function(options, success, response) {
+	CQ.shared.HTTP.get("/bin/querybuilder.json?" + 
+			"1_property=data/payload/path" +
+			"&1_property.value=" + path +
+			"&2_property=status" +
+			"&2_property.value=RUNNING" +
+			"&3_property=modelId" +
+			"&3_property.value=/etc/workflow/models/scheduled_activation/jcr:content/model" +
+			"&path=/etc/workflow",
+		function(options, success, response) {
 		var result = JSON.parse(response.body);
 		var hits = result.hits;
 		if (hits != undefined && hits.length >= 1) {
@@ -10,7 +18,8 @@ CQ.wcm.SiteAdmin.cancelScheduledActivation = function() {
 			        title: CQ.I18n.getMessage('Cancellation'),
 			        msg: CQ.I18n.getMessage('Scheduled activation successfully canceled.'),
 			        buttons: CQ.Ext.Msg.OK,
-			        icon: CQ.Ext.Msg.INFO
+			        icon: CQ.Ext.Msg.INFO,
+			        fn: function(){location.reload()}
 			    });
 			}, {
 				state: "ABORTED"
