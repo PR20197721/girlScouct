d87eedad9d7f5f1556d64f67f5baf6e135daa47e
Unknown <dmitriy.bakum@ey.com>
2018-01-10 13:52:59 -0500

    fixed deployment script for gsusa dev

--- gsusa/deployDev.sh
+++ gsusa/deployDev.sh
@@ -7,8 +7,8 @@ if [ -z $VERSION ]; then
     VERSION=`head -1 ../VERSIONS.txt | cut -d ' ' -f 1`
 fi
 
-curl  -u 'admin:e$Fz&rsBS.XZk$6F'  -X POST http://34.205.130.12:4502/crx/packmgr/service/.json/etc/packages/org.girlscouts.web.gsusa/gsusa-app-$VERSION.zip?cmd=delete
-curl -u 'admin:e$Fz&rsBS.XZk$6F' -F file=@"$HOME/.m2/repository/org/girlscouts/web/gsusa-app/$VERSION/gsusa-app-$VERSION.zip" -F name="gsusa-app" -F force=true -F install=true http://52.71.87.139:4502/crx/packmgr/service.jsp
+curl -u 'admin:e$Fz&rsBS.XZk$6F' -X POST http://34.205.130.12:4502/crx/packmgr/service/.json/etc/packages/org.girlscouts.web.gsusa/gsusa-app-$VERSION.zip?cmd=delete
+curl -u 'admin:e$Fz&rsBS.XZk$6F' -F file=@"$HOME/.m2/repository/org/girlscouts/web/gsusa-app/$VERSION/gsusa-app-$VERSION.zip" -F name="gsusa-app" -F force=true -F install=true http://34.205.130.12:4502/crx/packmgr/service.jsp
 
-curl  -u 'admin:e$Fz&rsBS.XZk$6F'  -X POST http://34.236.166.152:4503/crx/packmgr/service/.json/etc/packages/org.girlscouts.web.gsusa/gsusa-app-$VERSION.zip?cmd=delete
-curl -u 'admin:e$Fz&rsBS.XZk$6F' -F file=@"$HOME/.m2/repository/org/girlscouts/web/gsusa-app/$VERSION/gsusa-app-$VERSION.zip" -F name="gsusa-app" -F force=true -F install=true http://52.1.73.148:4503/crx/packmgr/service.jsp
+curl -u 'admin:e$Fz&rsBS.XZk$6F' -X POST http://34.236.166.152:4503/crx/packmgr/service/.json/etc/packages/org.girlscouts.web.gsusa/gsusa-app-$VERSION.zip?cmd=delete
+curl -u 'admin:e$Fz&rsBS.XZk$6F' -F file=@"$HOME/.m2/repository/org/girlscouts/web/gsusa-app/$VERSION/gsusa-app-$VERSION.zip" -F name="gsusa-app" -F force=true -F install=true http://34.236.166.152:4503/crx/packmgr/service.jsp
