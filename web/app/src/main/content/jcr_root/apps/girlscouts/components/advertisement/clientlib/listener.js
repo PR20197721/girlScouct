386faf9fcf20f45f17a7de7bdcd5d323890d8a1d
Dmitriy Bakum <dmitriy.bakum@ey.com>
2018-01-12 16:59:33 -0500

    fixed a custom mode checkbox in advertisement component

--- web/app/src/main/content/jcr_root/apps/girlscouts/components/advertisement/clientlib/listener.js
+++ web/app/src/main/content/jcr_root/apps/girlscouts/components/advertisement/clientlib/listener.js
@@ -1,7 +1,24 @@
-/*(function (document, $, ns) {
+(function (document, $, ns) {
     "use strict";
-    
     $(document).on("dialog-ready", function() {
-        $(window).adaptTo("foundation-ui").alert("Open", "Dialog now open, event [dialog-ready]");
+		var $resourceType = $("coral-dialog-content").find("[name='./sling:resourceType']").val();
+        if("girlscouts/components/advertisement" == $resourceType){
+        	var $multifield = $("label:contains('Ads Selection')").parent();
+            $("input[type='checkbox'][name='./customized']").change(function() {
+				var $isCustomizedChecked = $("input[type='checkbox'][name='./customized']").is(':checked');
+                if($isCustomizedChecked){
+                	$multifield.find("input").prop( "disabled", true );
+                	$multifield.find("button").prop( "disabled", true );
+                }else{
+                	$multifield.find("input").prop( "disabled", false );
+                	$multifield.find("button").prop( "disabled", false );
+                }
+            });
+            var $isCustomizedChecked = $("input[type='checkbox'][name='./customized']").is(':checked');
+            if($isCustomizedChecked){
+            	$multifield.find("input").prop( "disabled", true );
+            	$multifield.find("button").prop( "disabled", true );
+            }
+        }
     });
-})(document, Granite.$, Granite.author);*/
+})(document, Granite.$, Granite.author);
