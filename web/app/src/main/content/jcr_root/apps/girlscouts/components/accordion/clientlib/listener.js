f3fc68a7b77b059682bc81d15d4f17fbe5d0572c
Dmitriy Bakum <dmitriy.bakum@ey.com>
2018-02-21 16:44:36 -0500

    fixed js code so that remove and id generation will only trigger when its a accordion dialog

--- web/app/src/main/content/jcr_root/apps/girlscouts/components/accordion/clientlib/listener.js
+++ web/app/src/main/content/jcr_root/apps/girlscouts/components/accordion/clientlib/listener.js
@@ -1,35 +1,51 @@
 (function (document, $, ns) {
     "use strict";
     var DELETE_ACCORDION_FLAG = false;
-    $(document).on("coral-component:attached", "[name='idField']", function (e) {
-		var id = $(this).val();
-        if(id == null || id == ""){
-            var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-            for( var i=0; i < 8; i++ ){
-                id += possible.charAt(Math.floor(Math.random() * possible.length));
-            }
-			$(this).val(id);
+    $(document).on("dialog-ready", function() {
+		var $resourceType = $("coral-dialog-content").find("[name='./sling:resourceType']").val();
+        if("girlscouts/components/accordion" == $resourceType){
+        	var $multifield = $("coral-dialog-content").find("coral-multifield[data-granite-coral-multifield-name='./children']");
+        	$multifield.find("input[name='idField']").each(function() {
+        		setAccordionIdentifier($(this));
+        	});
+        	$multifield.on("coral-component:attached", "input[name='idField']", function (e) {
+                if("girlscouts/components/accordion" == $resourceType){
+                	setAccordionIdentifier($(this));
+                }
+            });
+        	$multifield.on("coral-component:attached", "button[handle='remove']", function (e) {            	
+                if("girlscouts/components/accordion" == $resourceType){
+        	        e.stopPropagation();
+        	        e.stopImmediatePropagation();
+        	        e.preventDefault();
+        	        $(this).click(function(){
+        	        	if(DELETE_ACCORDION_FLAG == false){
+        	        		DELETE_ACCORDION_FLAG = true;
+        	                ns.ui.helpers.prompt({
+        	                    title: Granite.I18n.get("Warning"),
+        	                    message: "<div>If you delete this field, all content contained within will be removed as well.</div>" +
+        	                            "<div>You can RESTORE the content at a later point IN THIS ACCORDION COMPONENT ONLY by using the same identifier.</div>" +
+        	                            "<div>If you do not want to delete this field, click 'X' above to undo all changes</div>",
+        	                    actions: [{
+        	                            id: "OK",
+        	                            text: "OK",
+        	                            className: "coral-Button"
+        	                    }]
+        	                });
+        	             }
+        	        });
+                }
+            });
         }
     });
-    $(document).on("coral-component:attached", "[is='coral-button'][handle='remove']", function (e) {
-        e.stopPropagation();
-        e.stopImmediatePropagation();
-        e.preventDefault();
-        $(this).click(function(){
-        	if(DELETE_ACCORDION_FLAG == false){
-        		DELETE_ACCORDION_FLAG = true;
-                ns.ui.helpers.prompt({
-                    title: Granite.I18n.get("Warning"),
-                    message: "<div>If you delete this field, all content contained within will be removed as well.</div>" +
-                            "<div>You can RESTORE the content at a later point IN THIS ACCORDION COMPONENT ONLY by using the same identifier.</div>" +
-                            "<div>If you do not want to delete this field, click 'X' above to undo all changes</div>",
-                    actions: [{
-                            id: "OK",
-                            text: "OK",
-                            className: "coral-Button"
-                    }]
-                });
-             }
-        });
-    });
 })(document, Granite.$, Granite.author);
+function setAccordionIdentifier(el){
+	var id = el.val();
+    if(id == null || id == ""){
+        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        for( var i=0; i < 8; i++ ){
+            id += possible.charAt(Math.floor(Math.random() * possible.length));
+        }
+        el.val(id);
+    }
+}
