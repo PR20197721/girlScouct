b1f3ac6d53748f0a7d7879000d1e1fde449d201a
Dmitriy Bakum <dmitriy.bakum@ey.com>
2018-02-22 12:03:27 -0500

    fixed js for accordion dialog

--- web/app/src/main/content/jcr_root/apps/girlscouts/components/accordion/clientlib/listener.js
+++ web/app/src/main/content/jcr_root/apps/girlscouts/components/accordion/clientlib/listener.js
@@ -4,48 +4,50 @@
     $(document).on("dialog-ready", function() {
 		var $resourceType = $("coral-dialog-content").find("[name='./sling:resourceType']").val();
         if("girlscouts/components/accordion" == $resourceType){
+        	DELETE_ACCORDION_FLAG = false;
         	var $multifield = $("coral-dialog-content").find("coral-multifield[data-granite-coral-multifield-name='./children']");
+        	//handle existing items in multifield
         	$multifield.find("input[name='idField']").each(function() {
-        		setAccordionIdentifier($(this));
+        		setAccordionIdentifier(this);
         	});
+        	$multifield.find("button[handle='remove']").each(function() {
+        		setRemoveButtonMessage(this);
+        	});
+        	//attach event handlers for future items in multifield
         	$multifield.on("coral-component:attached", "input[name='idField']", function (e) {
-                if("girlscouts/components/accordion" == $resourceType){
-                	setAccordionIdentifier($(this));
-                }
+            	setAccordionIdentifier(this);
             });
         	$multifield.on("coral-component:attached", "button[handle='remove']", function (e) {            	
-                if("girlscouts/components/accordion" == $resourceType){
-        	        e.stopPropagation();
-        	        e.stopImmediatePropagation();
-        	        e.preventDefault();
-        	        $(this).click(function(){
-        	        	if(DELETE_ACCORDION_FLAG == false){
-        	        		DELETE_ACCORDION_FLAG = true;
-        	                ns.ui.helpers.prompt({
-        	                    title: Granite.I18n.get("Warning"),
-        	                    message: "<div>If you delete this field, all content contained within will be removed as well.</div>" +
-        	                            "<div>You can RESTORE the content at a later point IN THIS ACCORDION COMPONENT ONLY by using the same identifier.</div>" +
-        	                            "<div>If you do not want to delete this field, click 'X' above to undo all changes</div>",
-        	                    actions: [{
-        	                            id: "OK",
-        	                            text: "OK",
-        	                            className: "coral-Button"
-        	                    }]
-        	                });
-        	             }
-        	        });
-                }
+        		setRemoveButtonMessage(this);
             });
         }
     });
-})(document, Granite.$, Granite.author);
-function setAccordionIdentifier(el){
-	var id = el.val();
-    if(id == null || id == ""){
-        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-        for( var i=0; i < 8; i++ ){
-            id += possible.charAt(Math.floor(Math.random() * possible.length));
+    function setAccordionIdentifier(el){
+    	var id = $(el).val();
+        if(id == null || id == ""){
+            var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            for( var i=0; i < 8; i++ ){
+                id += possible.charAt(Math.floor(Math.random() * possible.length));
+            }
+            el.val(id);
         }
-        el.val(id);
     }
-}
+    function setRemoveButtonMessage(el){
+    	$(el).click(function(){
+        	if(DELETE_ACCORDION_FLAG == false){
+        		DELETE_ACCORDION_FLAG = true;
+                ns.ui.helpers.prompt({
+                    title: Granite.I18n.get("Warning"),
+                    message: "<div>If you delete this field, all content contained within will be removed as well.</div>" +
+                            "<div>You can RESTORE the content at a later point IN THIS ACCORDION COMPONENT ONLY by using the same identifier.</div>" +
+                            "<div>If you do not want to delete this field, click 'X' above to undo all changes</div>",
+                    actions: [{
+                            id: "OK",
+                            text: "OK",
+                            className: "coral-Button"
+                    }]
+                });
+             }
+        });
+    }
+})(document, Granite.$, Granite.author);
