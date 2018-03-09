223e3ec48e5fb6d1084de2eb7f0026934324f464
kapluig <kapluig@US013760499W10.local>
2018-03-09 15:46:12 -0500

    Added refresh for hero banner

---
+++ web/app/src/main/content/jcr_root/apps/girlscouts/components/hero-banner/clientlib/listener.js
@@ -0,0 +1,30 @@
+/**
+ * Extension to the standard dropdown/select component. It enabled hidding/unhidding of other components based on the
+ * selection made in the dropdown/select.
+ *
+ * How to use:
+ *
+ * - add the class cq-dialog-dropdown-showhide to the dropdown/select element
+ * - add the data attribute cq-dialog-dropdown-showhide-target to the dropdown/select element, value should be the
+ *   selector, usually a specific class name, to find all possible target elements that can be shown/hidden.
+ * - add the target class to each target component that can be shown/hidden
+ * - add the class hidden to each target component to make them initially hidden
+ * - add the attribute showhidetargetvalue to each target component, the value should equal the value of the select
+ *   option that will unhide this element.
+ */
+(function (document, $, ns) {
+    "use strict";
+
+
+    $(document).on("dialog-success", function() {
+		var $resourceType = $("coral-dialog-content").find("[name='./sling:resourceType']").val();
+        if("girlscouts/components/hero-banner" == $resourceType){
+        	window.location.reload(false);
+        }
+
+	});
+
+
+
+
+})(document, Granite.$);
