50c55e89681c7451b7db16c624051a91d1b316c9
Unknown <dmitriy.bakum@ey.com>
2018-04-18 13:03:01 -0400

    migrated footer nav to 6.3 and migration script

--- gsusa/app/src/main/content/jcr_root/apps/gsusa/components/footer-nav/footer-nav.jsp
+++ gsusa/app/src/main/content/jcr_root/apps/gsusa/components/footer-nav/footer-nav.jsp
@@ -2,30 +2,24 @@
 <%@include file="/libs/foundation/global.jsp" %>
 <%@include file="/apps/gsusa/components/global.jsp" %>
 <%
-    final String[] navs = properties.get("navs", String[].class);
-	if (navs != null) {
-%>
-		<ul class="inline-list">
-<% 
-	    for (int i = 0; i < navs.length; i++) {
-		    String[] split = navs[i].split("\\|\\|\\|");
-		    String label = split.length >= 1 ? split[0] : "";
-		    String link = split.length >= 2 ? split[1] : "";
-		    String mediumLabel = split.length >= 4 ? split[3] : label;
-		    mediumLabel = mediumLabel.isEmpty() ? label : mediumLabel;
-		    int footerTabindex = 150 + i ;
-
-            Page linkPage = resourceResolver.resolve(link).adaptTo(Page.class);
-                    
-            if (linkPage != null && !link.contains(".html")) {
-                link += ".html";
+if(currentNode.hasNode("navs")){	
+	Node links = currentNode.getNode("navs");
+    NodeIterator iter = links.getNodes();
+    %><ul class="inline-list"><%
+    	while(iter.hasNext()){ 
+    		Node linkNode = iter.nextNode();
+    		String label = linkNode.hasProperty("label") ? linkNode.getProperty("label").getString():"";
+    		String path = linkNode.hasProperty("path") ? linkNode.getProperty("path").getString():"";
+    		String cssClass = linkNode.hasProperty("class") ? linkNode.getProperty("class").getString():"";
+    		long footerTabindex = 150 + iter.getPosition();
+    		Page linkPage = resourceResolver.resolve(path).adaptTo(Page.class);            
+            if (linkPage != null && !path.contains(".html")) {
+            	path += ".html";
             }
             if (!label.isEmpty()) {%>
-				<li id="tag_footer_<%= linkifyString(label, 25)%>"><a href="<%= link %>" title="<%= label %>" tabindex=<%=footerTabindex%>><%= label %></a></li>
+				<li id="tag_footer_<%= linkifyString(label, 25)%>"><a href="<%= path %>" title="<%= label %>" tabindex=<%=footerTabindex%>><%= label %></a></li>
 <%			}
-        } 
-%>
-        </ul>
-<%
-	}
+    	}
+    %></ul><%
+}
 %>
---
+++ tools/6.3-migration/gsusa/groovy-console-scripts/footer-nav-migration.groovy
@@ -0,0 +1,96 @@
+import javax.jcr.Node
+import org.apache.jackrabbit.commons.JcrUtils
+import javax.jcr.query.*
+
+
+String QUERY_LANGUAGE = "JCR-SQL2";
+String PATH = "/content";
+String RESOURCE_TYPE = 'gsusa/components/footer-nav';
+
+String EXPRESSION = "SELECT s.[jcr:path] "+
+                    "FROM [nt:unstructured] AS s "+
+                    "WHERE ISDESCENDANTNODE('"+PATH+"') AND "+
+                    "s.[sling:resourceType]='"+RESOURCE_TYPE+"'";
+QueryResult result = search(EXPRESSION, QUERY_LANGUAGE)
+
+if (result != null) {
+	try {
+		RowIterator rowIter = result.getRows()
+		while (rowIter.hasNext()) {
+			Row row = rowIter.nextRow()
+			Node node = row.getNode()
+			if(node.hasProperty("navs") ){
+			    Node linksNode = null;
+			    if(!node.hasNode("navs")){
+			        linksNode = node.addNode("navs", "nt:unstructured");
+			    }else{
+			        linksNode = node.getNode("navs");
+			    }
+			     println("**************************************************************************************")
+			     println(linksNode.getPath())
+			     try{
+				     Value[] values = node.getProperty("navs").getValues()
+	                 println("Processing eyebrow-nav with " + values.length + " values")
+	                    
+	                 for(int i = 0; i < values.length; i++){
+	                    String linkSet = values[i].getString()
+	                    println(linkSet);
+	                    String[] linkProperties = linkSet.split("\\|\\|\\|");
+	                    Node itemNode = null
+	                    if(!linksNode.hasNode("item" + i)){
+	                        itemNode = linksNode.addNode("item" + i, "nt:unstructured")
+	                    } else{
+	                        itemNode = linksNode.getNode("item" + i);
+	                    }						
+	                    String label = linkProperties[0]
+	                    String path = linkProperties.length >= 2 ? linkProperties[1] : ""
+	                    String clazz = linkProperties.length >= 3 ? linkProperties[2] : ""
+	                    itemNode.setProperty("label", label)
+	                    itemNode.setProperty("class", clazz)
+	                    itemNode.setProperty("path", path)
+	                        
+	                        
+	                }
+			     }catch(Exception e1){
+					 try{
+						 Value value = node.getProperty("navs").getValue()
+						 String linkSet = value.getString()
+						 println(linkSet);
+						 String[] linkProperties = linkSet.split("\\|\\|\\|");
+						 Node itemNode = null
+						 if(!linksNode.hasNode("item" + i)){
+							 itemNode = linksNode.addNode("item" + i, "nt:unstructured")
+						 } else{
+							 itemNode = linksNode.getNode("item" + i);
+						 }
+						String label = linkProperties[0]
+	                    String path = linkProperties.length >= 2 ? linkProperties[1] : ""
+	                    String clazz = linkProperties.length >= 3 ? linkProperties[2] : ""
+	                    itemNode.setProperty("label", label)
+	                    itemNode.setProperty("class", clazz)
+	                    itemNode.setProperty("path", path)
+					 }catch(Exception e2){}
+				 }			        
+			    save()
+			        println("saved converted content")			    
+			}
+			
+		}
+	} catch (Exception e) {
+	    println(e.getMessage())
+		e.printStackTrace()
+	}
+}
+
+def QueryResult search(EXPRESSION, QUERY_LANGUAGE) {
+    println(EXPRESSION)
+	QueryResult result = null;
+	try {
+		QueryManager queryManager = session.getWorkspace().getQueryManager();
+		Query sql2Query = queryManager.createQuery(EXPRESSION, QUERY_LANGUAGE);
+		return sql2Query.execute();
+	} catch (Exception e) {
+		e.printStackTrace();
+	}
+	return result;
+}
