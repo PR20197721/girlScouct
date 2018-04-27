2109c2ef4d29e2b147ea56cf91d0ae32a5b3b37a
Juan Carlos Duran <qheolet@gmail.com>
2018-04-27 12:05:28 -0400

    add scroll to the dialogo

--- common/app/src/main/content/jcr_root/etc/clientlibs/girlscouts-common/util/dialog-scroll-fix/main.js
+++ common/app/src/main/content/jcr_root/etc/clientlibs/girlscouts-common/util/dialog-scroll-fix/main.js
@@ -1,10 +1,41 @@
-(function (document, $, ns) { 
-    "use strict";
-    $(document).on("dialog-ready", function() { 
-        try{ 
-        	alert("dialog ready");
-        }catch(err){
-        	
-        }
-    });
+(function(document, $, ns) {
+	'use strict';
+
+	$(document).on('dialog-loaded', function(s) {
+		try {
+			setTimeout(function(){
+				var clicked = false,
+					clickY;
+				$('.coral-Multifield-item').on({
+					mousemove: function(e) {
+						if (clicked) {
+							updateScrollPos(e);
+						}
+					},
+					mousedown: function(e) {
+						console.log(e.target);
+						clicked = true;
+						clickY = e.pageY;
+					},
+					mouseup: function() {
+						clicked = false;
+						$('html').css('cursor', 'auto');
+					},
+				});
+
+				var updateScrollPos = function(e) {
+					$('html').css('cursor', 'row-resize');
+					$('.cq-dialog-content').scrollTop(
+						$('.cq-dialog-content').scrollTop() + (e.pageY - clickY)
+					);
+				};
+
+				$(document).on('dialog-closed', function(s) {
+					$('.coral-Multifield-item').off('mousemove');
+					$('.coral-Multifield-item').off('mousedown');
+					$('.coral-Multifield-item').off('mouseup');
+				});
+			}, 50);
+		} catch (err) {}
+	});
 })(document, Granite.$, Granite.author);
