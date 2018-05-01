dfdfdf09a6b49eeebc0b99e8598ad418dbcf3848
Juan Carlos Duran <qheolet@gmail.com>
2018-05-01 15:25:52 -0400

    new propose

--- common/app/src/main/content/jcr_root/etc/clientlibs/girlscouts-common/util/dialog-scroll-fix/main.js
+++ common/app/src/main/content/jcr_root/etc/clientlibs/girlscouts-common/util/dialog-scroll-fix/main.js
@@ -2,6 +2,7 @@
 	'use strict';
 
 	$(document).on('dialog-loaded', function(s) {
+
 		try {
 			setTimeout(function(){
 				var clicked = false,
@@ -9,27 +10,47 @@
 					elementinDrag;
 				$('.coral-Multifield-item').on({
 					mousemove: function(e) {
+						var content = $('.cq-dialog-content');
+						var top = content.offset().top;
+						var place = content.height();
+						var updown;
+
+						console.log(top,place,e.pageY, e.pageY + 100 < place * 0.30, place * 0.30)
+
+						if( e.pageY-50 < place * 0.30){
+								console.log('up',e.pageY)
+								updown=-1;
+						}
+
+
+						if(e.pageY > place* 0.80){
+								console.log('down',e.pageY)
+								updown=1
+						}
+					
 						if (clicked) {
-							updateScrollPos(e, this);
+							updateScrollPos(e, this, updown);
 						}
 					},
 					mousedown: function(e) {
 						clicked = true;
 						clickY = e.pageY;
+
+					
 						
 					},
 					mouseup: function() {
 						clicked = false;
-					
+
 					},
 				});
 
 				var max = $('.coral-Multifield').height();
 
-				var updateScrollPos = function(e, element) {
+				var updateScrollPos = function(e, element, dir) {
 					// $('html').css('cursor', 'row-resize');
-
-					var top = $('.cq-dialog-content').scrollTop() + (e.pageY - clickY);
+				
+					var top = $('.cq-dialog-content').scrollTop() + dir*60 ;
 
 					if(top < max - $(element).height()){
 						$('.cq-dialog-content').scrollTop(top);
