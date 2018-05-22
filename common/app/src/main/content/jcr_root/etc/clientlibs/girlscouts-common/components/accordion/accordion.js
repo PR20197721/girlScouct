1ce2dfbb474a37487f52908779fbdb969ab96f23
Unknown <matt@idea-refinery.com>
2018-05-22 08:47:17 -0400

    Animate to anchor and open accordion tab by hash.

--- common/app/src/main/content/jcr_root/etc/clientlibs/girlscouts-common/components/accordion/accordion.js
+++ common/app/src/main/content/jcr_root/etc/clientlibs/girlscouts-common/components/accordion/accordion.js
@@ -1,22 +1,22 @@
 window.AccordionWidgetManager = (function(window, document, $){
-	
+
 	function toggleAccordionElement(){
-		
+
 		var accordionElement = $(this).data('accordion-element') || $(this).parent('.accordion');
 		var headerElements = accordionElement.find('.accordionComponentHeader');
 		var contentElements = accordionElement.find('.accordion-navigation .content');
-		
+
 		var show = !$(this).data('accordion-element-shown');
 
 		// Get the target content elements.
 		var contentElement = $(this).next('.accordion-navigation').children('.content');
-		
+
 		// Fix the classes.
 		headerElements.add(contentElements).removeClass('on accordionHeaderShown').data('accordion-element-shown', false);
 		if(show){
 			$(this).add(contentElement).addClass('on accordionHeaderShown').data('accordion-element-shown', true);
 		}
-				
+
 		// Toggle parsys if we have one after the animation is complete.
 		headerElements.not($(this)).each(function(){
 			var nonActiveToggleParsys = $(this).data('toggle-parsys');
@@ -24,7 +24,7 @@ window.AccordionWidgetManager = (function(window, document, $){
 				nonActiveToggleParsys.hideParsys();
 			}
 		});
-		
+
 		// Show the parsys after open.
 		var activeToggleParsys = $(this).data('toggle-parsys');
 		if(activeToggleParsys){
@@ -34,7 +34,7 @@ window.AccordionWidgetManager = (function(window, document, $){
 				activeToggleParsys.hideParsys();
 			}
 		}
-					
+
 		// Hide other elements and show the selected.
 		if(show){
 			contentElements.not(contentElement).slideUp('slow');
@@ -43,21 +43,21 @@ window.AccordionWidgetManager = (function(window, document, $){
 			contentElements.slideUp('slow');
 		}
 	}
-	
+
 	function _initAccordionComponent(element){
-		
-		var headerElements = element.find('.accordionComponentHeader');
-		var contentElements = element.find('.accordion-navigation .content');
+
+		var headerElements = element.children('.accordionComponentHeader');
 
 		var isEdit = false;
 		try{
 			isEdit = readCookie('wcmmode') == 'edit';
 		}catch(err){}
-		
+
 		var toggleParsysExists = false;
 		try{
 			toggleParsysExists = !!TouchUI.Utils.ToggleParsys;
 		}catch(er){}
+
 		if(isEdit && toggleParsysExists){
 			headerElements.each(function(){
 				var parsysIdentifier = $(this).data('parsys-identifier');
@@ -72,30 +72,58 @@ window.AccordionWidgetManager = (function(window, document, $){
 				toggleParsys.hideParsys();
 			});
 		}
-		
+
 		// Apply the header elements click functions.
 		headerElements
 			.data('accordion-element', $(element))
 			.data('accordion-element-shown', false)
 			.off('click.accordionHeader')
 			.on('click.accordionHeader', toggleAccordionElement);
-		
+
+		// Open accordion with ID = hash.
+		var currentHash = document.location.hash;
+		if(currentHash && currentHash.length > 1){
+			currentHash = currentHash.replace('#', '');
+		}
+
+		var thisHeader = headerElements.filter(function(){
+			return $(this).attr('id') == currentHash;
+		});
+
+		var parentHeaders = thisHeader
+			.parents('.accordion-navigation')
+			.prev();
+
+		for(var i = parentHeaders.length - 1; i >= 0; i--){
+			$(parentHeaders[i]).trigger('click.accordionHeader');
+		}
+
+		if(thisHeader.length > 0){
+			thisHeader.trigger('click.accordionHeader');
+			window.setTimeout(function(){
+				var targetHeader = $('#' + currentHash);
+				targetHeader.next('.accordion-navigation').children('.content').promise().then(function(){
+					$('body').animate({scrollTop: $('#' + currentHash).offset().top - 200});
+				});
+			}, 100);
+		}
+
 	}
-	
-	function init(){	
+
+	function init(){
 		$('.accordionComponent').filter(function(){
 			return !$(this).data('accordion-initialized');
 		}).each(function(){
 			AccordionWidgetManager.initAccordionComponent($(this).data('accordion-initialized', true));
 		});
 	}
-	
+
 	// Init everything that's already on the page.
 	$(init);
-	
+
 	return {
 		// Expose so this can be used for async accordions.
 		initAccordionComponent : _initAccordionComponent
 	}
-		
+
 })(window, document, $);
