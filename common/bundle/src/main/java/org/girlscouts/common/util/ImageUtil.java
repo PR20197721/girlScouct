d6247ffd45ca77ff64289a09abc512d071f87ebf
Unknown <matt@idea-refinery.com>
2018-05-22 13:37:02 -0400

    Allow custom image types.

--- common/bundle/src/main/java/org/girlscouts/common/util/ImageUtil.java
+++ common/bundle/src/main/java/org/girlscouts/common/util/ImageUtil.java
@@ -7,12 +7,20 @@ import org.girlscouts.web.wcm.foundation.Image;
 
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Function;
 
 public class ImageUtil {
 
+	private static final List<String> imageResourceTypes = Arrays.asList(
+			"foundation/components/image",
+			"gsusa/components/image",
+			"girlscouts/components/image"
+	);
+
 	public static Image getImage(ResourceResolver resourceResolver, Resource resource, String relativePath){
-		return getImage(resourceResolver, resource, relativePath, res -> new Image(res));
+		return getImage(resourceResolver, resource, relativePath, Image::new);
 	}
 
 	public static Image getImage(ResourceResolver resourceResolver, Resource resource, String relativePath, Function<Resource, Image> resourceImageCreator){
@@ -26,7 +34,11 @@ public class ImageUtil {
 			return null;
 		}
 
-		if(!resourceResolver.isResourceType(imageResource, "foundation/components/image")){
+		boolean isImageResource = imageResourceTypes
+				.stream()
+				.anyMatch(type -> resourceResolver.isResourceType(imageResource, type));
+
+		if(!isImageResource){
 			Node node = imageResource.adaptTo(Node.class);
 			try {
 				Image img = resourceImageCreator.apply(resourceResolver.resolve(node.getProperty("fileReference").getString()));
