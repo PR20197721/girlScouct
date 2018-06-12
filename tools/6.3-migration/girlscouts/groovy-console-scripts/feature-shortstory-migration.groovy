37f45f03e56d7dff3fd82985569317034cd4a7fc
kapluig <kapluig@US013760499W10.local>
2018-06-12 14:40:09 -0400

    Added migration script for featured shortstory

---
+++ tools/6.3-migration/girlscouts/groovy-console-scripts/feature-shortstory-migration.groovy
@@ -0,0 +1,53 @@
+import javax.jcr.Node
+import org.apache.jackrabbit.commons.JcrUtils
+import javax.jcr.query.*
+
+
+String QUERY_LANGUAGE = "JCR-SQL2";
+String PATH = "/content";
+String RESOURCE_TYPE = 'girlscouts/components/news-list';
+
+String EXPRESSION = "SELECT s.[jcr:path] "+
+                    "FROM [nt:unstructured] AS s "+
+                    "WHERE ISDESCENDANTNODE('"+PATH+"') AND "+
+                    "s.[sling:resourceType]='"+RESOURCE_TYPE+"'";
+QueryResult result = search(EXPRESSION, QUERY_LANGUAGE)
+
+if (result != null) {
+	try {
+		RowIterator rowIter = result.getRows()
+		while (rowIter.hasNext()) {
+			Row row = rowIter.nextRow()
+			Node node = row.getNode()
+			if(node.hasProperty("isonhomepage") ){
+                String homePageCheck = node.getProperty("isonhomepage").getValue().getString();
+                if(homePageCheck.equals("on")){
+                    println("Found old newslist set to on at: " + node.getPath())
+                    node.setProperty("isonhomepage", "true")
+                }
+                if(homePageCheck.equals("off")){
+                    println("Found old newslist set to off at: " + node.getPath())
+                    node.setProperty("isonhomepage", "false")
+                }
+                save()
+			}
+			
+		}
+	} catch (Exception e) {
+	    println(e.getMessage())
+		e.printStackTrace()
+	}
+}
+
+def QueryResult search(EXPRESSION, QUERY_LANGUAGE) {
+    println(EXPRESSION)
+	QueryResult result = null;
+	try {
+		QueryManager queryManager = session.getWorkspace().getQueryManager();
+		Query sql2Query = queryManager.createQuery(EXPRESSION, QUERY_LANGUAGE);
+		return sql2Query.execute();
+	} catch (Exception e) {
+		e.printStackTrace();
+	}
+	return result;
+}
