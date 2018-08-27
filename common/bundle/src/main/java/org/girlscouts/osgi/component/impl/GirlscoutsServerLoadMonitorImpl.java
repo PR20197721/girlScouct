b134505fd16fb3f74701650c919f0ba864f4afb9
Unknown <dmitriy.bakum@ey.com>
2018-08-27 13:47:02 -0400

    minor refactoring

--- common/bundle/src/main/java/org/girlscouts/osgi/component/impl/GirlscoutsServerLoadMonitorImpl.java
+++ common/bundle/src/main/java/org/girlscouts/osgi/component/impl/GirlscoutsServerLoadMonitorImpl.java
@@ -36,8 +36,6 @@ public class GirlscoutsServerLoadMonitorImpl implements Runnable,GirlscoutsServe
 
 	private static final Logger log = LoggerFactory.getLogger(GirlscoutsServerLoadMonitor.class);
 
-	private boolean author;
-
 	private GirlscoutsServerLoadMonitorConfiguration config;
 
 	@Reference
@@ -49,23 +47,23 @@ public class GirlscoutsServerLoadMonitorImpl implements Runnable,GirlscoutsServe
 	@Reference
 	protected GSEmailService gsEmailService;
 
+	private boolean author;
 	private Map<String, Object> serviceParams;
-
 	private boolean isMaintenanceActive = false;
 	private boolean isNotificationSent = false;
 	private boolean isRecovered = true;
 	private String host = "";
-	List<String> emails = new ArrayList<String>();
+	List<String> emails;
 	OperatingSystemMXBean operatingSystemMXBean;
 
 	@Activate
 	private void activate(GirlscoutsServerLoadMonitorConfiguration config) {
 		this.config = config;
-		operatingSystemMXBean = ManagementFactory.getOperatingSystemMXBean();
-		author = settingsService.getRunModes().contains("author");
-		serviceParams = new HashMap<String, Object>();
-		serviceParams.put(ResourceResolverFactory.SUBSERVICE, "gs-server-load-monitor");
-		emails.addAll(Arrays.asList(config.emailAddresses()));
+		this.operatingSystemMXBean = ManagementFactory.getOperatingSystemMXBean();
+		this.author = settingsService.getRunModes().contains("author");
+		this.serviceParams = new HashMap<String, Object>();
+		this.serviceParams.put(ResourceResolverFactory.SUBSERVICE, "gs-server-load-monitor");
+		this.emails = Arrays.asList(config.emailAddresses());
 		try {
 			InetAddress addr;
 			addr = InetAddress.getLocalHost();
@@ -78,34 +76,34 @@ public class GirlscoutsServerLoadMonitorImpl implements Runnable,GirlscoutsServe
 
 	@Override
 	public void run() {
-		if (author) {
+		if (this.author) {
 			try {
 				double loadAvg = operatingSystemMXBean.getSystemLoadAverage();
 				log.info("current load = {}, max load={}, alert load={}, recover load={}", loadAvg,
-						config.maxLoad(), config.alertLoad(), config.recoverLoad());
-				if (loadAvg <= config.recoverLoad()) {
-					if (!isRecovered) {
+						this.config.maxLoad(), this.config.alertLoad(), this.config.recoverLoad());
+				if (loadAvg <= this.config.recoverLoad()) {
+					if (!this.isRecovered) {
 						log.info("Server recovered, current load = {}", loadAvg);
-						if (isMaintenanceActive) {
+						if (this.isMaintenanceActive) {
 							log.info("Deactivating maintenance page");
 							setMaintenance("", "");
 						}
-						if (isNotificationSent) {
+						if (this.isNotificationSent) {
 							sendRecoveryEmail(loadAvg);
 						}
 						resetFlags();
 					}
 				} else {
-					if (loadAvg >= config.alertLoad()) {
-						if (loadAvg >= config.maxLoad()) {
-							isRecovered = false;
-							if (!isMaintenanceActive) {
+					if (loadAvg >= this.config.alertLoad()) {
+						if (loadAvg >= this.config.maxLoad()) {
+							this.isRecovered = false;
+							if (!this.isMaintenanceActive) {
 								log.info("Activating maintenance page");
 								setMaintenance(config.maintenancePage(), config.regex());
 								sendMaintenanceEmail(loadAvg);
 							}
 						} else {
-							if (!isNotificationSent) {
+							if (!this.isNotificationSent) {
 								sendAlertEmail(loadAvg);
 							}
 						}
@@ -118,21 +116,22 @@ public class GirlscoutsServerLoadMonitorImpl implements Runnable,GirlscoutsServe
 	}
 
 	private void resetFlags() {
-		isRecovered = true;
-		isNotificationSent = false;
-		isMaintenanceActive = false;
+		this.isRecovered = true;
+		this.isNotificationSent = false;
+		this.isMaintenanceActive = false;
 	}
 
 	private void sendMaintenanceEmail(double loadAvg) {
 		try {
-			if (config.sendNotifications()) {
+			if (this.config.sendNotifications()) {
 				log.info("Sending Maintenance Notification");
 				String html = "Current system load average is : " + loadAvg
-						+ ". Redirect (" + config.regex() + ") to maintenance page (" + config.maintenancePage()
+						+ ". Redirect (" + this.config.regex() + ") to maintenance page ("
+						+ this.config.maintenancePage()
 						+ ") is activated.";
 				String subject = "Alert: Server " + host + " experiencing high load";
-				gsEmailService.sendEmail(subject, emails, html);
-				isNotificationSent = true;
+				this.gsEmailService.sendEmail(subject, emails, html);
+				this.isNotificationSent = true;
 				log.info("Maintenance Notification email sent for {}", host);
 			}
 		} catch (Exception e) {
@@ -142,12 +141,12 @@ public class GirlscoutsServerLoadMonitorImpl implements Runnable,GirlscoutsServe
 
 	private void sendAlertEmail(double loadAvg) {
 		try {
-			if (config.sendNotifications()) {
+			if (this.config.sendNotifications()) {
 				log.info("Sending warning email");
 				String html = "Current system load average is : " + loadAvg;
 				String subject = "Warning: Server " + host + " experiencing increased load";
-				gsEmailService.sendEmail(subject, emails, html);
-				isNotificationSent = true;
+				this.gsEmailService.sendEmail(subject, emails, html);
+				this.isNotificationSent = true;
 				log.info("Warning email sent for {}", host);
 			}
 		} catch (Exception e) {
@@ -157,12 +156,12 @@ public class GirlscoutsServerLoadMonitorImpl implements Runnable,GirlscoutsServe
 
 	private void sendRecoveryEmail(double loadAvg) {
 		try {
-			if (config.sendNotifications()) {
+			if (this.config.sendNotifications()) {
 				log.info("Sending recovery email");
 				String html = "Current system load average is : " + loadAvg
 						+ ". Redirect to maintenance page is deactivated.";
 				String subject = "Alert: Server " + host + " recovered from high load";
-				gsEmailService.sendEmail(subject, emails, html);
+				this.gsEmailService.sendEmail(subject, emails, html);
 				log.info("Recovery email sent for {}", host);
 			}
 		} catch (Exception e) {
@@ -173,14 +172,14 @@ public class GirlscoutsServerLoadMonitorImpl implements Runnable,GirlscoutsServe
 	private void setMaintenance(String page, String regex) {
 		ResourceResolver rr = null;
 		try {
-			rr = resolverFactory.getServiceResourceResolver(serviceParams);
-			Resource mapping = rr.resolve(config.mappingPath());
+			rr = this.resolverFactory.getServiceResourceResolver(this.serviceParams);
+			Resource mapping = rr.resolve(this.config.mappingPath());
 			if (!ResourceUtil.isNonExistingResource(mapping)) {
 				ModifiableValueMap map = mapping.adaptTo(ModifiableValueMap.class);
 				map.put("sling:internalRedirect", page);
 				map.put("sling:match", regex);
 				rr.commit();
-				isMaintenanceActive = !StringUtils.isBlank(regex);
+				this.isMaintenanceActive = !StringUtils.isBlank(regex);
 				log.info("Set maintenance mapping to:  sling:internalRedirect = {}, sling:match={}", page, regex);
 			}
 		} catch (Exception e) {
