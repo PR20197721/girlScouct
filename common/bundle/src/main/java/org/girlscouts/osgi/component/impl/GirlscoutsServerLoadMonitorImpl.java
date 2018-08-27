b45be80f5ee0e4ed0e288517c49e13cc4a325746
Unknown <dmitriy.bakum@ey.com>
2018-08-27 15:35:02 -0400

    modified regex so maintenance redirect doesn't interfere with admin functions

--- common/bundle/src/main/java/org/girlscouts/osgi/component/impl/GirlscoutsServerLoadMonitorImpl.java
+++ common/bundle/src/main/java/org/girlscouts/osgi/component/impl/GirlscoutsServerLoadMonitorImpl.java
@@ -4,7 +4,6 @@ import java.lang.management.ManagementFactory;
 import java.lang.management.OperatingSystemMXBean;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
