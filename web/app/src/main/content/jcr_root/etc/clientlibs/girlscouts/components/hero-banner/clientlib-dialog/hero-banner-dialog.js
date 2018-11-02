fd53f747610fe8dbbbf6242a23d2d4ff5ef1fb30
Unknown <dmitriy.bakum@ey.com>
2018-11-02 14:34:16 -0400

    implemented a workaround a bug when removed images in fileupload component will come back if you hit add button. added confirmation message when item is removed from multifield

--- web/app/src/main/content/jcr_root/etc/clientlibs/girlscouts/components/hero-banner/clientlib-dialog/hero-banner-dialog.js
+++ web/app/src/main/content/jcr_root/etc/clientlibs/girlscouts/components/hero-banner/clientlib-dialog/hero-banner-dialog.js
@@ -1,27 +1,51 @@
 (function (document, Granite, $) {
-	$(document).on("dialog-ready", function(e){
-		generateWarning("test");
+	$(document).on("coral-overlay:open", function(overlayEvent){
+		var $multifield = $("coral-dialog-content").find("coral-multifield[data-granite-coral-multifield-name='./slides']");
+		$multifield.find("button[handle='remove']").each(function() {
+    		setRemoveButtonMessage(this);
+    	});
+		$multifield.on("coral-component:attached", "button[handle='remove']", function (buttonEvent) {   
+    		setRemoveButtonMessage(this);
+        });	
+		$multifield.on("coral-component:attached", "coral-fileupload", function (fileUploadEvent) { 
+			if($(fileUploadEvent.target).is("coral-fileupload")){
+				$(fileUploadEvent.target).attr("data-cq-is-new","true");	
+			}
+        });			
 	});
-	function generateWarning(path){
-    	var dialog = new Coral.Dialog().set({
-    	    id: 'unpublish_warning',
-    	    backdrop:'static',
-    	    variant: 'error',
-    	    header: {
-    	      innerHTML: 'Warning!'
-    	    },
-    	    content: {
-    	      innerHTML: '<pAre you sure?</p>'
-    	    },
-    	    footer: {
-    	      innerHTML: '<button is="coral-button" variant="warning" coral-close>Yes</button><button is="coral-button" variant="warning" coral-close>No</button>'
-    	    }
+	function setRemoveButtonMessage(el){
+		$(el).on('mousedown', function(e){
+			e.stopPropagation();
+			e.preventDefault();
+    		generateWarning(el);
     	});
-    	dialog.on('coral-overlay:close', function(event) {
-    		dialog.remove();
-    		//$(".pageinfo-unpublish-confirm")[0].show();
-    	});
-    	document.body.appendChild(dialog);
-    	dialog.show();
+    }
+	function generateWarning(el){
+		if($('#unpublish_warning').length == 0){
+	    	var dialog = new Coral.Dialog().set({
+	    	    id: 'unpublish_warning',
+	    	    backdrop:'static',
+	    	    variant: 'warning',
+	    	    header: {
+	    	      innerHTML: 'Warning!'
+	    	    },
+	    	    content: {
+	    	      innerHTML: '<p>Are you sure?</p>'
+	    	    },
+	    	    footer: {
+	    	      innerHTML: '<button id="removeYesButton" is="coral-button" variant="warning">Yes</button><button is="coral-button" variant="warning" coral-close>No</button>'
+	    	    }
+	    	});
+	    	dialog.on('coral-overlay:close', function(event) {
+	    		dialog.remove();
+	    		//$(".pageinfo-unpublish-confirm")[0].show();
+	    	});
+	    	dialog.on('click', '#removeYesButton', function() {	   				
+	    		$(el).closest("coral-multifield-item").remove();
+	    		dialog.remove();
+	    	});
+	    	document.body.appendChild(dialog);
+	    	dialog.show();
+		}
     }
 })(document, Granite, Granite.$);
