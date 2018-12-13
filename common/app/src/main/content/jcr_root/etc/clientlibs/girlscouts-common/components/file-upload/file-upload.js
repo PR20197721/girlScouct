406390105dbd884ca14a6da6dcab50c62bdb05f1
Justin Said <Justin.P.Said@ey.com>
2018-12-13 15:39:08 -0500

    Added file-upload clientlib to restrict uploading Json files with ' . 's in the name to assets.

---
+++ common/app/src/main/content/jcr_root/etc/clientlibs/girlscouts-common/components/file-upload/file-upload.js
@@ -0,0 +1,46 @@
+(function (document, Granite, $) {
+    "use strict";
+    var isJson;
+    $(document).on('change','coral-chunkfileupload.coral-chunkfileupload', function(event, isJson) {
+        event.stopPropagation();
+        event.preventDefault();
+        var fileNames = "";
+        $.each(this.uploadQueue,function(index, item){
+            fileNames+=item.name;
+        });
+        isJson = fileNames.endsWith(".json") && fileNames.includes(".");
+        if(isJson){
+            var prevDialog = document.querySelector('#uploadListDialog');
+            prevDialog.hide();
+			var dialog = new Coral.Dialog().set({
+		    	    id: 'assets_folder_deletion_warning',
+		    	    backdrop:'static',
+		    	    variant: 'error',
+		    	    header: {
+		    	      innerHTML: 'Warning!'
+		    	    },
+		    	    content: {
+		    	      innerHTML: fileNames + " is invalid. Please remove all ' . 's from the file name before uploading"
+		    	    }
+		    	  });
+		    	var footer = dialog.querySelector('coral-dialog-footer');
+		        var okButton = new Coral.Button();
+		        okButton.label.textContent = Granite.I18n.get('OK');
+		        okButton.variant = 'primary';
+		        footer.appendChild(okButton).on('click', function (){
+		            dialog.hide();
+		            dialog.remove();
+                    var prevDialog = document.querySelector('#uploadListDialog');
+                    prevDialog.show();
+		        });
+		    	document.body.appendChild(dialog);
+		    	dialog.show();
+        }
+
+    });
+    $(document).on('coral-overlay:open','#assets_folder_deletion_warning', function(event, isJson) {
+        var dialog = document.querySelector('#uploadListDialog');
+        $(document.querySelector('#uploadListDialog').footer).find("button.dam-asset-upload-button").hide()
+        //dialog.footer.remove();
+    });
+})(document, Granite, Granite.$);
