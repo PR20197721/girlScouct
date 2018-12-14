a9e22d1da63ebb54bb070723df5994399ed77a2f
Justin Said <Justin.P.Said@ey.com>
2018-12-14 15:42:18 -0500

    Added and tested array implementation for multiple json files.

--- common/app/src/main/content/jcr_root/etc/clientlibs/girlscouts-common/components/file-upload/file-upload.js
+++ common/app/src/main/content/jcr_root/etc/clientlibs/girlscouts-common/components/file-upload/file-upload.js
@@ -4,21 +4,27 @@
     $(document).on('change','coral-chunkfileupload.coral-chunkfileupload', function(event, isJson) {
         event.stopPropagation();
         event.preventDefault();
-        var fileNames = "";
+        var fileNames = [];
         var baseUri = this.baseURI;
         var regex = new RegExp("^[^\.]+\.json$");
         $.each(this.uploadQueue,function(index, item){
             //if((item.name.endsWith(".json") && item.name.includes("."))){
             if(regex.test(item.name)){
-                fileNames+=item.name;
+                fileNames.push(item.name);
             }
         });
+        var printString = "";
+        for(var i = 0; i<fileNames.length; i++){
+			printString = printString + baseUri+"/"+fileNames[i] + " is invalid. Please remove all ' . 's from the file/folder name and replace with ' - ' before uploading";
+			printString = printString + "<br>";
+        }
         var folderRegex = new RegExp("^.+\/dam[^\.]+$");
         isJson = fileNames.length > 0 || folderRegex.test(baseUri);
         if(isJson){
             var prevDialog = document.querySelector('#uploadListDialog');
             prevDialog.hide();
             prevDialog.remove();
+            var i
 			var dialog = new Coral.Dialog().set({
 		    	    id: 'json_warning',
 		    	    backdrop:'static',
@@ -27,7 +33,7 @@
 		    	      innerHTML: 'Warning!'
 		    	    },
 		    	    content: {
-		    	      innerHTML: baseUri+"/"+fileNames + " is invalid. Please remove all ' . 's from the file/folder name and replace with ' - ' before uploading"
+		    	      innerHTML: printString
 		    	    }
 		    	  });
 		    	var footer = dialog.querySelector('coral-dialog-footer');
