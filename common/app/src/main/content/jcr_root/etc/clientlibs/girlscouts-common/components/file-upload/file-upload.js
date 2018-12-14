2f6f2792bb0fbfee0c3623c2a345937273524147
Justin Said <Justin.P.Said@ey.com>
2018-12-14 16:01:51 -0500

    Cleanup

--- common/app/src/main/content/jcr_root/etc/clientlibs/girlscouts-common/components/file-upload/file-upload.js
+++ common/app/src/main/content/jcr_root/etc/clientlibs/girlscouts-common/components/file-upload/file-upload.js
@@ -1,30 +1,27 @@
 (function (document, Granite, $) {
     "use strict";
-    var isJson;
-    $(document).on('change','coral-chunkfileupload.coral-chunkfileupload', function(event, isJson) {
+    $(document).on('change','coral-chunkfileupload.coral-chunkfileupload', function(event) {
         event.stopPropagation();
         event.preventDefault();
         var fileNames = [];
         var baseUri = this.baseURI;
         var regex = new RegExp("^[^\.]+\.json$");
         $.each(this.uploadQueue,function(index, item){
-            //if((item.name.endsWith(".json") && item.name.includes("."))){
             if(regex.test(item.name)){
                 fileNames.push(item.name);
             }
         });
-        var printString = "";
-        for(var i = 0; i<fileNames.length; i++){
-			printString = printString + baseUri+"/"+fileNames[i] + " is invalid. Please remove all ' . 's from the file/folder name and replace with ' - ' before uploading";
-			printString = printString + "<br>";
-        }
+
         var folderRegex = new RegExp("^.+\/dam[^\.]+$");
-        isJson = fileNames.length > 0 || folderRegex.test(baseUri);
-        if(isJson){
+        if(fileNames.length > 0 || folderRegex.test(baseUri)){
+            var printString = "";
+                for(var i = 0; i<fileNames.length; i++){
+                    printString = printString + baseUri+"/"+fileNames[i] + " is invalid. Please remove all ' . 's from the file/folder name and replace with ' - ' before uploading";
+            	    printString = printString + "<br>";
+                }
             var prevDialog = document.querySelector('#uploadListDialog');
             prevDialog.hide();
             prevDialog.remove();
-            var i
 			var dialog = new Coral.Dialog().set({
 		    	    id: 'json_warning',
 		    	    backdrop:'static',
