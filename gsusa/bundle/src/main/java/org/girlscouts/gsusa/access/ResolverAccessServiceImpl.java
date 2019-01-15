41f47b1be99168687830791c929b20efd794dce6
Unknown <dmitriy.bakum@ey.com>
2019-01-15 12:37:58 -0500

    refactored use of resource resolver to make sure it is closed

--- gsusa/bundle/src/main/java/org/girlscouts/gsusa/access/ResolverAccessServiceImpl.java
+++ gsusa/bundle/src/main/java/org/girlscouts/gsusa/access/ResolverAccessServiceImpl.java
@@ -3,8 +3,11 @@ package org.girlscouts.gsusa.access;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.jcr.Node;
+
 import org.apache.felix.scr.annotations.Activate;
 import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Deactivate;
 import org.apache.felix.scr.annotations.Properties;
 import org.apache.felix.scr.annotations.Property;
 import org.apache.felix.scr.annotations.Reference;
@@ -14,6 +17,8 @@ import org.apache.sling.api.resource.ResourceResolverFactory;
 import org.osgi.service.component.ComponentContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.day.cq.tagging.TagManager;
 
 @Component(metatype = true, label = "Resolver Access Service",
 description = "Assists with bulkeditor performance")
@@ -25,14 +30,12 @@ public class ResolverAccessServiceImpl implements ResolverAccessService{
 	
 	@Reference
 	private ResourceResolverFactory resolverFactory;
-	
-	
+
 	private Map<String, Object> serviceParams;
-    
+
     private static Logger log = LoggerFactory.getLogger(ResolverAccessServiceImpl.class);
     
-    private ResourceResolver rr = null;
-	
+
 	@Activate
 	private void activate(ComponentContext context) {
 		serviceParams = new HashMap<String, Object>();
@@ -40,16 +43,39 @@ public class ResolverAccessServiceImpl implements ResolverAccessService{
 		log.info("Access Resolver Service Activated.");
 	}
 	
-	public ResourceResolver getAccessResolver() {
+	@Override
+	public String getColorFromTagNode(String tagId) {
+		ResourceResolver rr = null;
+		String color = null;
 		try {
 			rr = resolverFactory.getServiceResourceResolver(serviceParams);
-		} catch(Exception e) {
-			log.error("Getting Access Resolver Failed with message: " + e.getMessage());
-		}
-		
-		return rr;
-		
-	}
-	
+			TagManager tagManager = rr.adaptTo(TagManager.class);
+			Node tag = tagManager.resolve(tagId).adaptTo(Node.class);
+			if (tag.hasProperty("color")) {
+				String hexColor = "FFFFFF";
+				hexColor = tag.getProperty("color").getString();
+				int rPart = Integer.parseInt(hexColor.substring(0, 2), 16);
+				int gPart = Integer.parseInt(hexColor.substring(2, 4), 16);
+				int bPart = Integer.parseInt(hexColor.substring(4, 6), 16);
+				color = "rgba(" + rPart + ", " + gPart + ", " + bPart + ", 0.8)";
 
+			}
+		} catch (Exception e) {
+			log.error("Getting Tag Node Failed with message: " + e.getMessage());
+		} finally {
+			if (rr != null) {
+				try {
+					rr.close();
+				} catch (Exception e) {
+					log.error("Error while closing resource resolver: " + e);
+				}
+			}
+		}
+		return color;
+	}
+
+	@Deactivate
+	private void deactivate(ComponentContext context) {
+		log.info("Access Resolver Service Deactivated.");
+	}
 }
