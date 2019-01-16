c50eae3bf1b71204a1f763301c403ef613afde53
Justin Said <Justin.P.Said@ey.com>
2019-01-16 14:53:11 -0500

    Updates to change constraints for hero-banners with spring positions to the right.

--- web/app/src/main/content/jcr_root/etc/clientlibs/girlscouts/components/hero-slideshow-images/hero-slideshow-images-dialog.js
+++ web/app/src/main/content/jcr_root/etc/clientlibs/girlscouts/components/hero-slideshow-images/hero-slideshow-images-dialog.js
@@ -9,7 +9,11 @@
               showDialog(el, width, length);
 			  var name = el.attr("name");
               if(name.includes("regular") || name.includes("medium")){
-				return "Size invalid. Correct dimensions: 960 x 420";
+                  if(springPosition == "right"){
+						return "Size invalid. Correct dimensions: 655 x 360";
+                  }else{
+					    return "Size invalid. Correct dimensions: 960 x 420";
+                  }
               }
               else if(name.includes("small")){
 				return "Size invalid. Correct dimensions: 500 x 655";
@@ -19,11 +23,19 @@
     });
 	var width;
     var length;
+    var springPosition;
     function showDialog(el){
         var test;
         var url = el.find("input[name='fileReference']").val();
         var name = el.attr("name");
         name = name.substring(0, name.indexOf("/"));
+
+        var message;
+        if(springPosition == "right"){
+			message = "The " + name + " asset size is invalid, your dimensions are: "+width+" x "+length+" <br>"+"<li>" + url+ "</li>" +  "It is detected that you are using the wrong size image. For design options with the springboards under hero, use 655 x 360 for regular and medium, 500 x 655 for small.";
+        } else{
+			message = "The " + name + " asset size is invalid, your dimensions are: "+width+" x "+length+" <br>"+"<li>" + url+ "</li>" +  "It is detected that you are using the wrong size image. For design options with the springboards under hero, use 960 x 420 for regular and medium, 500 x 655 for small.";
+        }
         var dialog = new Coral.Dialog().set({
               id: 'fileSize-warning',
               backdrop:'static',
@@ -32,7 +44,7 @@
                   innerHTML: 'Warning!'
               },
               content: {
-                  innerHTML: "The " + name + " asset size is invalid, your dimensions are: "+width+" x "+length+" <br>"+"<li>" + url+ "</li>" +  "It is detected that you are using the wrong size image. For design options with the springboards under hero, use 960 x 420 for regular and medium, 500 x 655 for small."
+                  innerHTML: message
               }
           });
           var footer = dialog.querySelector('coral-dialog-footer');
@@ -54,10 +66,18 @@
                     width = regWidth;
                     var regLength = data["tiff:ImageLength"];
                     length = regLength;
+                    springPosition = document.getElementsByName("./spplacement")[0].value;
                     if($(e.target).attr("name").includes("regular") || $(e.target).attr("name").includes("medium")){
-						if(regWidth != 960 || regLength != 420){
-                            $(e.target).attr('isValidSize',false);
-                    	}
+                        if(springPosition == "right"){
+							if(regWidth != 655 || regLength != 360){
+                            	$(e.target).attr('isValidSize',false);
+                    		}
+                        } else{
+							if(regWidth != 960 || regLength != 420){
+                            	$(e.target).attr('isValidSize',false);
+                    		}
+                        }
+
                     }
                     else if($(e.target).attr("name").includes("small")){
 						if(regWidth != 500 || regLength != 655){
