79437c427678ce76c554637909c3277a672c6818
Unknown <dmitriy.bakum@ey.com>
2019-02-22 10:25:26 -0500

    small fix for createStore

--- vtk/app/src/main/typescript/vtk-mtg-plan/store/store.ts
+++ vtk/app/src/main/typescript/vtk-mtg-plan/store/store.ts
@@ -3,7 +3,7 @@ import thunk from 'redux-thunk';
 import { createStore, applyMiddleware } from 'redux';
 import {createLogger} from 'redux-logger';
 
- const store = <any>createStore(reduce, undefined ,applyMiddleware(thunk, createLogger())) ;
+ const store = <any>createStore(reduce, void 0 ,applyMiddleware(thunk, createLogger())) ;
 
 
  export default store;
