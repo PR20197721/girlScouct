55d30e074732f987cc295da44215d0efa4a6e433
Unknown <dmitriy.bakum@ey.com>
2019-03-08 16:27:37 -0500

    updates

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+VERSION=$1
+
+# Get the current version if version number is not specified
+if [ -z $VERSION ]; then
+    VERSION=`head -1 VERSIONS.txt | cut -d ' ' -f 1`
+fi
+
+./common/deploy.sh $VERSION  && ./web/deploy.sh $VERSION && ./gsusa/deploy.sh $VERSION && ./vtk/deploy.sh $VERSION && ./gsactivities/deploy.sh $VERSION
+
+done
