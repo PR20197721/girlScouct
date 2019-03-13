e700192ebe4c4d0f18218313da385f2c26a2412a
Spencer <spencer.c.mulligan@ey.com>
2019-03-13 16:25:16 -0400

    DeployDev now deletes web-app

--- web/deployDev.sh
+++ web/deployDev.sh
@@ -7,6 +7,8 @@ if [ -z $VERSION ]; then
     VERSION=`head -1 ../VERSIONS.txt | cut -d ' ' -f 1`
 fi
 
+curl -u 'admin:e$Fz&rsBS.XZk$6F'  -X POST http://34.205.130.12:4502/crx/packmgr/service/.json/etc/packages/Girl%20Scouts/girlscouts-web-app-$VERSION.zip?cmd=delete
+
 curl -u 'admin:e$Fz&rsBS.XZk$6F' -F file=@"$HOME/.m2/repository/org/girlscouts/aem/web/girlscouts-web-app/$VERSION/girlscouts-web-app-$VERSION.zip" -F name="girlscouts-web-app" -F force=true -F install=true http://34.205.130.12:4502/crx/packmgr/service.jsp
 
 curl -u 'admin:e$Fz&rsBS.XZk$6F' -F file=@"$HOME/.m2/repository/org/girlscouts/aem/web/girlscouts-web-app/$VERSION/girlscouts-web-app-$VERSION.zip" -F name="girlscouts-web-app" -F force=true -F install=true http://34.236.166.152:4503/crx/packmgr/service.jsp
