401977aa953b2454ff2b7ce785f8340ddd21021a
Justin Said <Justin.P.Said@ey.com>
2019-03-20 16:30:34 -0400

    Cleanup

--- vtk/bundle/src/main/java/org/girlscouts/vtk/services/GirlscoutsEmailServlet.java
+++ vtk/bundle/src/main/java/org/girlscouts/vtk/services/GirlscoutsEmailServlet.java
@@ -58,16 +58,7 @@ public class GirlscoutsEmailServlet extends SlingAllMethodsServlet implements Op
         //parse files, get bytes, name, and mimeType
         try{
             while(slingRequest.getParameterMap().containsKey("file"+count)) {
-                    RequestParameter reqFile = slingRequest.getRequestParameter("file" + count);
-                    if(reqFile != null){
-                        String fN = slingRequest.getParameter("file" + count + "Name");
-                        String fT = reqFile.getContentType() != null ? reqFile.getContentType() : "";
-                        fT = fT.replaceAll("/", "_");
-                        fT = fT.toUpperCase();
-                        byte[] fB = reqFile.get();
-                        attachments.add(new GSEmailAttachment(fN, fB, "", GSEmailAttachment.MimeType.valueOf(fT)));
-                    }
-
+                addAttachment(slingRequest, attachments, count);
                 count++;
             }
         }catch (Exception e){
@@ -91,6 +82,25 @@ public class GirlscoutsEmailServlet extends SlingAllMethodsServlet implements Op
             }
         }
     }
+    private void addAttachment(SlingHttpServletRequest slingRequest,HashSet<GSEmailAttachment> attachments, int count){
+        try{
+            RequestParameter reqFile = slingRequest.getRequestParameter("file" + count);
+            if(reqFile != null){
+                String fN = slingRequest.getParameter("file" + count + "Name");
+                String fT = reqFile.getContentType() != null ? reqFile.getContentType() : "";
+                fT = fT.replaceAll("/", "_");
+                fT = fT.toUpperCase();
+                byte[] fB = reqFile.get();
+                attachments.add(new GSEmailAttachment(fN, fB, "", GSEmailAttachment.MimeType.valueOf(fT)));
+            }
+        }catch (Exception e){
+            log.error("Error adding attachment: ", e);
+        }
+
+    }
+
+
+
     /** OptingServlet Acceptance Method **/
     @Override
     public final boolean accepts(SlingHttpServletRequest request) {
