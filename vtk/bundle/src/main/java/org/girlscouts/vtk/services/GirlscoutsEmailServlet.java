561524c720b2b91bf16308426b840a1abe7518b9
Justin Said <Justin.P.Said@ey.com>
2019-03-20 11:40:36 -0400

    Servlet cleanup

--- vtk/bundle/src/main/java/org/girlscouts/vtk/services/GirlscoutsEmailServlet.java
+++ vtk/bundle/src/main/java/org/girlscouts/vtk/services/GirlscoutsEmailServlet.java
@@ -59,13 +59,18 @@ public class GirlscoutsEmailServlet extends SlingAllMethodsServlet implements Op
         try{
             while(slingRequest.getParameterMap().containsKey("file"+count)) {
                 try{
-                    RequestParameter req_file = slingRequest.getRequestParameter("file" + count);
-                    String fN = slingRequest.getParameter("file" + count + "Name");
-                    String fT = req_file.getContentType();
-                    fT = fT.replaceAll("/", "_");
-                    fT = fT.toUpperCase();
-                    byte[] fB = req_file.get();
-                    attachments.add(new GSEmailAttachment(fN, fB, "", GSEmailAttachment.MimeType.valueOf(fT)));
+                    RequestParameter reqFile = slingRequest.getRequestParameter("file" + count);
+                    if(reqFile != null){
+                        String fN = slingRequest.getParameter("file" + count + "Name");
+                        String fT = reqFile.getContentType();
+                        if(fT != null){
+                            fT = fT.replaceAll("/", "_");
+                            fT = fT.toUpperCase();
+                        }
+                        byte[] fB = reqFile.get();
+                        attachments.add(new GSEmailAttachment(fN, fB, "", GSEmailAttachment.MimeType.valueOf(fT)));
+                    }
+
                 }catch (Exception e){
                     log.error("Error parsing file information: ",e);
                 }
