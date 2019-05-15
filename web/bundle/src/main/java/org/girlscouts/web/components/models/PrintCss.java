e87882c56447baf9706b159589166f51c7d36fc5
Justin Said <Justin.P.Said@ey.com>
2019-05-15 12:57:48 -0400

    Added image component spacing

---
+++ web/bundle/src/main/java/org/girlscouts/web/components/models/PrintCss.java
@@ -0,0 +1,42 @@
+package org.girlscouts.web.components.models;
+
+import com.day.cq.wcm.api.Page;
+import jdk.nashorn.internal.ir.annotations.Reference;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.Self;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.PostConstruct;
+import javax.jcr.Node;
+import javax.jcr.NodeIterator;
+
+@Model(adaptables = SlingHttpServletRequest.class)
+public class PrintCss {
+    private static final Logger log = LoggerFactory.getLogger(PrintCss.class);
+    @Self
+    private SlingHttpServletRequest request;
+
+    @Reference
+    private Page currentPage;
+
+    private boolean addSpacing;
+
+    @PostConstruct
+    public void init(){
+        try{
+            NodeIterator nodeItrFirstEl = currentPage.adaptTo(Node.class).getNode("jcr:content/content/middle/par").getNodes();
+            Node currNode = nodeItrFirstEl.nextNode();
+            addSpacing = "girlscouts/components/image".equals(currNode.getProperty("sling:resourceType").getString());
+        }catch (Exception e){
+            log.error("Error parsing page components: ",e);
+            addSpacing = true;
+        }
+
+
+    }
+    public boolean getAddSpacing() {
+        return addSpacing;
+    }
+}
