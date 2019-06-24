4309aeba7fe5298636da4b723cb56752b4814de4
Justin Said <Justin.P.Said@ey.com>
2019-06-24 14:54:44 -0400

    Add reset year plan button

--- vtk/app/src/main/typescript/vtk-yp/vtk-yp-main.tsx
+++ vtk/app/src/main/typescript/vtk-yp/vtk-yp-main.tsx
@@ -358,12 +358,18 @@ VtkMainYpState > {
                         <div className="columns small-20 small-centered">
                             <div className="_intro ">
                                 <p style={{fontSize:'25px', fontWeight:'bold'}}><span><i style={{color:'orange',float:'initial', display:'inline-block', fontSize:'50px', marginBottom:'10px'}} className="icon-check"></i></span>The Troop's Year Plan is set<br /> <span style={{fontSize:'18px'}}>{________currentYearPlanName________ }</span>
-                                </p> 
-
-                                <p>To add, delete, or change a meeting, go to your Year Plan and click on the date square</p>
-                                <a href="/content/girlscouts-vtk/en/vtk.html" className="btn button btn-default" style={{maxWidth:'300px'}}>View my year plan</a> <br /> <br />
+                                </p>
+                                <div style={{display:'inline-flex'}}>
+                                <p style={{marginLeft:'35px', width:'166px'}}>To start over and erase all meeting details, including attendance and achievements:</p>
+                                <p style={{marginLeft:'200px', width:'166px'}}>To add, delete, or change a meeting, go to your current Year Plan</p>
+                                </div>
+                                <div>
+                                <a className="btn button btn-default resetExploreButton" style={{marginLeft:'50px', maxWidth:'300px', border:'1px solid #00a850', color:'#00a850', backgroundColor:'white'}}>Reset my year plan</a>
+                                <a href="/content/girlscouts-vtk/en/vtk.html" className="btn button btn-default selectedExploreButton" style={{marginLeft:'182px', maxWidth:'300px'}}>View my year plan</a>
+                                </div>
+                                <br /> <br />
                                 <p>To start over with a new Year Plan, use the choices below</p>
-                            </div>   
+                            </div>
                         </div>
                       </div>
                     :null}
