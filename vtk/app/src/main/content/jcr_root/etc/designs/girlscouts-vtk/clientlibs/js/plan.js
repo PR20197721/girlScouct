2cf33ac5ccfb4d75b87708350c2e7d14595f5fd7
Spencer <spencer.c.mulligan@ey.com>
2019-07-17 13:37:22 -0400

    Try #2

--- vtk/app/src/main/content/jcr_root/etc/designs/girlscouts-vtk/clientlibs/js/plan.js
+++ vtk/app/src/main/content/jcr_root/etc/designs/girlscouts-vtk/clientlibs/js/plan.js
@@ -172,7 +172,7 @@ function loadModalPage(link, showTitle, title, fullPageScroll, print, data, cach
     resetModalPage();
     var dataP = data || {};
 
-    $("#vtk-loading").css("display: block");
+    $("#vtk-loading").css("display","block");
 
     $.ajax({
         url: link,
@@ -186,7 +186,7 @@ function loadModalPage(link, showTitle, title, fullPageScroll, print, data, cach
     }).fail(function(response, status, xhr){
        $("#error").html(response + xhr.status + " " + xhr.statusText);
     }).complete(function(response){
-        $("#vtk-loading").css("display: none");
+        $("#vtk-loading").css("display","none");
     })
 
 
