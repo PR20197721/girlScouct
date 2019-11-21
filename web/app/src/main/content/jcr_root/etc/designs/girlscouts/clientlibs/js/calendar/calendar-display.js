7966821e311094cf4b5acfd47421600b6c9c96cf
Shyam_Gattupally <ssgattupally@girlscouts.org>
2019-11-20 19:19:37 -0500

    Final Code frezze version
    Final Code frezze version

--- web/app/src/main/content/jcr_root/etc/designs/girlscouts/clientlibs/js/calendar/calendar-display.js
+++ web/app/src/main/content/jcr_root/etc/designs/girlscouts/clientlibs/js/calendar/calendar-display.js
@@ -60,7 +60,7 @@ $('#fullcalendar').fullCalendar({
                 var start = new Date(data.start);
                 var end = new Date(data.end);
                 var description = data.description.substring(0,100);
-                var content = '<div class="row"><div class="small-24 large-24 medium-24 columns"><span class="calTitle"><h6><a href="'+data.path+'"><span class="calTitle">'+data.title+'</a></h6></span></div></div>' +
+                var content = '<div class="row"><div class="small-24 large-24 medium-24 columns"><span class="calTitle"><h6><a target="_blank" href="'+data.path+'"><span class="calTitle">'+data.title+'</a></h6></span></div></div>' +
                 '<div class="row"><div class="small-4 large-4 medium-4 columns" style="padding-right:0px"><b>Date:</b></div><div class="small-14 large-14 medium-14 columns" style="padding:0px"><b>'+data.displayDate+'</b></div><div class="small-6 large-6 medium-6 columns">&nbsp;</div></div>'+'<div class="row"><div class="small-6 large-6 medium-6 columns" style="padding-right:0px"><b>Location:</b></div><div class="small-12 large-12 medium-12 columns" style="padding:0px"><b>'+data.location+'</b></div><div class="small-6 large-6 medium-6 columns">&nbsp;</div></div>'+'<div class="row"><div class="small-24 large-24 medium-24 columns">'+description+'</div></div>'+'<div class="row"><div class="small-24 large-24 medium-24 columns">&nbsp</div></div>';
 
             tooltip.set({
