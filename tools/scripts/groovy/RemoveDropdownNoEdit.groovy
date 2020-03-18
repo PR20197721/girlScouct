f2b1c3b47310021936ff765c0876862f12340371
Rohini <rohini.nadendla@gds.ey.com>
2020-03-18 14:28:03 +0530

    GSWP-1928-code to hide dropdown-no-edit and script to remove occurrences in existing pages

---
+++ tools/scripts/groovy/RemoveDropdownNoEdit.groovy
@@ -0,0 +1,31 @@
+import javax.jcr.query.*
+import javax.jcr.*
+import java.util.logging.Logger
+import java.util.*
+import javax.jcr.Node.*
+import org.apache.commons.lang3.*
+
+def root = "/etc";
+def compName = "foundation/components/form/dropdown-no-edit";
+def values = [];
+
+//get all nodes having dropdown-no-edit component as a property value and remove it
+getNode(root).recurse {rootNode ->
+    if (rootNode.hasProperty('components')) {
+        Property prop = rootNode.getProperty("components");
+        if (prop.isMultiple()) {
+            values = prop.getValues();
+            def val = values as String[];
+            if (Arrays.asList(val).contains(compName)) {
+                val = ArrayUtils.removeAllOccurences(val, compName);
+                rootNode.setProperty("components", val);
+            }
+        } else {
+            final def compPath = prop.getString();
+            if (compPath.equals(compName)) {
+                prop.remove();
+            }
+        }
+        rootNode.save();
+    }
+}
