36607c1bb53af8bc330879d82bd7d399652af345
Zach Bebel <zach.bebel@ey.com>
2020-04-24 08:40:43 -0400

    GSVTK-2669 -- Refactor createAgenda to fix edge cases

--- vtk/app/src/main/typescript/vtk-mtg-plan/common/modal.tsx
+++ vtk/app/src/main/typescript/vtk-mtg-plan/common/modal.tsx
@@ -8,9 +8,9 @@ import { ActionsTypes } from '../store/actionsType';
 import { DetailedHTMLProps } from 'react';
 
 export interface VtkModalProps {
-    modal:{
-        title:string,
-        description:any
+    modal: {
+        title: string,
+        description: any
     }
     dispatch: any
 }
@@ -20,46 +20,31 @@ class VtkModal extends React.PureComponent<VtkModalProps, any> {
         window.onresize = () => {
             this.forceUpdate();
         };
-        window.onscroll = () =>{
+        window.onscroll = () => {
             this.forceUpdate();
         }
     }
 
     componentWillUnmount() {
-        window.onresize = () => {};
-        window.onscroll = () => {};
+        window.onresize = () => { };
+        window.onscroll = () => { };
     }
-    replaceAll(str, rep, val) {
-        if(str === null){
-            return;
-        }
-        return str.replace(new RegExp(rep, 'g'), val);
-    }
-    checkAgenda(innerText){
-        var assets = [];
-        var assetNames = [];
 
-        //Parse Meeting aid Titles and Hrefs into arrays
-        $(".__list_of_assets").children().each(function(){
-            assetNames.push($.trim($(this).find("a").text()));
-            assets.push($(this).find("a").attr("href"));
-        });
-        var contentVal;
-        try{
-            contentVal = innerText;
-        }catch(err){
-            return innerText;
-        }
-        for(var i = 0; i < $(".__list_of_assets").children().length; i++) {
-            try{
-                if(contentVal !== undefined){
-                    contentVal = this.replaceAll(contentVal, assetNames[i], "<a href='"+assets[i]+"'target='_blank'>" + assetNames[i]+"</a>");
-                }
-            } catch(err){
-                return innerText;
+    checkAgenda(innerText) {
+        var newHtml = innerText || "";
+        if (!newHtml.length) return "";
+
+        $(".__list_of_assets li a").each(() => {
+            var text = $(this).text() || "",
+                href = $(this).attr("href") || "",
+                link = `<a href='${href}' target='_blank'>${text}</a>`;
+            if (text.length && href.length) {
+                // Replace modal description text with asset links
+                newHtml.split(text).join(link);
             }
-        }
-        return contentVal;
+        });
+
+        return newHtml;
     }
 
     render() {
@@ -85,14 +70,13 @@ class VtkModal extends React.PureComponent<VtkModalProps, any> {
             });
         };
 
-        const heigthLogic = () => {
+        const heightLogic = () => {
             const w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
             let h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-
             const ratio = w / h;
 
-            if(h>600 && ratio > 1){
-                h=600;
+            if (h > 600 && ratio > 1) {
+                h = 600;
             }
             return h;
         }
@@ -102,14 +86,14 @@ class VtkModal extends React.PureComponent<VtkModalProps, any> {
         }
 
         return HELPER.objectIsEmpty(this.props.modal) ? <div className="vtk_modal __background" style={{ top: top() + "px" }}>
-            <div className="__modal" style={{height:heigthLogic()+'px'}}>
+            <div className="__modal" style={{ height: heightLogic() + 'px' }}>
                 <div className="__header">
                     <h3>
                         {this.props.modal.title}{" "}
                     </h3>
                     <span className="modal-close-btn" onClick={close} >X</span>
                 </div>
-                <div className="__description" style={{height:(heigthLogic()-40)+'px'}}>
+                <div className="__description" style={{ height: (heightLogic() - 40) + 'px' }}>
                     <div className="__scroll" dangerouslySetInnerHTML={{ __html: this.checkAgenda(this.props.modal.description) }} />
                 </div>
             </div>
