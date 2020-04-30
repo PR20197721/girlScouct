9d62d0e83dbac2d6ef34b654b4abac9720d37e24
Rohini <rohini.nadendla@gds.ey.com>
2020-04-30 17:31:09 +0530

    Refactored.

--- common/app/src/main/content/jcr_root/apps/girlscouts-common/clientlibs/base/base.js
+++ common/app/src/main/content/jcr_root/apps/girlscouts-common/clientlibs/base/base.js
@@ -2,10 +2,8 @@
 $(window).on('load', function() {
     // if runmode is not author
     if (!($('#currentPagePath').data('run-mode'))) {
-        console.log("Does anchor tag href starts with '#' ?"+ $('body a[href^="#"]'));
         var hashObj = $('body a[href^="#"]');
         hashObj.each(function() {
-            console.log("PAGEPATH:"+$('#currentPagePath').data('page-path') + ".html" + $(this).attr('href'));
             var newhref = $('#currentPagePath').data('page-path') + ".html" + $(this).attr('href');
             $(this).attr('href', newhref);
         });
