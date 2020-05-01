7d1aad45cb7e1cd604d534da0f968e62880febf8
Zach Bebel <zach.bebel@ey.com>
2020-05-01 08:59:40 -0400

    GSWP-1789 -- Changed extensionPattern for validators to allow for all extension types

--- common/app/src/main/content/jcr_root/apps/girlscouts-common/clientlibs/validators/classic-ui/js/vanityurl.validation.js
+++ common/app/src/main/content/jcr_root/apps/girlscouts-common/clientlibs/validators/classic-ui/js/vanityurl.validation.js
@@ -1,33 +1,18 @@
 (function($, window, document) {
-	var ignoredExtensions = [
-			'html',
-			'pdf',
-			'doc',
-			'docx',
-			'ppt',
-			'pptx',
-			'xls',
-			'xlsx',
-			'jpg',
-			'png',
-			'gif'
-		],
-		ignoredExtensionsStr = ignoredExtensions.join(', '),
-		extensionPattern = new RegExp(ignoredExtensions.join('$|') + '$'),
+	var extensionPattern = new RegExp("\\.[a-z]{1,5}$", "i"), // Matches any file ".ext" 1-5 chars, case-insensitive
 		externalSitePattern = new RegExp('\\w+\\.\\w+\\/'); // Matches "sitename.ext/"
 
 	// Set vtype for ExtJS validator
 	CQ.Ext.form.VTypes.vanityPathValidator = function(value, field) {
         var path = value,
 			isLowerCase = path === path.toLowerCase(),
-			containsIgnoredExtension = extensionPattern.test(path),
+			containsExtension = extensionPattern.test(path),
 			isExternalRedirect = externalSitePattern.test(path),
-			isValidVanityPath = isLowerCase || containsIgnoredExtension || isExternalRedirect;
-		
+			isValidVanityPath = isLowerCase || containsExtension || isExternalRedirect;
 		return isValidVanityPath;
     };
 
 	// Set vtype message
-	CQ.Ext.form.VTypes.vanityPathValidatorText = CQ.I18n.getMessage("Vanity paths not ending in the following extensions (" + 
-		ignoredExtensionsStr + ") or redirecting to an external site must be completely lower case");
+	CQ.Ext.form.VTypes.vanityPathValidatorText = 
+		CQ.I18n.getMessage("Vanity paths not ending in a file extension or redirecting to an external site must be completely lower case");
 }) ($, window, document);
--- common/app/src/main/content/jcr_root/apps/girlscouts-common/clientlibs/validators/touch-ui/js/vanityurl.validation.js
+++ common/app/src/main/content/jcr_root/apps/girlscouts-common/clientlibs/validators/touch-ui/js/vanityurl.validation.js
@@ -2,21 +2,7 @@
     /* Adapting window object to foundation-registry */
     var registry = $(window).adaptTo("foundation-registry"),
         selector = '[name="./sling:vanityPath"]',
-        ignoredExtensions = [
-            'html',
-            'pdf',
-            'doc',
-            'docx',
-            'ppt',
-            'pptx',
-            'xls',
-            'xlsx',
-            'jpg',
-            'png',
-            'gif'
-        ],
-        ignoredExtensionsStr = ignoredExtensions.join(', '),
-        extensionPattern = new RegExp(ignoredExtensions.join('$|') + '$'),
+        extensionPattern = new RegExp("\\.[a-z]{1,5}$", "i"), // Matches any file ".ext" 1-5 chars, case-insensitive
         externalSitePattern = new RegExp('\\w+\\.\\w+\\/'); // Matches "sitename.ext/"
 
     /*Validator for TextField - Any Custom logic can go inside validate function - starts */
@@ -26,17 +12,16 @@
             var element = $(el),
                 path = element.val().trim(),
                 isLowerCase = path === path.toLowerCase(),
-                containsIgnoredExtension = extensionPattern.test(path),
+                containsExtension = extensionPattern.test(path),
 				isExternalRedirect = externalSitePattern.test(path),
-                isValidVanityPath = isLowerCase || containsIgnoredExtension || isExternalRedirect,
+                isValidVanityPath = isLowerCase || containsExtension || isExternalRedirect,
                 pathList = $(selector).map(function() { return $(this).val().trim(); }).get(),
                 isDuplicate = pathList.filter(function(p){ return p === path; }).length > 1;
 
             if (path.length == 0) {
                 return "Please enter path";
             } else if (!isValidVanityPath) {
-                return "Vanity paths not ending in the following extensions (" + ignoredExtensionsStr + 
-                    ") or redirecting to an external site must be completely lower case"; 
+                return "Vanity paths not ending in a file extension or redirecting to an external site must be completely lower case"; 
             } else if (isDuplicate) {
                 return "Vanity paths cannot contain duplicates";
             }
