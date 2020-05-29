f3f6fcea525c274115521b1a590e6520fd9eba27
Zach Bebel <zach.bebel@ey.com>
2020-05-29 12:06:07 -0400

    GSWP-1789 -- Reformat JS

--- common/app/src/main/content/jcr_root/apps/girlscouts-common/clientlibs/validators/classic-ui/js/vanityurl.validation.js
+++ common/app/src/main/content/jcr_root/apps/girlscouts-common/clientlibs/validators/classic-ui/js/vanityurl.validation.js
@@ -1,18 +1,18 @@
-(function($, window, document) {
+(function ($, window, document) {
 	var extensionPattern = new RegExp("\\.[a-z]{1,5}$", "i"), // Matches any file ".ext" 1-5 chars, case-insensitive
 		externalSitePattern = new RegExp('\\w+\\.\\w+\\/'); // Matches "sitename.ext/"
 
 	// Set vtype for ExtJS validator
-	CQ.Ext.form.VTypes.vanityPathValidator = function(value, field) {
-        var path = value,
+	CQ.Ext.form.VTypes.vanityPathValidator = function (value, field) {
+		var path = value,
 			isLowerCase = path === path.toLowerCase(),
 			containsExtension = extensionPattern.test(path),
 			isUrl = externalSitePattern.test(path),
 			isValidVanityPath = (isLowerCase || containsExtension) && !isUrl;
 		return isValidVanityPath;
-    };
+	};
 
 	// Set vtype message
-	CQ.Ext.form.VTypes.vanityPathValidatorText = 
+	CQ.Ext.form.VTypes.vanityPathValidatorText =
 		CQ.I18n.getMessage("Vanity paths can only be in the form of a relative path (e.g. /content/gsusa/camp, cannot be an external site) and must be completely lower case if not ending in a file extension (e.g. pdf)");
-}) ($, window, document);
+})($, window, document);
--- common/app/src/main/content/jcr_root/apps/girlscouts-common/clientlibs/validators/touch-ui/js/vanityurl.validation.js
+++ common/app/src/main/content/jcr_root/apps/girlscouts-common/clientlibs/validators/touch-ui/js/vanityurl.validation.js
@@ -1,4 +1,4 @@
-(function($, window, document) {
+(function ($, window, document) {
     /* Adapting window object to foundation-registry */
     var registry = $(window).adaptTo("foundation-registry"),
         selector = '[name="./sling:vanityPath"]',
@@ -8,23 +8,23 @@
     /*Validator for TextField - Any Custom logic can go inside validate function - starts */
     registry.register("foundation.validation.validator", {
         selector: selector,
-        validate: function(el) {
+        validate: function (el) {
             var element = $(el),
                 path = element.val().trim(),
                 isLowerCase = path === path.toLowerCase(),
                 containsExtension = extensionPattern.test(path),
-				isUrl = externalSitePattern.test(path),
+                isUrl = externalSitePattern.test(path),
                 isValidVanityPath = (isLowerCase || containsExtension) && !isUrl,
-                pathList = $(selector).map(function() { return $(this).val().trim(); }).get(),
-                isDuplicate = pathList.filter(function(p){ return p === path; }).length > 1;
+                pathList = $(selector).map(function () { return $(this).val().trim(); }).get(),
+                isDuplicate = pathList.filter(function (p) { return p === path; }).length > 1;
 
             if (path.length == 0) {
                 return "Please enter path";
             } else if (!isValidVanityPath) {
-                return "Vanity paths can only be in the form of a relative path (e.g. /content/gsusa/camp, cannot be an external site) and must be completely lower case if not ending in a file extension (e.g. pdf)"; 
+                return "Vanity paths can only be in the form of a relative path (e.g. /content/gsusa/camp, cannot be an external site) and must be completely lower case if not ending in a file extension (e.g. pdf)";
             } else if (isDuplicate) {
                 return "Vanity paths cannot contain duplicates";
             }
         }
     });
-}) ($, window, document);
+})($, window, document);
