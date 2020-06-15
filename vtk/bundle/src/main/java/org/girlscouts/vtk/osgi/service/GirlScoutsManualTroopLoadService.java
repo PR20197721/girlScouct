9227018801806126ab443f6c6e4542214945d3a3
Dmitriy <dmitriy.bakum@ey.com>
2020-06-15 12:28:51 -0400

    temp save

---
+++ vtk/bundle/src/main/java/org/girlscouts/vtk/osgi/service/GirlScoutsManualTroopLoadService.java
@@ -0,0 +1,22 @@
+package org.girlscouts.vtk.osgi.service;
+
+import org.girlscouts.vtk.auth.models.ApiConfig;
+import org.girlscouts.vtk.models.Contact;
+import org.girlscouts.vtk.models.Troop;
+import org.girlscouts.vtk.models.User;
+
+import java.util.List;
+
+public interface GirlScoutsManualTroopLoadService {
+    ApiConfig getApiConfig(String token);
+
+    User getUser(ApiConfig apiConfig);
+
+    User getUserById(ApiConfig apiConfig, String userId);
+
+    List<Troop> getTroopInfoByUserId(ApiConfig apiConfig, String userId);
+
+    List<Contact> getContactsForTroop(ApiConfig apiConfig, Troop troop);
+
+    List<Contact> getTroopLeaderInfoByTroopId(ApiConfig apiConfig, String sfTroopId);
+}
