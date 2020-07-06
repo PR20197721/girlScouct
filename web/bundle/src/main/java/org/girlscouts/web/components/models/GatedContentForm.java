76093a56105f9eeba1428af8754b702958c1cc5e
Khalil Ganiga <kganiga02@users.noreply.github.com>
2020-07-06 11:40:17 +0530

    GSWP:2155: Added Gated Content Form component

--- web/bundle/src/main/java/org/girlscouts/web/components/models/GatedContentForm.java
+++ web/bundle/src/main/java/org/girlscouts/web/components/models/GatedContentForm.java
@@ -1,30 +1,107 @@
 package org.girlscouts.web.components.models;
 
-import com.day.cq.wcm.api.Page;
-import org.apache.sling.api.SlingHttpServletRequest;
-import org.apache.sling.models.annotations.Model;
-import org.apache.sling.models.annotations.injectorspecific.Self;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
+import javax.inject.Named;
 
-@Model(adaptables = SlingHttpServletRequest.class)
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.Self;
+
+@Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class GatedContentForm {
-    //todo
-    private Boolean remember;
 
-    @Self
-    private SlingHttpServletRequest request;
+	@Self
+	private Resource resource;
 
-    @Inject
-    private Page currentPage;
+	@Inject
+	private String remember;
 
-    @PostConstruct
-    public void init() {
-        //todo
-    }
+	@Inject
+	@Named("show-birthday")
+	private String showBirthdayPopup;
 
-    public Boolean getRemember() {
-        return remember;
-    }
+	@Inject
+	@Named("age-limit")
+	private String ageLimit;
+
+	@Inject
+	@Named("privacy-policy")
+	private String privacyPolicyUrl;
+
+	@Inject
+	@Named("expiration")
+	private String cookieExpirationPeriod;
+
+	@Inject
+	@Named("salesforce-campaign-id")
+	private String salesforceCampaignId;
+
+	@Inject
+	@Named("council-name")
+	private String councilName;
+
+	@Inject
+	private Resource extensions;
+
+	@Inject
+	private Resource files;
+
+	private Map<String, Object> attrMap = new HashMap<>();
+
+	private List<String> extensionList = new ArrayList<>();
+
+	private List<String> filesList = new ArrayList<>();
+
+	@PostConstruct
+	public void init() {
+
+		attrMap.put("remember", remember);
+		attrMap.put("ageLimit", ageLimit);
+		attrMap.put("cookieExpirationPeriod", cookieExpirationPeriod);
+		attrMap.put("salesforceCampaignId", salesforceCampaignId);
+
+		Iterator<Resource> extItr = extensions.listChildren();
+		while (extItr.hasNext()) {
+			extensionList.add(extItr.next().getValueMap().get("extension", String.class));
+		}
+
+		Iterator<Resource> filesItr = files.listChildren();
+		while (filesItr.hasNext()) {
+			filesList.add(filesItr.next().getValueMap().get("file", String.class));
+		}
+
+	}
+
+	public String getCouncilName() {
+		return councilName;
+	}
+
+	public String getShowBirthdayPopup() {
+		return showBirthdayPopup;
+	}
+
+	public String getPrivacyPolicyUrl() {
+		return privacyPolicyUrl;
+	}
+
+	public Map<String, Object> getAttrMap() {
+		return attrMap;
+	}
+
+	public List<String> getExtensionList() {
+		return extensionList;
+	}
+
+	public List<String> getFilesList() {
+		return filesList;
+	}
+
 }
---
+++ web/bundle/src/main/java/org/girlscouts/web/servlets/GatedContentFormServlet.java
@@ -0,0 +1,126 @@
+package org.girlscouts.web.servlets;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+
+import org.apache.commons.httpclient.DefaultHttpMethodRetryHandler;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.HttpStatus;
+import org.apache.commons.httpclient.NameValuePair;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.httpclient.params.HttpMethodParams;
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Properties;
+import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.Reference;
+import org.apache.felix.scr.annotations.Service;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.request.RequestParameter;
+import org.apache.sling.api.servlets.SlingAllMethodsServlet;
+import org.girlscouts.web.webtolead.config.WebToLeadConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.day.cq.wcm.foundation.forms.FormsHelper;
+
+@Component(metatype = false)
+@Service(Servlet.class)
+@Properties({ @Property(name = "sling.servlet.paths", value = "/bin/gatedcontentform"),
+		@Property(name = "sling.servlet.methods", value = "POST"),
+		@Property(name = "service.description", value = "Gated Content Form Servlet"), })
+public class GatedContentFormServlet extends SlingAllMethodsServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6610151934476719719L;
+
+	private static final Logger logger = LoggerFactory.getLogger(GatedContentFormServlet.class);
+
+	protected static final String URL_PROPERTY = "apiUrl";
+	private List<NameValuePair> requestParams = new LinkedList<>();
+
+	private int statusCode;
+
+	@Reference
+	private WebToLeadConfig webToLeadConfig;
+
+	@Override
+	protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response)
+			throws ServletException, IOException {
+		this.doPost(request, response);
+
+	}
+
+	@Override
+	protected void doPost(SlingHttpServletRequest request, SlingHttpServletResponse response)
+			throws ServletException, IOException {
+
+		String endPoint = webToLeadConfig.getAPIURL();
+		logger.info("End Point configured :" + endPoint);
+
+		requestParams.add(new NameValuePair("oid", webToLeadConfig.getOID()));
+		logger.info("OrgId :" + webToLeadConfig.getOID());
+
+		for (Iterator<String> itr = FormsHelper.getContentRequestParameterNames(request); itr.hasNext();) {
+			final String paraName = itr.next();
+			RequestParameter[] paras = request.getRequestParameters(paraName);
+			for (RequestParameter paraValue : paras) {
+				if (paraValue.isFormField()) {
+					requestParams.add(new NameValuePair(paraName, paraValue.getString()));
+				}
+			}
+		}
+		response.setContentType("text/html");
+
+		statusCode = getResponse(endPoint);
+		if (statusCode == HttpStatus.SC_OK) {
+			response.getWriter().write("success");
+			logger.info("Gated content form response code :" + HttpStatus.SC_OK);
+		} else {
+			response.sendError(statusCode,
+					"Sorry, we are having a difficulty processing your request.\nPlease try again later.");
+		}
+
+	}
+
+	private int getResponse(String endPoint) {
+
+		HttpClient client = new HttpClient();
+
+		PostMethod postMethod = new PostMethod(endPoint);
+
+		NameValuePair[] dataArray = requestParams.toArray(new NameValuePair[requestParams.size()]);
+
+		postMethod.setRequestBody(dataArray);
+
+		postMethod.addRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+		postMethod.getParams().setParameter(HttpMethodParams.RETRY_HANDLER,
+				new DefaultHttpMethodRetryHandler(3, false));
+
+		try {
+			statusCode = client.executeMethod(postMethod);
+			if (statusCode != HttpStatus.SC_OK) {
+				logger.error("Method failed: " + postMethod.getStatusLine());
+			}
+		} catch (HttpException e) {
+			logger.error("Fatal protocol violation: " + e.getMessage());
+		} catch (IOException e) {
+			logger.error("Fatal transport error: " + e.getMessage());
+		} catch (Exception e) {
+			logger.error("Fatal error:: " + e.getMessage());
+		} finally {
+			postMethod.releaseConnection();
+		}
+
+		return statusCode;
+	}
+
+}
