e2d104573f3547f5042c6eb4a17efcfab4c85a2d
Dmitriy <dmitriy.bakum@ey.com>
2020-08-07 15:42:38 -0400

    no message

--- vtk/app/src/main/content/jcr_root/apps/girlscouts-vtk/components/sso/logout/clientlibs/js/logout.js
+++ vtk/app/src/main/content/jcr_root/apps/girlscouts-vtk/components/sso/logout/clientlibs/js/logout.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-    gigya.accounts.logout();
+    gigya.accounts.logout({callback:logoutAEM});
 });
+function logoutAEM(response){
+    if ( response.errorCode == 0 ) {
+        window.location.href = "/system/sling/logout?resource=/content/girlscouts-vtk";
+    }
+    else {
+        alert('Error :' + response.errorMessage);
+    }
+}
