4daf5bbd43ddf84360286ca455644e746af369c9
Srawat09 <Saurav.Rawat@gds.ey.com>
2020-08-17 19:55:46 +0530

    GSWP-1913 hiding image display options for non-privileged users

---
+++ common/app/src/main/content/jcr_root/apps/girlscouts-common/clientlibs/hideDisplayOptions/hideDisplayOptions.js
@@ -0,0 +1,31 @@
+(function($, $document) {
+	$(document).on("cq-layer-activated", function(e) {
+		var showAllOptions;
+		$.ajax({
+			url: '/bin/getAssetFilter.html',
+			dataType: 'json',
+			success: function(data, status, xhr) { // success callback function
+				hideDisplayOptions(data.displayAllOptions);
+			},
+			error: function(jqXhr, textStatus, errorMessage) { // error callback 
+			}
+		});
+		function hideDisplayOptions(displayAllOptions) {
+			showAllOptions = displayAllOptions;
+			var hideList = ["Products", "Paragraphs", "Content Fragments", "Experience Fragments", "Paragraphs", "Design Packages", "Adaptive Forms", "Manuscript", "UGC", "Interactive Communications"];
+			if (!displayAllOptions) {
+				$(".coral3-SelectList-item").each(function() {
+					$(".coral3-SelectList-item").each(function() {
+						if (hideList.indexOf($(this).attr('value')) > -1) {
+							$(this).hide();
+						}
+					});
+				});
+			}
+		}
+		$('[name="assetfilter_type_selector"]').click(function(event) {
+			event.preventDefault();
+			hideDisplayOptions(showAllOptions);
+		});
+	});
+})(jQuery, jQuery(document));
