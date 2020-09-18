877145df861bea3e514d159f6e6fd72bd0529af8
Dmitriy <dmitriy.bakum@ey.com>
2020-09-18 11:41:00 -0400

    resolve merge conflict

--- vtk/replication-bundle/src/main/java/org/girlscouts/vtk/replication/NodeListener.java
+++ vtk/replication-bundle/src/main/java/org/girlscouts/vtk/replication/NodeListener.java
@@ -1,7 +1,6 @@
 package org.girlscouts.vtk.replication;
 
 import com.day.cq.replication.*;
-import com.day.cq.replication.ReplicationLog.Level;
 import org.girlscouts.vtk.osgi.component.TroopHashGenerator;
 import org.girlscouts.vtk.replication.NodeEventCollector.NodeEvent;
 import org.slf4j.Logger;
@@ -117,42 +116,6 @@ public class NodeListener implements EventListener {
         if (affectedCouncilInfo != null) {
             // BASE = /vtk-data
             cacheInvalidator.addPath(troopHashGenerator.getBase());
-        }
-    }
-
-    private class VTKReplicationListener implements ReplicationListener {
-        public void onStart(Agent agent, ReplicationAction action) {
-            // Do nothing
-            log.debug("Starting replication...");
-        }
-
-        public void onMessage(Level level, String msg) {
-            // Do nothing
-        }
-
-        public void onEnd(Agent agent, ReplicationAction action, ReplicationResult result) {
-            if (result.isSuccess()) {
-                if (log.isDebugEnabled()) {
-                    log.debug("Replication succeeded type: " + action.getType().getName() + " path: " + action.getPath());
-                }
-            } else {
-                ReplicationActionType type = action.getType();
-                String path = action.getPath();
-                replicateAsync(type, path);
-            }
-        }
-
-        public void onError(Agent agent, ReplicationAction action, Exception exception) {
-            replicateAsync(action.getType(), action.getPath());
-        }
-
-        private void replicateAsync(ReplicationActionType type, String path) {
-            try {
-                log.warn("Sync replication error. Trying to replicate asynchronously. type = " + type.getName() + " path = " + path);
-                replicator.replicate(session, type, path, asyncOpts);
-            } catch (ReplicationException re) {
-                log.error("Replication Exception even in async mode. Event not handled. type = " + type + " path = " + path);
-            }
         }
     }
 }
