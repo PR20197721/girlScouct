1ac5eef0fb5531f56e096fcbfad19aa701ea53ae
Raghav Chawla <ust48635@adobe.com>
2020-11-05 00:36:19 +0530

    Removed activity id mapping code from vtk data migration

--- vtk/bundle/src/main/java/org/girlscouts/vtk/osgi/component/util/VTKDataMigrationUtil.java
+++ vtk/bundle/src/main/java/org/girlscouts/vtk/osgi/component/util/VTKDataMigrationUtil.java
@@ -33,8 +33,6 @@ public class VTKDataMigrationUtil{
 
         @AttributeDefinition(name = "Path to Contact Id Mapping File", type = AttributeType.STRING) String contactIdMappingFile() default "/content/dam/vtk-vs2-data-migration/contact-id.csv";
 
-        @AttributeDefinition(name = "Path to Activity Id Mapping File", type = AttributeType.STRING) String activityIdMappingFile() default "/content/dam/vtk-vs2-data-migration/activity-id.csv";
-
         @AttributeDefinition(name = "Activity Registration URL pattern", type = AttributeType.STRING) String activityRegPattern() default "gsmembers.force.com";
 
     }
@@ -47,20 +45,17 @@ public class VTKDataMigrationUtil{
     private String troopIdMappingFile;
     private String userIdMappingFile;
     private String contactIdMappingFile;
-    private String activityIdMappingFile;
     private String activityRegPattern;
 
     private Map<String, String> troopIdMapping = new HashMap<String, String>();
     private Map<String, String> userIdMapping = new HashMap<String, String>();
     private Map<String, String> contactIdMapping = new HashMap<String, String>();
-    private Map<String, String> activityIdMapping = new HashMap<String, String>();
 
     @Activate
     void activate(VTKDataMigrationUtilConfiguration config) {
         this.troopIdMappingFile = config.troopIdMappingFile();
         this.userIdMappingFile = config.userIdMappingFile();
         this.contactIdMappingFile = config.contactIdMappingFile();
-        this.activityIdMappingFile = config.activityIdMappingFile();
         this.activityRegPattern = config.activityRegPattern();
         this.resolverParams.put(ResourceResolverFactory.SUBSERVICE, "vtkService");
         ResourceResolver rr = null;
@@ -81,11 +76,6 @@ public class VTKDataMigrationUtil{
             if(csvResource != null && !ResourceUtil.isNonExistingResource(csvResource)){
                 loadContents(csvResource, contactIdMapping);
                 log.debug("Loaded {} mappings for contact id", contactIdMapping.size());
-            }
-            csvResource = rr.resolve(this.activityIdMappingFile);
-            if(csvResource != null && !ResourceUtil.isNonExistingResource(csvResource)){
-                loadContents(csvResource, activityIdMapping);
-                log.debug("Loaded {} mappings for activity id", activityIdMapping.size());
             }
         } catch (Exception e) {
             log.error("Error Occurred: ", e);
@@ -143,10 +133,6 @@ public class VTKDataMigrationUtil{
 
     public Map<String, String> getContactIdMapping() {
         return this.contactIdMapping;
-    }
-
-    public Map<String, String> getActivityIdMapping() {
-        return this.activityIdMapping;
     }
 
     public String getActivityRegPattern() {
