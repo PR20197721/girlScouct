0f4f1d54502c8ce359b95dad7b51c82e0603d9bf
Subharun Mukherjee <subharun@moonraft.com>
2020-12-21 20:25:24 +0530

    fixed lookup call

--- gsusa/app/src/main/content/jcr_root/apps/gsusa/components/booth-finder/clientlibs/js/booth-finder.js
+++ gsusa/app/src/main/content/jcr_root/apps/gsusa/components/booth-finder/clientlibs/js/booth-finder.js
@@ -200,7 +200,7 @@ BoothFinder.prototype.processResult = function(result) {
 
     if (templateId == 'booths') {
         // Bind "View Details" buttons
-        $('.viewmap.button').on('click', function() {
+        $('.booth-finder .viewmap.button').on('click', function() {
             var booth = JSON.parse($(this).attr('data'));
             if(booth.detailsText == "Get Cookies"){
                 window.open(booth.visitBoothUrl);
@@ -269,6 +269,27 @@ BoothFinder.prototype.processResult = function(result) {
             }
             //GSDO-1024 :multiple-gmaps-api-call :End
 
+            //Lookup Call
+            var value = JSON.parse($(this).attr("data"));
+            var data = {
+                l : value.Location,
+                d : value.DateStart,
+                z : value.ZipCode,
+                s : "Website"
+            }
+
+            $.ajax({
+                url: boothListingLookupApiURL,
+                dataType: "json",
+                data: data,
+                success: function(data) {
+                if (data) {
+                    console.log('Redirecting from BoothFinder');
+                } else {
+                    console.log('Error occured in redirecting');
+                }
+                }
+            });
         });
 
         if (this.page == 1) {
@@ -476,25 +497,6 @@ function initFB() {
 
 function registerClickOfBoothFinderButton(){
   $('.booth-finder .viewmap').on('click', function() {
-	  var value = JSON.parse($(this).attr("data"));
-    var data = {
-        l : value.Location,
-        d : value.DateStart,
-        z : value.ZipCode,
-        s : "Website"
-    }
-
-      $.ajax({
-        url: boothListingLookupApiURL,
-        dataType: "json",
-        data: data,
-        success: function(data) {
-          if (data) {
-            console.log('Redirecting from BoothFinder');
-          } else {
-            console.log('Error occured in redirecting');
-          }
-        }
-      });
+    
   });
 }
