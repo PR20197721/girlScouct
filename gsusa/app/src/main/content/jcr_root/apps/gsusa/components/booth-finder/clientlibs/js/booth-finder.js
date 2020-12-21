835bedbfe2779f33bbc1249d237eeb1cff54712c
Ayush Mishra <hcl62603@adobe.com>
2020-12-21 21:56:33 +0530

    fixing 87 issue

--- gsusa/app/src/main/content/jcr_root/apps/gsusa/components/booth-finder/clientlibs/js/booth-finder.js
+++ gsusa/app/src/main/content/jcr_root/apps/gsusa/components/booth-finder/clientlibs/js/booth-finder.js
@@ -298,8 +298,9 @@ BoothFinder.prototype.processResult = function(result) {
     }
 
     // Share dialog
+    const shareModelEle = $("#booth-finder-result").find(".share-modal");
     var showShareDialog = $('#share-showShareDialog').attr('data') == 'true';
-    if (showShareDialog) {
+    if (showShareDialog && shareModelEle && shareModelEle.length ===0) {
         var shareModalHtml = Handlebars.compile($('#template-sharemodal').html())({
             buttonCaption: "SHARE WITH YOUR FRIENDS",
             header: $('#share-shareDialogHeader').attr('data'),
