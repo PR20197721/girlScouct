287a7cad4f20111d84bc6b208bbd3123701c2c47
Subharun Mukherjee <subharun@moonraft.com>
2021-02-17 20:43:53 +0530

    form id field hidden and dialog js changes

--- common/app/src/main/content/jcr_root/apps/foundation/components/form/start/clientlib/form-select.js
+++ common/app/src/main/content/jcr_root/apps/foundation/components/form/start/clientlib/form-select.js
@@ -1,23 +1,28 @@
 (function (document, $, Coral) {
 	var $doc = $(document);
 	$doc.on('foundation-contentloaded', function(e) {
-        $(".cq-dialog-dropdown-showhide").on("change",function (event, $formId) {
+        Coral.commons.ready(function (component) {
+        $(component).on("change",function (event, $formId) {
             switch ($(".cq-dialog-dropdown-showhide coral-select-item:selected").text()) {
               case "Send Mail":
-                $(".form-identifier").val("gsmail");
+                $(".form-identifier").attr('value', 'gsmail');
                 $(".form-identifier").prop('formid', 'gsmail');
                 break;
               case "Store Content":
-                $(".form-identifier").val("gsstore");
+                    $(".form-identifier").attr('value', 'gsstore');
+                $(".form-identifier").prop('formid', 'gsstore');
                 break;
               case "Web To Lead":
-                $(".form-identifier").val("web-to-lead");
+                    $(".form-identifier").attr('value','web-to-lead');
+                $(".form-identifier").prop('formid', 'web-to-lead');
                 break;
               case "Web To Case":
-                $(".form-identifier").val("web-to-case");
+                    $(".form-identifier").attr('value','web-to-case');
+                $(".form-identifier").prop('formid', 'web-to-case');
                 break;
-    
             }
+
+        })
         })
 	});
 })(document, Granite.$, Coral);
