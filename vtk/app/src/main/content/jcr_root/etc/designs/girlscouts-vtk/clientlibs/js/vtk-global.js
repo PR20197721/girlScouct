211c4c216132031c8468aa1bfc38054a4a3f753e
Subharun Mukherjee <subharun@moonraft.com>
2021-03-03 00:55:20 +0530

    non requirement images made static, requirement images clickable

--- vtk/app/src/main/content/jcr_root/etc/designs/girlscouts-vtk/clientlibs/js/vtk-global.js
+++ vtk/app/src/main/content/jcr_root/etc/designs/girlscouts-vtk/clientlibs/js/vtk-global.js
@@ -888,9 +888,9 @@ function generatePreviewMeetingHtml(data, meeting) {
 
 	$("#meetingSelect").append("<div class='meeting-item column small-24' id='vtk-mtg-" + data[meeting].id + "' data-url='" + data[meeting].path + "' data-meetingid='" + data[meeting].id + "'></div>")
 	$("[data-meetingid=" + data[meeting].id + "]").append("<div class='row' style='background:#f6f6f6;'><div>");
-	if (!(data[meeting].req !== null && data[meeting].req !== undefined && data[meeting].req !== "")) {
-		$("[data-meetingid=" + data[meeting].id + "]").find(".row").append("<div class='column small-24 medium-4'><div style='min-height:110px; width:100%'><div style='height:inherit;vertical-align:middle; text-align:center;width:100%'><img width='100' onclick='openRequirementDetail(this)' class='image  _requirement_modal' height='100' src='/content/girlscouts-vtk/service/meeting/icon." + data[meeting].id + ".png'></div></div></div>");
-	} else {
+	if (data[meeting].req === null || data[meeting].req == undefined || data[meeting].req === "") {
+		$("[data-meetingid=" + data[meeting].id + "]").find(".row").append("<div class='column small-24 medium-4'><div style='min-height:110px; width:100%'><div style='height:inherit;vertical-align:middle; text-align:center;width:100%'><img width='100' class='' height='100' src='/content/girlscouts-vtk/service/meeting/icon." + data[meeting].id + ".png'></div></div></div>");
+	} else if (data[meeting].req !== null && data[meeting].req !== undefined && data[meeting].req !== "") {
 		$("[data-meetingid=" + data[meeting].id + "]").find(".row").append("<div class='column small-24 medium-4'><div style='min-height:110px; width:100%'><div style='height:inherit;vertical-align:middle; text-align:center;width:100%'><img width='100' onclick='openRequirementDetail(this)' class='image  _requirement_modal' height='100' src='/content/girlscouts-vtk/service/meeting/icon." + data[meeting].id + ".png'></div></div></div>");
 		$("[data-meetingid=" + data[meeting].id + "]").append("<div class='__requiments_details row' style='display:none'><div class='column small-24' style='padding:10px;'><div class='_requiments_description'><p style='margin-bottom: 5px'><b>" + data[meeting].reqTitle + "</b></p>" + data[meeting].req + "</div><p style='text-align:center; margin-top:20px'><span class='vtk-button' style='cursor:pointer;' onclick='_closeME(this)'>&nbsp;&nbsp;&nbsp;CLOSE&nbsp;&nbsp;&nbsp;</span></p></div></div>");
 	}
